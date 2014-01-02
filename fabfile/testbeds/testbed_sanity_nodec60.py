c06e09eff60738d41fe8b23dfc52ddf3f9e6e345
Prashant Shetty <shettyp@nodeb11.englab.juniper.net>
2014-01-01 21:42:12 -0800

    new sanity setup for github image

---
+++ fabfile/testbeds/testbed_sanity_nodec60.py
@@ -0,0 +1,39 @@
+from fabric.api import env
+
+host1 = 'root@10.204.217.100'
+
+ext_routers = [('blr-mx1', '10.204.216.253')]
+router_asn = 64512
+public_vn_rtgt = 10003
+public_vn_subnet = "10.204.219.40/29"
+
+host_build = 'stack@10.204.216.49'
+
+env.roledefs = {
+    'all': [host1],
+    'cfgm': [host1],
+    'openstack': [host1],
+    'control': [host1],
+    'compute': [host1],
+    'collector': [host1],
+    'webui': [host1],
+    'database': [host1],
+    'build': [host_build],
+}
+
+env.hostnames = {
+    'all': ['nodec60']
+}
+
+env.passwords = {
+    host1: 'c0ntrail123',
+    host_build: 'contrail123',
+}
+
+env.test_repo_dir='/home/stack/centos_github_sanity/contrail-test'
+env.mail_from='vjoshi@juniper.net'
+env.mail_to='dl-contrail-sw@juniper.net'
+env.log_scenario='CentOS(GITHub) Single-Node Sanity'
+multi_tenancy=True
+env.interface_rename = False
+env.encap_priority =  "'MPLSoUDP','MPLSoGRE','VXLAN'"
