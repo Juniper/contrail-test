8eb991056ec9049b122df350e35713ad1bac0d2f
Shaju V.K <shajuvk@build01.contrail.juniper.net>
2014-01-23 16:20:20 -0800

    adding multitenancy test suite.

--- fabfile/tasks/tester.py
+++ fabfile/tasks/tester.py
@@ -348,7 +348,8 @@ def run_sanity(feature='sanity', test=None):
               'vdns'         : ['%s/scripts/vdns/vdns_tests.py' % repo],
               'discovery'    : ['%s/scripts/discovery_tests_with_setup.py' % repo],
               'analytics_scale' : ['%s/scripts/analytics_scale_tests_with_setup.py' % repo],
-              'performance'  : ['%s/scripts/performance/sanity.py' % repo]
+              'performance'  : ['%s/scripts/performance/sanity.py' % repo],
+              'multitenancy'  : ['%s/scripts/test_perms.py' % repo]
               }
 
     pre_cmd = 'source /opt/contrail/api-venv/bin/activate && '
