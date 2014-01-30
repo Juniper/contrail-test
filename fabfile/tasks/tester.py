03a2482b8999c63ca67fee7ff9a106badd88dd8c
Ganesha H V <ganeshahv@juniper.net>
2014-01-30 22:01:15 +0530

    Adding ECMP to sanity

--- fabfile/tasks/tester.py
+++ fabfile/tasks/tester.py
@@ -349,7 +349,8 @@ def run_sanity(feature='sanity', test=None):
               'discovery'    : ['%s/scripts/discovery_tests_with_setup.py' % repo],
               'analytics_scale' : ['%s/scripts/analytics_scale_tests_with_setup.py' % repo],
               'performance'  : ['%s/scripts/performance/sanity.py' % repo],
-              'multitenancy'  : ['%s/scripts/test_perms.py' % repo]
+              'multitenancy'  : ['%s/scripts/test_perms.py' % repo],
+              'ecmp'            : ['%s/scripts/ecmp/sanity_with_setup.py' %repo],
               }
 
     pre_cmd = 'source /opt/contrail/api-venv/bin/activate && '
