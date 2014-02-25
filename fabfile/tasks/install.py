1e75135cf33b3e384bba991b2481ac1d7903b0e0
Ignatious Johnson <ijohnson@juniper.net>
2014-02-25 14:33:08 -0800

    check platfrom and install interface rename package.

--- fabfile/tasks/install.py
+++ fabfile/tasks/install.py
@@ -392,7 +392,7 @@ def install_contrail(reboot='True'):
     execute(install_vrouter)
     execute(upgrade_pkgs)
     execute(update_keystone_log)
-    if getattr(env, 'interface_rename', True):
+    if getattr(env, 'interface_rename', True) and detect_ostype() in ['Ubuntu']:
         print "Installing interface Rename package and rebooting the system."
         execute(install_interface_name, reboot)
 
