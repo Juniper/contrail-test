2c9e565815976b56ff7c00e7c228942d6dbb6fab
Ganesha H V <ganeshahv@juniper.net>
2014-02-26 12:07:41 +0530

    Revert "Increasing retry and changing ping packet count for transparent service mode"
    This reverts commit bf5345c522bf817782b861ba3aa2809388a55f51.

--- fixtures/svc_instance_fixture.py
+++ fixtures/svc_instance_fixture.py
@@ -59,13 +59,10 @@ class SvcInstanceFixture(fixtures.Fixture):
             else:
                 si_prop = ServiceInstanceType()
             si_prop.set_scale_out(ServiceScaleOutType(self.max_inst))
-            bridge= False
-            if 'bridge_svc_instance_1' in self.si_fq_name:
-                bridge = True
             for itf in self.if_list:
-                if (self.if_list.index(itf) == 1 and not bridge):
+                if self.if_list.index(itf) == 1:
                     virtual_network= self.left_vn_name
-                elif (self.if_list.index(itf) == 2 and not bridge):
+                elif self.if_list.index(itf) == 2:
                     virtual_network= self.right_vn_name
                 else:
                     virtual_network= ""
