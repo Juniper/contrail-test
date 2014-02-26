7faef4036b9f843dc6a84b6847f45bf121e81d69
Kiran Desai <kirand@juniper.net>
2014-02-25 16:32:09 -0800

    Merge from bitbucket

--- fabfile/xenserver.py
+++ fabfile/xenserver.py
@@ -30,10 +30,10 @@ def add_contrail_repo():
 
 def check_xen_version():
     if not 'xen_ver' in env:
-        print "ERROR: Please set xen_ver=<6.1 or 6.2> in testbed.py"
+        print "ERROR: Please set xen_ver=<6.1 or 6.2SP1> in testbed.py"
         sys.exit(1)
-    elif env.xen_ver != '6.1' and env.xen_ver != '6.2':
-        print "ERROR: xen_ver should be 6.1 or 6.2 in testbed.py"
+    elif env.xen_ver != '6.1' and env.xen_ver != '6.2SP1':
+        print "ERROR: xen_ver should be 6.1 or 6.2SP1 in testbed.py"
         sys.exit(1)
     return
 #end check_xen_version
@@ -54,11 +54,23 @@ def reimage():
     check_xen_version()
     if env.xen_ver == '6.1':
         local('/cs-shared/cf/bin/xen.reimage %s' % host)
-    if env.xen_ver == '6.2':
+    if env.xen_ver == '6.2SP1':
         local('/cs-shared/cf/bin/xen62.reimage %s' % host)
+        
     print 'Waiting for reimage to finish...'
     wait_until_host_down()
     reconnect(1500)
+
+    if env.xen_ver == '6.2SP1':
+        print 'Applying SP1 patch...'
+        if not 'xen62sp1_repo' in env:
+            print 'Xen SP1 repo not set. Defaulting to the US repo'
+            env.xen62sp1_repo = 'http://10.84.5.100/xen62sp1'
+        run('cd /tmp && wget %s/XS62ESP1.xsupdate && wget %s/XS62ESP1-src-pkgs.tar.bz2' % (env.xen62sp1_repo, env.xen62sp1_repo))
+        run('xe patch-upload file-name=/tmp/XS62ESP1.xsupdate')
+        run('xe patch-pool-apply uuid=0850b186-4d47-11e3-a720-001b2151a503')
+        run('xe patch-list name-label=XS62ESP1')
+        reboot(360)
 
 
 @roles('compute')
