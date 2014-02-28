ae5264d4680631758a4a899b1431f3a1e9a574a0
Sandip Dey <sandipd@nodeb11.englab.juniper.net>
2014-02-28 02:14:09 -0800

    Adding retry decorator until value received

--- scripts/util.py
+++ scripts/util.py
@@ -147,3 +147,36 @@ def fab_put_file_to_vm(host_string, password, src, dest):
     output = run(cmd_str)
     real_output = _remove_unwanted_content(output)
 #end fab_put_file_to_vm
+
+def retry_for_value(tries=5, delay=3):
+    '''Retries a function or method until it returns True.
+        delay sets the initial delay in seconds. 
+    '''
+    tries=tries*1
+    tries = math.floor(tries)
+    if tries < 0:
+        raise ValueError("tries must be 0 or greater")
+
+    if delay <= 0:
+        raise ValueError("delay must be greater than 0")
+    def deco_retry(f):
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay # make mutable
+
+            result = None
+
+            while not result:
+                result = f(*args, **kwargs) # first attempt
+                if result:
+                    return result
+                else:
+                    if mtries > 0:
+                        mtries -= 1      # consume an attempt
+                        time.sleep(mdelay) # wait...
+                        result = f(*args, **kwargs) # Try again
+                    else:
+                        return result
+            return result
+        return f_retry # true decorator -> decorated function
+    return deco_retry  # @retry(arg[, ...]) -> true decorator
+#end retry_for_value
