a4a8a9c1449c1c7a7ec7d633286e9fd623b79147
Ankit Jain <ankitja@nodeb11.englab.juniper.net>
2014-03-17 02:44:39 -0700

    removed extra code

--- scripts/contrail_test_init.py
+++ scripts/contrail_test_init.py
@@ -830,8 +830,7 @@ class ContrailTestInit(fixtures.Fixture):
         return string
     
     def check_juniper_intranet(self):
-        #cmd = 'ping -c 5 www-int.juniper.net'
-        cmd = 'ping -c 5 ntp.juniper.net'
+        cmd = 'ping -c 5 www-int.juniper.net'
         try:
             subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
             self.is_juniper_intranet = True
