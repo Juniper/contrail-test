d065c456da7248dc82d410b52d6448cc52d8a5d4
Venkatesh Velpula <vvelpula@noded1.englab.juniper.net>
2014-03-25 11:27:16 +0530

    Added tempest location

--- scripts/tempest/tempest_base.py
+++ scripts/tempest/tempest_base.py
@@ -21,7 +21,6 @@ from time import sleep
 import shutil
 
 class TempestBase(testtools.TestCase, fixtures.TestWithFixtures):
-    ClassIsSetup = False 
 
     def setUp(self):
         super(TempestBase,self).setUp()
@@ -37,47 +36,18 @@ class TempestBase(testtools.TestCase, fixtures.TestWithFixtures):
         self.tempest_sample_path = '/opt/stack/tempest/etc/tempest.conf.sample'
         self.tempest_cnf_path = '/opt/stack/tempest/etc/tempest.conf'
 
-        if not self.ClassIsSetup:
-            self.__class__.ClassIsSetup = True
- 
-            with settings(host_string='%s@%s' % (self.inputs.username, self.inputs.compute_ips[0]), password=self.inputs.password,
-                          warn_only=True, abort_on_prompts= False ):
+        with settings(host_string='%s@%s' % (self.inputs.username, self.inputs.compute_ips[0]), password=self.inputs.password,
+            warn_only=True, abort_on_prompts= False ):
 
-                 shutil.copy2(self.tempest_sample_path, self.tempest_cnf_path)
-                 s= open (self.tempest_cnf_path).read()
-                 s= s.replace('neutron = false' , 'neutron = True')
-                 s= s.replace('cli_dir = /usr/local/bin' , 'cli_dir = /usr/bin')
-                 s= s.replace('admin_password = secret' , 'admin_password = contrail123')
-                 f= open (self.tempest_cnf_path, 'w')        
-                 f.write (s)
-                 f.close() 
-                 output = run('source /etc/contrail/openstackrc; keystone user-password-update  --pass secret demo')
+            shutil.copy2(self.tempest_sample_path, self.tempest_cnf_path)
+            s= open (self.tempest_cnf_path).read()
+            s= s.replace('neutron = false' , 'neutron = True')
+            s= s.replace('cli_dir = /usr/local/bin' , 'cli_dir = /usr/bin')
+            s= s.replace('admin_password = secret' , 'admin_password = contrail123')
+            f= open (self.tempest_cnf_path, 'w')        
+            f.write (s)
+            f.close() 
+            output = run('source /etc/contrail/openstackrc; keystone user-password-update  --pass secret demo')
 
-                 '''if int(self.inputs.devstack) == 0:
-                    output = run('apt-get install -y git')
-                    output = run('apt-get install -y vim')
-                    output = run('apt-get install -y gcc')
-                    output = run('apt-get install -y python-pip')
-                    output = run('apt-get install -y libxml2-dev libxslt-dev')
-
-                    with cd('/var/tmp') :
-                         output = run('rm -rf tempest')
-                         output = run('git clone https://github.com/openstack/tempest.git')
-                         output = run('pip install testrepository')
-                         output = run('pip install --upgrade fixtures')
-                         output = run('pip install --upgrade testtools')
-                         output = run('pip install nose testresources')
-                    with cd('/var/tmp/tempest'):
-                         output = run('git checkout remotes/origin/stable/havana')
-                         os.rename('/var/tmp/tempest/etc/tempest.conf.sample' , '/var/tmp/tempest/etc/tempest.conf') 
-                         s= open ("/var/tmp/tempest/etc/tempest.conf").read()
-                         s= s.replace('neutron = false' , 'neutron = True')
-                         s= s.replace('cli_dir = /usr/local/bin' , 'cli_dir = /usr/bin')
-                         s= s.replace('admin_password = secret' , 'admin_password = contrail123')
-                         f= open ("/var/tmp/tempest/etc/tempest.conf" , 'w')        
-                         f.write (s)
-                         f.close() 
-                         output = run('source /etc/contrail/openstackrc; keystone user-password-update  --pass secret demo')
-                 '''
     def cleanUp(self):
         super(TempestBase, self).cleanUp()
