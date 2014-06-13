ba4485442a05077eee7b630e6f48176c525354b2
Mahesh Loni <mloni@noded1.englab.juniper.net>
2014-06-13 11:04:10 +0530

    Added the following changes 1.Attach and detach policy from/to VN with option as contrail 2.Added the changes for creation of VN contrail 3.VM will launch with combination of customized SG and default SG

--- fixtures/vn_policy_test.py
+++ fixtures/vn_policy_test.py
@@ -1,5 +1,5 @@
 import fixtures
-from vnc_api.vnc_api import *
+from vnc_api import vnc_api
 import inspect
 from quantum_test import *
 
@@ -11,7 +11,8 @@ class VN_Policy_Fixture(fixtures.Fixture):
           Fixture will take care of cleanup in reverse order.
           Ex. createVN, createPolicy, attachVNPolicy, test, cleanup [detachVNPolicy, delete Policy, deleteVN]
     """
-    def __init__(self, connections, vn_name, vn_obj,topo, project_name, policy_obj=[]):
+    def __init__(self, connections, vn_name, vn_obj,topo, project_name,options='openstack', policy_obj=[]):
+
         self.connections= connections
         self.inputs= self.connections.inputs
         self.quantum_fixture= self.connections.quantum_fixture
@@ -25,6 +26,7 @@ class VN_Policy_Fixture(fixtures.Fixture):
         self.skip_verify='no'
         self.vn=vn_name
         self.already_present= False
+        self.option=options
         
     #end __init__
 
@@ -39,9 +41,17 @@ class VN_Policy_Fixture(fixtures.Fixture):
         else:
            if self.policy_obj[self.vn] :
               self.logger.info ("Setup step: Associating the policy to VN'")
-              policy_fq_names= [ self.quantum_fixture.get_policy_fq_name( x ) for x in self.policy_obj[self.vn]]
-              self.vn_obj[self.vn].bind_policies( policy_fq_names, self.vn_obj[self.vn].vn_id)
-	      self.logger.info ('Associated Policy:%s to %s'%(policy_fq_names,self.vn))
+              if self.option == 'openstack' :
+                 policy_fq_names= [ self.quantum_fixture.get_policy_fq_name( x ) for x in self.policy_obj[self.vn]]
+                 self.vn_obj[self.vn].bind_policies( policy_fq_names, self.vn_obj[self.vn].vn_id)
+	         self.logger.info ('Associated Policy:%s to %s'%(policy_fq_names,self.vn))
+              elif self.option == 'contrail' :
+                   ref_tuple=[]
+                   vn_update_rsp = None
+                   for conf_policy in self.policy_obj[self.vn]:
+                       self.vn_obj[self.vn].add_network_policy(conf_policy,vnc_api.VirtualNetworkPolicyType(sequence=vnc_api.SequenceType(major=0, minor=0)))
+                   vn_update_rsp = self.vnc_lib.virtual_network_update(self.vn_obj[self.vn]._obj) 
+	           self.logger.info ('Associated Policy to %s'%(self.vn))
         return self
     #end attachPolicytoVN
 
@@ -66,8 +76,16 @@ class VN_Policy_Fixture(fixtures.Fixture):
         if self.policy_obj[self.vn] :
            policy_of_vn=self.api_s_inspect.get_cs_vn_policys(project=self.project_name,vn=self.vn,refresh=True)
            if policy_of_vn :
-              for policy in policy_of_vn :
-                  policy_fq_names.append(self.api_s_inspect.get_cs_policy(project=self.project_name,policy=policy)['network-policy']['fq_name'])
-              self.vn_obj[self.vn].unbind_policies(self.vn_obj[self.vn].vn_id,policy_fq_names)
-              self.logger.info ('Detached Policy:%s from %s'%(policy_fq_names,self.vn))
+              if self.option == 'openstack' :
+                 for policy in policy_of_vn :
+                     policy_fq_names.append(self.api_s_inspect.get_cs_policy(project=self.project_name,policy=policy)['network-policy']['fq_name'])
+                 self.vn_obj[self.vn].unbind_policies(self.vn_obj[self.vn].vn_id,policy_fq_names)
+                 self.logger.info ('Detached Policy:%s from %s'%(policy_fq_names,self.vn))
+              elif self.option == 'contrail' :
+                   vn_update_rsp = None
+                   for conf_policy in self.policy_obj[self.vn]:
+                       self.vn_obj[self.vn]._obj.del_network_policy(conf_policy)
+                   vn_update_rsp = self.vnc_lib.virtual_network_update(self.vn_obj[self.vn]._obj)
+                   self.logger.info ('Detached Policy from %s'%(self.vn))
+               
     #end of detach_policy_VN
