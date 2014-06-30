2b462fe6f23a37d513a5a619899462d8ef49dc85
Shweta Naik <stnaik@juniper.net>
2014-06-30 12:28:12 -0700

    Performance testcases.

--- scripts/performance/sanity.py
+++ scripts/performance/sanity.py
@@ -30,9 +30,34 @@ class PerformanceSanity(testtools.TestCase, PerformanceTest):
         super(PerformanceSanity, self).cleanUp()
 
     @preposttest_wrapper
-    def test_performance_netperf_within_vn(self):
-        """Check the throughput between the VM's within the same VN"""
-        return self.test_check_netperf_within_vn()
+    def test_performance_netperf_within_vn_TCP_STREAM(self):
+        """Check the throughput between the VM's within the same VN for TCP_STREAM"""
+        return self.test_check_netperf_within_vn(duration=60)
+
+    @preposttest_wrapper
+    def test_performance_netperf_within_vn_TCP_STREAM_with_MPLSoGRE(self):
+        """Check the throughput between the VM's within the same VN for TCP_STREAM using MPLSoGRE"""
+        return self.test_check_netperf_within_vn(encap='MPLSoGRE', duration=60)
+
+    @preposttest_wrapper
+    def test_performance_netperf_within_vn_TCP_RR(self):
+        """TCP Request/Response test between the VM's within the same VN"""
+        return self.test_check_netperf_within_vn(test_name='TCP_RR')
+
+    @preposttest_wrapper
+    def test_performance_netperf_within_vn_with_UDP_STREAM(self):
+        """Check the throughput between the VM's within the same VN for UDP_STREAM"""
+        return self.test_check_netperf_within_vn(test_name='UDP_STREAM', duration=60)
+
+    @preposttest_wrapper
+    def test_performance_netperf_within_vn_UDP_STREAM_with_MPLSoGRE(self):
+        """Check the throughput between the VM's within the same VN for UDP_STREAM using MPLSoGRE"""
+        return self.test_check_netperf_within_vn(encap='MPLSoGRE', duration=60)
+
+    @preposttest_wrapper
+    def test_performance_netperf_within_vn_UDP_RR(self):
+        """UDP Request/Response test between the VM's within the same VN"""
+        return self.test_check_netperf_within_vn(test_name='UDP_RR')
 
     @preposttest_wrapper
     def test_performance_netperf_in_diff_vn(self):
@@ -41,8 +66,24 @@ class PerformanceSanity(testtools.TestCase, PerformanceTest):
 
     @preposttest_wrapper
     def test_performance_ping_latency_within_vn(self):
-        """Check the throughput between the VM's within the same VN"""
+        """Check the ping latency between the VM's within the same VN"""
         return self.test_ping_latency()
 
+    @preposttest_wrapper
+    def test_performance_ping_latency_within_vn_icmp_flood(self):
+        """Check the ping latency between the VM's within the same VN"""
+        return self.test_ping_latency(no_of_pkt=20)
+
+    @preposttest_wrapper
+    def test_flow_setup_within_vn_1000_flows(self):
+        """Check the flow setup rate between the VM's within the same VN"""
+        return self.test_check_flow_setup_within_vn(no_of_flows=1000, dst_port_min=1000, dst_port_max=2001,
+                                                    src_port_min=10000, src_port_max=10000)
+
+    @preposttest_wrapper
+    def test_flow_setup_within_vn_20000_flows(self):
+        """Check the flow setup rate between the VM's within the same VN"""
+        return self.test_check_flow_setup_within_vn(no_of_flows=20000, dst_port_min=1000, dst_port_max=21000,
+                                                    src_port_min=10000, src_port_max=10000)
 if __name__ == '__main__':
     unittest.main()
