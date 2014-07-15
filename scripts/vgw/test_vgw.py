714dbf2129bdb30ac751ff90cd1672804538b7b0
Kumar Harsh <hkumar@juniper.net>
2014-07-15 18:36:16 +0530

    pep8 changes

--- scripts/vgw/test_vgw.py
+++ scripts/vgw/test_vgw.py
@@ -8,55 +8,56 @@
 from tcutils.wrappers import preposttest_wrapper
 from vgw import base
 from vgw.verify import VerifyVgwCases
+
+
 class TestVgwCases(base.BaseVgwTest, VerifyVgwCases):
 
     @classmethod
     def setUpClass(cls):
         super(TestVgwCases, cls).setUpClass()
-        
+
     def runTest(self):
         pass
-    #end runTest
+    # end runTest
 
     @preposttest_wrapper
     def test_vgw_with_fip_on_same_node(self):
         '''Test VM is launched on the same compute node where VGW is configured and VM got FIP from VGW network
         '''
-       
+
         return self.verify_vgw_with_fip(compute_type='same')
 
     @preposttest_wrapper
     def test_vgw_with_native_vm_on_same_node(self):
         '''Test VM is launched on the same compute node where VGW is configured and VM is launched on VGW network
         '''
-      
+
         return self.verify_vgw_with_native_vm(compute_type='same')
 
     @preposttest_wrapper
     def test_vgw_with_native_vm_on_different_node(self):
         '''Test VM is launched on the same compute node where VGW is configured and VM is laucnhed on VGW network
         '''
-       
+
         return self.verify_vgw_with_native_vm(compute_type='different')
 
     @preposttest_wrapper
     def test_vgw_with_fip_on_different_node(self):
         '''Test VM is launched on the different compute node where VGW is configured and VM got FIP from VGW network
         '''
-    
-        return self.verify_vgw_with_fip(compute_type='different')
 
+        return self.verify_vgw_with_fip(compute_type='different')
 
     @preposttest_wrapper
     def test_vgw_with_multiple_subnet_for_single_vgw(self):
         '''Test VGW having multiple subnet is working properly
         '''
-      
+
         return self.verify_vgw_with_multiple_subnet()
 
     @preposttest_wrapper
     def test_vgw_with_restart_of_vgw_node(self):
         '''Test VGW with restarting the VGW node
         '''
-        
+
         return self.vgw_restart_of_vgw_node()
