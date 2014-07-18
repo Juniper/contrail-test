f28ee7d150151359f76b5c297797c97fa4e8e308
Saravanan Musuvathi <smusuvathi@juniper.net>
2014-07-18 05:24:04 -0700

    fix path for traffic scripts

--- fixtures/traffic_tests.py
+++ fixtures/traffic_tests.py
@@ -4,11 +4,11 @@ import fixtures
 #from contrail_test_init import *
 from nova_test import *
 from connections import ContrailConnections
-from tcutils.pkgs.Traffic.traffic.core.stream import Stream
-from tcutils.pkgs.Traffic.traffic.core.profile import create, ContinuousProfile, ContinuousSportRange
-from tcutils.pkgs.Traffic.traffic.core.helpers import Host
-from tcutils.pkgs.Traffic.traffic.core.helpers import Sender, Receiver
-
+sys.path.append(os.path.realpath('scripts/tcutils/pkgs/Traffic'))
+from traffic.core.stream import Stream
+from traffic.core.profile import create, ContinuousProfile, ContinuousSportRange
+from traffic.core.helpers import Host
+from traffic.core.helpers import Sender, Receiver
 
 class trafficTestFixture(fixtures.Fixture):
 
