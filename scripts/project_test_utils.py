5dbee37a17d2ab089e8c04cf21a8ec89c44c6226
Vedu Joshi <vjoshi@juniper.net>
2014-08-21 01:04:36 +0530

    Support auth urls which could be set separately
    In Load-balanced auth urls, it would not just be openstack ip.
    So, the user can export their openrc/openstackrc before running the test
    
    Change-Id: Ic7e30187626e3b8fedada431c2cef9e5f365ebfb

--- scripts/project_test_utils.py
+++ scripts/project_test_utils.py
@@ -23,6 +23,8 @@ def _create_project_return_connection(self, projectname):
         self.logger.info('Project admin already exist, no need to create')
         return [self.inputs, self.connections]
     auth_url = 'http://%s:5000/v2.0' % (self.inputs.cfgm_ip)
+    auth_url = os.getenv('OS_AUTH_URL') or \
+                   'http://' + self.inputs.openstack_ip + ':5000/v2.0'
     kc = ksclient.Client(
         username=self.inputs.stack_user, password=self.inputs.stack_password,
         tenant_name=self.inputs.project_name, auth_url=auth_url)
