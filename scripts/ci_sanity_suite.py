67640deacc9282b68075d5e2af2682b52d5ed11a
Kamlesh Parmar <kparmar@juniper.net>
2014-10-27 16:42:00 -0700

    fix link local services test for ci environment
    Change-Id: Icc4ded7387a9d7d81adaef4787f939229e524e1d

--- scripts/ci_sanity_suite.py
+++ scripts/ci_sanity_suite.py
@@ -81,8 +81,7 @@ if __name__ == "__main__":
     suite.addTest(
         TestSanityFixture('test_process_restart_with_multiple_vn_vm'))
     suite.addTest(TestSanityFixture('test_metadata_service'))
-    #disable test below, not passing in ci environment
-    #suite.addTest(TestSanityFixture('test_generic_link_local_service'))
+    suite.addTest(TestSanityFixture('test_generic_link_local_service'))
     #suite.addTest(TestVMVN('test_dns_resolution_for_link_local_service'))
     #suite.addTest(SvcMirrorSanityFixture('test_svc_mirroring'))
     suite.addTest(TestSanity('test_verify_generator_collector_connections'))
