7845e57649f930571d52064a6d73ecc64d144301
Kumar Harsh <hkumar@juniper.net>
2014-11-12 15:55:14 +0530

    vdns testcase documentation Change-Id: I4d99d8a8896dee575a525d745001244fc18eb90e

--- serial_scripts/vdns/test_vdns.py
+++ serial_scripts/vdns/test_vdns.py
@@ -42,28 +42,71 @@ class TestvDNSRestart(BasevDNSRestartTest):
  
     @preposttest_wrapper
     def test_vdns_controlnode_switchover(self):
-        ''' This test test control node switchover functionality'''
+        ''' This test tests control node switchover functionality
+            1. Create VDNS server object
+            2. Associate VDNS with IPAM
+            3. Launch VN with IPAM
+            4. Launch VM with VN Created above. This test verifies on launch of VM agent should update DNS 'A' and 'PTR' records
+            5. Ping VMs using VM name
+            6. Restart active control node
+            7. Ping VMs using VM name
+        Pass criteria: Step 4,5 and 7 should pass
+        Maintainer: cf-test@juniper.net
+        '''
+
         restart_process = 'ControlNodeRestart'
         self.vdns_with_cn_dns_agent_restart(restart_process)
         return True
 
     @preposttest_wrapper
     def test_vdns_dns_restart(self):
-        ''' This test test dns process restart functionality'''
+        ''' This test test dns process restart functionality
+            1. Create VDNS server object
+            2. Associate VDNS with IPAM
+            3. Launch VN with IPAM
+            4. Launch  VM with VN Created above. This test verifies on launch of VM agent should update DNS 'A' and 'PTR' records
+            5. Ping VMs using VM name
+            6. Restart the dns process in the active control node
+            7. Ping VMs using VM name
+        Pass criteria: Step 4, 5 and 7 should pass
+        Maintainer: cf-test@juniper.net
+        '''
         restart_process = 'DnsRestart'
         self.vdns_with_cn_dns_agent_restart(restart_process)
         return True
 
     @preposttest_wrapper
     def test_vdns_agent_restart(self):
-        '''This test tests agent process restart functionality'''
+        '''This test tests agent process restart functionality
+            1. Create VDNS server object
+            2. Associate VDNS with IPAM
+            3. Launch VN with IPAM
+            4. Launch VM with VN Created above. This test verifies on launch of VM agent should update DNS 'A' and 'PTR' records
+            5. Ping VMs using VM name
+            6. Restart the agent process in the compute node
+            7. Ping VMs using VM name
+        Pass criteria: Step 4, 5 and 7 should pass
+        Maintainer: cf-test@juniper.net
+        '''
+
         restart_process = 'AgentRestart'
         self.vdns_with_cn_dns_agent_restart(restart_process)
         return True
 
     @preposttest_wrapper
     def test_vdns_named_restart(self):
-        '''This test tests named process restart functionality'''
+        '''This test tests named process restart functionality
+            1. Create VDNS server object
+            2. Associate VDNS with IPAM
+            3. Launch VN with IPAM
+            4. Launch VM with VN Created above. This test verifies on launch of VM agent should update DNS 'A' and 'PTR' records
+            5. Ping VMs using VM name
+            6. Restart the named process in the active control node
+            7. Ping VMs using VM name
+        Pass criteria: Step 4, 5 and 7 should pass
+        Maintainer: cf-test@juniper.net
+        '''
+
         restart_process = 'NamedRestart'
         self.vdns_with_cn_dns_agent_restart(restart_process)
         return True
