befc3311b71cf1af7db9817821ecff35b62f6217
Kumar Harsh <hkumar@juniper.net>
2014-12-04 15:12:19 +0530

    Use isolated_creds for project creation in quota test Change-Id: I78818918b1f062a955be919bf1cef91be42182a8

--- scripts/neutron/test_quota.py
+++ scripts/neutron/test_quota.py
@@ -7,6 +7,7 @@ from user_test import UserFixture
 from project_test import ProjectFixture
 from common.neutron.base import BaseNeutronTest
 import test
+from common.isolated_creds import IsolatedCreds
 from tcutils.util import *
 
 
@@ -104,42 +105,37 @@ class TestQuota(BaseNeutronTest):
             'security_group_rule': 6}
 
         project_name = get_random_name('project1')
-        user_fixture = self.useFixture(UserFixture(
-            connections=self.connections, username='test_usr',
-            password='testusr123'))
-        project_fixture_obj = self.useFixture(ProjectFixture(
-            username=self.inputs.stack_user,
-            password=self.inputs.stack_password,
-            project_name=project_name,
-            vnc_lib_h=self.vnc_lib,
-            connections=self.connections))
-        user_fixture.add_user_to_tenant(project_name, 'test_usr', 'Member')
-        assert project_fixture_obj.verify_on_setup()
-        proj_connection = project_fixture_obj.get_project_connections(
-            'test_usr',
-            'testusr123')
+        isolated_creds = IsolatedCreds(
+            project_name,
+            self.admin_inputs,
+            ini_file=self.ini_file,
+            logger=self.logger)
+        isolated_creds.setUp()
+        project_obj = isolated_creds.create_tenant()
+        isolated_creds.create_and_attach_user_to_tenant()
+        proj_inputs = isolated_creds.get_inputs()
+        proj_connection = isolated_creds.get_conections()
 
         project_name1 = get_random_name('project2')
-        user_fixture1 = self.useFixture(UserFixture(
-            connections=self.connections, username='test_usr1',
-            password='testusr1231'))
-        project_fixture_obj1 = self.useFixture(ProjectFixture(
-            username='test1',
-            password='test1231',
-            project_name=project_name1,
-            vnc_lib_h=self.vnc_lib,
-            connections=self.connections))
-        user_fixture1.add_user_to_tenant(project_name1, 'test_usr1', 'Member')
-        assert project_fixture_obj1.verify_on_setup()
+        isolated_creds1 = IsolatedCreds(
+            project_name1,
+            self.admin_inputs,
+            ini_file=self.ini_file,
+            logger=self.logger)
+        isolated_creds1.setUp()
+        project_obj1 = isolated_creds1.create_tenant()
+        isolated_creds1.create_and_attach_user_to_tenant()
+        proj_inputs1 = isolated_creds1.get_inputs()
+        proj_connection1 = isolated_creds1.get_conections()
 
         self.logger.info(
             "Update quota for tenant %s to: \n %s by admin tenat " %
-            (project_fixture_obj1.inputs.project_name, quota_dict))
+            (proj_inputs1.project_name, quota_dict))
         quota_rsp = self.admin_connections.quantum_fixture.update_quota(
-            project_fixture_obj1.project_id,
+            project_obj1.project_id,
             quota_dict)
         quota_show_dict = self.admin_connections.quantum_fixture.show_quota(
-            project_fixture_obj1.project_id)
+            project_obj1.project_id)
 
         for neutron_obj in quota_rsp['quota']:
             if quota_rsp['quota'][neutron_obj] != quota_show_dict[
@@ -151,20 +147,20 @@ class TestQuota(BaseNeutronTest):
         assert result, 'Quota update by admin tenant failed'
         self.logger.info(
             "Quota for tenant %s updated to : \n %s" %
-            (project_fixture_obj1.inputs.project_name, quota_show_dict))
+            (proj_inputs1.project_name, quota_show_dict))
         self.logger.info(
             "Try to update quota for tenant %s to : \n %s by tenant %s" %
-            (project_fixture_obj1.inputs.project_name,
+            (proj_inputs1.project_name,
              quota_dict,
-             project_fixture_obj.inputs.project_name))
+             proj_inputs.project_name))
         result1 = proj_connection.quantum_fixture.update_quota(
-            project_fixture_obj1.project_id,
+            project_obj1.project_id,
             quota_dict)
         assert not result1, 'Quota update of %s by %s successful not expected' % (
             project_name1, project_name)
         self.logger.info(
             "Quota for tenant %s still set to : \n %s as expected " %
-            (project_fixture_obj1.inputs.project_name, quota_show_dict))
+            (proj_inputs1.project_name, quota_show_dict))
 
     @preposttest_wrapper
     def test_quota_update_of_specific_tenant(self):
