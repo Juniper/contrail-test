8918e603a96c3d25c7f51cac6b19bb8e12d2d44a
Sandip Dey <sandipd@juniper.net>
2015-06-26 03:19:58 -0700

    Adding Ceilometer test infra code
    1.Add infra code to test ceilometer
    2.Add one ceilometer test case
    
    Took care of the review comments
    Enabled objects for verifying floating ip meters
    
    Closes-Bug: #1469108
    
    Change-Id: If13f5fb2daa4e738581ba7f7fa6c4e18e26d4222

---
+++ scripts/ceilometer_tests/test_ceilometer.py
@@ -0,0 +1,57 @@
+import os
+import time
+from tcutils.wrappers import preposttest_wrapper
+from ceilometer_tests import base
+from ceilometer import ceilometer_client
+from openstack import OpenstackAuth
+
+import test
+
+
+class CeilometerTest(
+        base.CeilometerBaseTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(CeilometerTest, cls).setUpClass()
+        cls.res.setUp(cls.inputs, cls.connections, cls.public_vn_obj)
+        cls.auth = OpenstackAuth(cls.inputs.stack_user,
+                              cls.inputs.stack_password,
+                              cls.inputs.project_name, cls.inputs, cls.logger)
+        cls.auth_url = cls.auth.auth_url
+        cls.c_url = 'http://%s:8777/'%cls.inputs.openstack_ip
+        cls.cclient = ceilometer_client.CeilometerClient(cls.auth_url, 
+                                 cls.inputs.stack_user,
+                                 cls.inputs.stack_password,
+                                 'admin',
+                                 cls.c_url,
+                                 insecure = True)
+        cls.cclient = cls.cclient.get_cclient() 
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.res.cleanUp()
+        super(CeilometerTest, cls).tearDownClass()
+    # end tearDownClass
+
+    def runTest(self):
+        pass
+    # end runTest
+
+    @preposttest_wrapper
+    def test_meters(self):
+        """Verifying ceilometer meters"""
+        tenant_id = self.auth.get_project_id('default_domain',
+                                              'admin')
+        tenant_id = "".join(tenant_id.split('-')) 
+        q = ceilometer_client.make_query(tenant_id = tenant_id)
+        result = None
+        result =  ceilometer_client.resource_list(self.cclient,query=q) 
+        if not result:
+            self.logger.error("Ceilometer resource list did not work...")
+            assert False
+        if result:
+            self.logger.info("Ceilometer resource list did  work...")
+            assert True
+        return True
+            
