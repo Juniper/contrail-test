a5689fbc668e35153e0e41454b9e8185e251d361
Sandip Dey <sandipd@juniper.net>
2015-10-11 21:50:28 -0700

    Code change to enable evpn tests in vcenter sanity
    Removed pdb
    Missed a commit
    
    Change-Id: I92ff7c9091bc4755500de05260ce57b0845f3b9e

--- fixtures/vcenter.py
+++ fixtures/vcenter.py
@@ -741,20 +741,21 @@ class VcenterVM:
 
 class VcenterAuth(OrchestratorAuth):
 
-   def __init__(self, user, passwd, project_name, inputs, domain='default-domain'):
-       self.inputs = inputs
-       self.user = user
-       self.passwd = passwd
-       self.domain = domain
-       self.vnc = VncApi(username=user, password=passwd,
-                         tenant_name=project_name,
-                         api_server_host=self.inputs.cfgm_ip,
-                         api_server_port=self.inputs.api_server_port)
+    def __init__(self, user, passwd, project_name, inputs, domain='default-domain'):
+        self.inputs = inputs
+        self.user = user
+        self.passwd = passwd
+        self.domain = domain
+        self.project_name = project_name
+        self.vnc = VncApi(username=user, password=passwd,
+                          tenant_name=project_name,
+                          api_server_host=self.inputs.cfgm_ip,
+                          api_server_port=self.inputs.api_server_port)
 
-   def get_project_id(self, name=None):
-       if not name:
-           name = self.project_name
-       fq_name = [unicode(self.domain), unicode(name)]
+    def get_project_id(self, project_name=None):
+       if not project_name:
+           project_name = self.project_name
+       fq_name = [unicode(self.domain), unicode(project_name)]
        obj = self.vnc.project_read(fq_name=fq_name)
        if obj:
            return obj.get_uuid()
