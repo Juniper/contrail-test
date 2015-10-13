c7314cd038388091d9c59fdd09df7cb6c7f11b30
Vedu Joshi <vjoshi@juniper.net>
2015-10-13 10:46:18 +0530

    Create any router objects before running tests
    It can so happen that a previous instance of DM
    can leave stale config on the router causing none of the tests to
    start(fails at preposttest_wrapper itself)
    
    So, create router objects before running tests so that the current DM
    pushes the right config
    
    Change-Id: Id821a1d7bbb4d10528d92400aa8aff557cb5789e

--- tools/tor/setup_tors.py
+++ tools/tor/setup_tors.py
@@ -5,6 +5,7 @@ import ConfigParser
 import ast
 
 from tor_fixture import ToRFixtureFactory
+from physical_router_fixture import PhysicalRouterFixture
 from common.contrail_test_init import ContrailTestInit
 
 if __name__ == "__main__":
@@ -25,6 +26,19 @@ if __name__ == "__main__":
                 controller_ip=device_dict['controller_ip'],
                 bringup=True)
             tor_obj.setUp()
+        if device_dict['type'] == 'router':
+            phy_router_obj = PhysicalRouterFixture(
+                device_dict['name'], device_dict['mgmt_ip'],
+                model=device_dict['model'],
+                vendor=device_dict['vendor'],
+                asn=device_dict['asn'],
+                ssh_username=device_dict['ssh_username'],
+                ssh_password=device_dict['ssh_password'],
+                mgmt_ip=device_dict['mgmt_ip'],
+                tunnel_ip=device_dict['tunnel_ip'],
+                ports=device_dict['ports'],
+                )
+            phy_router_obj.setUp()
     # end for
 
 
