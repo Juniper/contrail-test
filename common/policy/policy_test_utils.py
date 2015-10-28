Juniper/contrail-test

commit 5bc05e7835bfe2219fd39bb6b7399420f2415f04

Convert true/false strings to boolean before rule comparison
Neutronclient returns True/False boolean objects for the
new keys log/alert (review https://review.opencontrail.org/#/c/14363)

This change makes sure that we do a boolean comparison for log and
alert keys

Change-Id: I3b3fb98ac149f0cefc71274fcd8116cef0906bb7

common/policy/policy_test_utils.py
@@ -298,6 +298,9 @@ def xlate_cn_rules(rules_list):
         new_rule['rule_sequence']['minor'] = int(
             new_rule['rule_sequence']['minor'])
         new_rule['rule_sequence'] = None
+        if 'log' in new_rule.keys():
+            new_rule['log'] = bool(new_rule['log'])
+            new_rule['alert'] = bool(new_rule['alert'])
         # appending each rule to new list
         new_rule_list.append(new_rule)
     print "after xlate: ", new_rule_list

commit 6d2b5af538a9bd445656daac4e9e01174c259010

Fix bug introduced in commit 5bc05e78
In commit 5bc05e78, the boolean conversion was done incorrectly.
Fixing it now

Change-Id: Ie52e8335941be8c7511f90245a5e6868c8f1e7be

common/policy/policy_test_utils.py
@@ -298,9 +298,9 @@ def xlate_cn_rules(rules_list):
         new_rule['rule_sequence']['minor'] = int(
             new_rule['rule_sequence']['minor'])
         new_rule['rule_sequence'] = None
-        if 'log' in new_rule.keys():
-            new_rule['log'] = bool(new_rule['log'])
-            new_rule['alert'] = bool(new_rule['alert'])
+        if 'log' in new_rule['action_list'].keys():
+            new_rule['action_list']['log'] = json.loads(new_rule['action_list']['log'])
+            new_rule['action_list']['alert'] = json.loads(new_rule['action_list']['alert'])
         # appending each rule to new list
         new_rule_list.append(new_rule)
     print "after xlate: ", new_rule_list