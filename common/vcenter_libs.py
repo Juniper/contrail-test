fe8979db8f444fbb00c58ee779e2647855573559
Sandip Dey <sandipd@juniper.net>
2015-12-10 10:04:37 -0800

    Merge vcenter as compute changes from R2.20
    Change-Id: I7d10a7da4ec26d09d98662bb818f72acf6aa9d41

--- common/vcenter_libs.py
+++ common/vcenter_libs.py
@@ -1,4 +1,6 @@
-
+import time
+import atexit
+import requests
 try:
     from pyVim import  connect
     from pyVmomi import vim
@@ -39,3 +41,79 @@ except:
     _vimtype_dict = {}
     connect = None
     vim = None
+    _vimtype_dict = None
+
+def _vim_obj(typestr, **kwargs):
+    return _vimtype_dict[typestr](**kwargs)
+
+def _wait_for_task (task):
+    while (task.info.state == vim.TaskInfo.State.running or
+           task.info.state == vim.TaskInfo.State.queued):
+        time.sleep(2)
+    if task.info.state != vim.TaskInfo.State.success:
+        if task.info.state == vim.TaskInfo.State.error:
+            raise ValueError(task.info.error.localizedMessage)
+        raise ValueError("wait_for_task failed:%s" % task.info)
+    return
+
+def _match_obj(obj, param):
+    attr = param.keys()[0]
+    attrs = [attr]
+    if '.' in attr:
+        attrs = attr.split('.')
+        for i in range(len(attrs) - 1):
+            if not hasattr(obj, attrs[i]):
+                break
+            obj = getattr(obj, attrs[i])
+    attr = attrs[-1]
+    return hasattr(obj, attr) and getattr(obj, attr) == param.values()[0]
+
+def get_vcenter_connection(inputs):
+    SI = None
+    try:
+        SI = connect.SmartConnect(host=inputs.vcenter_server,
+                              user=inputs.vcenter_username,
+                              pwd=inputs.vcenter_password,
+                              port=int(inputs.vcenter_port))
+        atexit.register(connect.Disconnect, SI)
+        content = SI.RetrieveContent()
+        return SI
+    except IOError, ex:
+        pass
+
+def get_vm_info_by_uuid(inputs,uuid):
+
+    try:
+        SI=get_vcenter_connection(inputs)
+        VM = SI.content.searchIndex.FindByUuid(None, uuid,
+                                           True,
+                                           True)
+        return VM
+    except IOError,ex:
+        pass
+
+def get_esxi_host_of_vm_by_uuid(inputs,uuid):
+    VM = get_vm_info_by_uuid(inputs,uuid)
+    return VM.runtime.host.name
+
+def get_contrail_vm_by_vm_uuid(inputs,uuid):
+    esxi_host = get_esxi_host_of_vm_by_uuid(inputs,uuid)
+    for esxi in inputs.esxi_vm_ips:
+        if esxi_host == esxi['ip']:
+            contrail_vm = esxi['contrail_vm']
+            ip = contrail_vm.split('@')[1]
+            return inputs.host_data[ip]['name']
+
+class Inputs:
+    def __init__(self):
+        self.vcenter_server='10.204.217.189'
+        self.vcenter_username='administrator@vsphere.local'
+        self.vcenter_password='Contrail123!'
+        self.vcenter_port='443'
+
+def main():
+    inputs=Inputs()
+    print get_contrail_vm_by_vm_uuid(inputs,'9175dc3b-5ff5-45ca-a836-05dc986ef19d')
+
+if __name__ == "__main__":
+    main()
