76925df43b33cc3cd0261ef2a643a89fbd9fd1dd
Vedu Joshi <vjoshi@juniper.net>
2015-12-14 11:08:33 +0530

    Call VM get() with a timeout in intial verifications
    It has been seen in CI environments , that in some error
    conditions, novaclient VM get() takes a huge time(upto 30min)
    causing the test jobs to timeout(One would see logs similar to below)
    This change tries to make the tests fail fast
    
    out: 2015-11-23 17:00:10,210 - INFO - VM <Server: vm_test> is in ACTIVE state now
    out: 2015-11-23 17:29:41,711 - ERROR - VM does not seem to have got an IP in VN vn22
    out: 2015-11-23 17:31:41,908 - ERROR - VM does not seem to have got an IP in VN vn22
    out: 2015-11-23 17:33:42,058 - ERROR - VM does not seem to have got an IP in VN vn22
    
    Change-Id: I2053a971f31c5c63ab3fd92779f26a3999bde107

---
+++ tcutils/timeout.py
@@ -0,0 +1,21 @@
+# From http://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish
+#
+# Usage : 
+# with timeout(seconds=3):
+#    sleep(4)
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
