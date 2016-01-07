664fb865dcd546ab55266d4eaa5471b725bd05de
Ankit Jain <ankitja@juniper.net>
2016-01-07 03:29:14 -0800

    Fix discovery regression failures
       For centos, consider node_name.englab.juniper.net:client_type as valid client id format in DS
    
    Change-Id: I8e65734653027f90819800438733144a31f46e92

--- tcutils/config/discovery_tests.py
+++ tcutils/config/discovery_tests.py
@@ -492,19 +492,29 @@ class DiscoveryVerification(fixtures.Fixture):
     @retry_for_value(delay=5, tries=5)
     def get_subscribed_service_id(self, ds_ip, client=(), service=None , 
 				instance = ''):
-        '''Returns service id subscribed by a client'''
-
+        '''Returns service id subscribed by a client''' 
         client_ip = client[0]
         client_svc = client[1]
         service_id = []
         host_name = self.get_hostname_from_hostdata_by_ip(client_ip)
 #        host_name = socket.gethostbyaddr(client_ip)[0]
         try:
-            host = host_name.split('.')[0]
-            client_id = '%s:%s:%s' % (host, client_svc,instance)
             obj = self.ds_inspect[ds_ip].get_ds_clients()
+            d_name = socket.gethostname().split('.')
+            d_name = '.'.join(d_name[1:])
+            host = host_name.split('.')[0]
+            host_with_dname = host + '.' + d_name
+            client_id = '%s:%s:%s' % (host, client_svc,instance)
             dct = obj.get_attr('Clients', match=('client_id', client_id))
-
+            if not dct:
+                client_id = '%s:%s:%s' % (host_with_dname, client_svc,instance)
+                dct = obj.get_attr('Clients', match=('client_id', client_id))
+            if not dct:
+               client_id = '%s:%s' % (host, client_svc)
+               dct = obj.get_attr('Clients', match=('client_id', client_id))
+            if not dct:
+                client_id = '%s:%s' % (host_with_dname, client_svc)
+                dct = obj.get_attr('Clients', match=('client_id', client_id))
             if not dct:
                 host_name = socket.gethostbyaddr(client_ip)[0]
                 # nodea18.englab.juniper.net:contrail-api
