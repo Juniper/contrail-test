55019b51b133ed73bbc383303def161c27736c68
Ritam Gangopadhyay <ritam@juniper.net>
2016-01-13 15:31:26 +0530

    Removing duplication of flow related tests.
    There were old files of flow related tests which
    were duplicate of tests under serial_scripts/system_test/flow_tests.
    So, entire serial_scripts/flow_tests is being removed and one of the
    recent test case comitted to this directory "test_flow_scenarios.py"
    is being moved under serial_scripts/system_test/flow_tests.
    
    Change-Id: Idc159036cac1b5f7f4a7ddf19fc9af2680840a67

--- serial_scripts/flow_tests/ReleaseToFlowSetupRateMapping.py
+++
@@ -1,7 +0,0 @@
-# Here the rate is set for Policy flows, local to a compute, which is
-# lesser than policy flows across computes
-expected_flow_setup_rate = {}
-expected_flow_setup_rate['policy'] = {
-    '1.04': 6000, '1.05': 9000, '1.06': 10000, '1.10': 10000}
-expected_flow_setup_rate['nat'] = {'1.04': 4200,
-                                   '1.05': 6300, '1.06': 7500, '1.10': 7500}
