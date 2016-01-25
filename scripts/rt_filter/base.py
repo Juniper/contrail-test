c0ce257024f6e3790ef981911ddea3465b33ee2b
Vedu Joshi <vjoshi@juniper.net>
2016-01-25 16:17:56 +0530

    Add support to run tests in a user-specified single tenant
    Currently each test suite creates its own projects/users
    In production test environments, the admin would allocate
    a specific project where the tests are expected to run
    (stackUser, stackPassword and stackTenant in sanity_params.ini)
    
    If tenant_isolation is enabled and admin credentials are
    mentioned in sanity_params.ini, the existing behavior will
    be seen
    
    Corresponding fab code changes : https://review.opencontrail.org/16483
    
    Change-Id: I13e5d0983472b1a8f9d2ecdd6f79e5df043ee04b

--- scripts/rt_filter/base.py
+++ scripts/rt_filter/base.py
@@ -1,4 +1,4 @@
-import test
+import test_v1
 from common.connections import ContrailConnections
 from common import isolated_creds
 from vm_test import VMFixture
@@ -6,24 +6,14 @@ from vn_test import VNFixture
 from tcutils.util import retry
 
 
-class BaseRtFilterTest(test.BaseTestCase):
+class BaseRtFilterTest(test_v1.BaseTestCase_v1):
 
     @classmethod
     def setUpClass(cls):
         super(BaseRtFilterTest, cls).setUpClass()
-        cls.isolated_creds = isolated_creds.IsolatedCreds(cls.__name__,
-                                                          cls.inputs, ini_file=cls.ini_file,
-                                                          logger=cls.logger)
-        cls.isolated_creds.setUp()
-        cls.project = cls.isolated_creds.create_tenant()
-        cls.isolated_creds.create_and_attach_user_to_tenant()
-        cls.inputs = cls.isolated_creds.get_inputs()
-        cls.connections = cls.isolated_creds.get_conections()
-        #cls.connections= ContrailConnections(cls.inputs)
         cls.quantum_h = cls.connections.quantum_h
         cls.nova_h = cls.connections.nova_h
         cls.vnc_lib = cls.connections.vnc_lib
-#        cls.logger= cls.inputs.logger
         cls.agent_inspect = cls.connections.agent_inspect
         cls.cn_inspect = cls.connections.cn_inspect
         cls.analytics_obj = cls.connections.analytics_obj
@@ -32,8 +22,6 @@ class BaseRtFilterTest(test.BaseTestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # cls.isolated_creds.delete_user()
-        cls.isolated_creds.delete_tenant()
         super(BaseRtFilterTest, cls).tearDownClass()
     # end tearDownClass
 
