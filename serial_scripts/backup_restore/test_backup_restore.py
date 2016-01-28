092a3844bc650036932de617cca9b7cd60550afd
Aswani Kumar Reddy G <aswanikr@juniper.net>
2016-01-28 12:56:09 +0530

    changes to upgrade test_suite and added new backup_restore testsuite
    1)code to setup configurations is moved from base.py to verify.py to reuse
    2)verify.py file contains the code to create configurations and verify them before and after feature test
    3)backup_restore does the following checks
        1)creates configurations and verify them before backup
        2)Takes backup of whole data and configurations
        3)Resets all the configurations
        4)Restores the configuration and data with backup copy
        5)verifies the the configuration after restore
    
    Change-Id: I2dc1b4be601e665eb1acec351eed6991a641bd93

---
+++ serial_scripts/backup_restore/test_backup_restore.py
@@ -0,0 +1,84 @@
+import time
+import os
+from contrail_fixtures import *
+import testtools
+from tcutils.commands import *
+from fabric.context_managers import settings
+from tcutils.wrappers import preposttest_wrapper
+from tcutils.util import *
+from fabric.api import run
+from fabric.state import connections
+import test
+from OpenSSL.rand import status
+from upgrade.verify import VerifyFeatureTestCases
+from base import BackupRestoreBaseTest
+
+class TestBackupRestore(BackupRestoreBaseTest,VerifyFeatureTestCases):
+    ''' backup and restore the configurations '''
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestBackupRestore, cls).setUpClass()
+        cls.res.setUp(cls.inputs , cls.connections, cls.logger)
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.res.cleanUp()
+        super(TestBackupRestore, cls).tearDownClass()
+
+    def runTest(self):
+        pass
+    #end runTest
+    
+    @preposttest_wrapper
+    def test_fiptraffic_before_backup(self):
+        ''' Test to create policy, security group  and floating ip rules on common resources and checking if they work fine
+        '''
+        return self.verify_config_before_feature_test()
+    #end test_fiptraffic_before_backup
+
+    @preposttest_wrapper
+    def test_to_backup_restore(self):
+        '''Test to backup and restore all the configurations and data'''
+        result = True
+        username = self.inputs.host_data[self.inputs.cfgm_ip]['username']
+        password = self.inputs.host_data[self.inputs.cfgm_ip]['password']
+        with settings(
+            host_string='%s@%s' % (
+                username, self.inputs.cfgm_ips[0]),
+                password = password, warn_only=True, abort_on_prompts=False, debug=True):
+            
+            backup_cmd = "cd /opt/contrail/utils;fab backup_data " 
+            restore_cmd = "cd /opt/contrail/utils;fab restore_data " 
+            reset_cmd = "cd /opt/contrail/utils;fab reset_config "
+            
+            self.logger.info("starting backup")
+            status = run(backup_cmd)
+            self.logger.debug("LOG for fab backup_data : %s" % status)
+            assert not(status.return_code), 'Failed while running  backup_data'
+            result = result and not(status.return_code)
+            self.logger.info("backup completed")
+            
+            self.logger.info("starting reset config")
+            status = run(reset_cmd)
+            self.logger.debug("LOG for fab reset_config : %s" % status)
+            assert not(status.return_code), 'Failed while running reset_config'
+            result = result and not(status.return_code)
+            self.logger.info("reset configuration completed")
+            
+            self.logger.info("starting restore")
+            restore_cmd = "cd /root/fabric-utils;fab restore_data " 
+            status = run(restore_cmd)
+            self.logger.debug("LOG for fab restore_data: %s" % status)
+            assert not(status.return_code), 'Failed while running restore_data'
+            result=result and not(status.return_code)
+            self.logger.info("restore of data and configuration completed")  
+        return result
+    #end test_backup_restore   
+    
+    @preposttest_wrapper
+    def test_traffic_after_restore(self):
+        '''Test to test traffic after restore using previouly defined  policy and floating ip and then adding new policy,fip to new resources also  validate service chaining in network  datapath and security group
+        '''
+        return self.verify_config_after_feature_test()
+    #end test_traffic_after_restore        
