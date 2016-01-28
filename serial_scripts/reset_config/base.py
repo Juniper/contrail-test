5447e567f960d4fd6698927b958cb746fe373292
Aswani Kumar Reddy G <aswanikr@juniper.net>
2016-01-28 13:22:14 +0530

    Testsuite to test reset_config
    1) Create configurations and verify
    2) Reset all the Configurations
    3) Check all the configurations has been reset
    
    Change-Id: I594d6dfd7ae73a4dd477345c89cbdf8a8fba17f0

---
+++ serial_scripts/reset_config/base.py
@@ -0,0 +1,48 @@
+import test
+from common.connections import ContrailConnections
+from common.contrail_test_init import ContrailTestInit
+from common import isolated_creds
+from upgrade.verify import BaseResource
+
+class ResetConfigBaseTest(test.BaseTestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        super(ResetConfigBaseTest, cls).setUpClass()
+        cls.isolated_creds = isolated_creds.IsolatedCreds(cls.__name__, cls.inputs, ini_file = cls.ini_file, logger = cls.logger)
+        cls.isolated_creds.setUp()
+        cls.project = cls.isolated_creds.create_tenant()
+        cls.isolated_creds.create_and_attach_user_to_tenant()
+        cls.inputs = cls.isolated_creds.get_inputs()
+        cls.connections = cls.isolated_creds.get_conections()
+        cls.quantum_h= cls.connections.quantum_h
+        cls.nova_h = cls.connections.nova_h
+        cls.vnc_lib= cls.connections.vnc_lib
+        cls.agent_inspect= cls.connections.agent_inspect
+        cls.cn_inspect= cls.connections.cn_inspect
+        cls.analytics_obj=cls.connections.analytics_obj
+        resource_class = cls.__name__ + 'Resource'
+        cls.res = ResourceFactory.createResource(resource_class)
+    #end setUpClass
+    
+class ResourceFactory:
+    factories = {}
+    def createResource(id):
+        if not ResourceFactory.factories.has_key(id):
+            ResourceFactory.factories[id] = \
+              eval(id + '.Factory()')
+        return ResourceFactory.factories[id].create()
+    createResource = staticmethod(createResource)
+
+
+class TestResetConfigResource(BaseResource):
+    
+    def setUp(self,inputs,connections, logger):
+        super(TestResetConfigResource, self).setUp(inputs,connections, logger)
+
+    class Factory:
+        def create(self): return TestResetConfigResource()
+
+    def runTest(self):
+        pass
+  
