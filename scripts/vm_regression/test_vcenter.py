23748b62c8296ec6882d5fdf3d978927a6d1780f
Sandip Dey <sandipd@juniper.net>
2016-02-03 02:17:28 -0800

    Fixing script issues in vcenter sanity
    Change-Id: Id5f63cae6cde1b593c62041651ca0bcd67c284bb

--- scripts/vm_regression/test_vcenter.py
+++ scripts/vm_regression/test_vcenter.py
@@ -46,7 +46,7 @@ class TestVcenter(BaseVnVmTest):
         vm1.vm_obj.assign_ip('eth0', '12.11.10.101', '12.11.10.1', '255.255.255.0')
         assert vm1.wait_till_vm_is_up()
         vm2 = self.create_vm(vn_fixture=vn_fixture, vm_name=get_random_name('vm'))
-        vm2.vm_obj.assign_ip('eth0', '12.11.10.102')
+        vm2.vm_obj.assign_ip('eth0', '12.11.10.102', '12.11.10.1', '255.255.255.0')
         assert vm2.wait_till_vm_is_up()
 
         assert vm1.ping_with_certainty(dst_vm_fixture=\
