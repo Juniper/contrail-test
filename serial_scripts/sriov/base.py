35cd5e985d26fd40e4f234d66600b1f8b7846a76
Chhandak <chhandak@juniper.net>
2016-02-24 04:09:56 +0530

    Adding infra support to retrieve SRIOV data and few more  test case.
    * Adding support in test infra to retrieve SRIOV data
    * Adding testcase to verify  vf exhaustion and reuse
    * Adding test case to verify ping with large mtu
    
    Change-Id: I4fe99a36c3693b38ef940a52f089fc09e32dbd9a

--- serial_scripts/sriov/base.py
+++ serial_scripts/sriov/base.py
@@ -1,4 +1,7 @@
 import test
+import time
+import struct
+import socket
 import fixtures
 from common import isolated_creds
 
@@ -15,11 +18,9 @@ class BaseSriovTest(test.BaseTestCase):
         cls.isolated_creds.create_and_attach_user_to_tenant()
         cls.inputs = cls.isolated_creds.get_inputs()
         cls.connections = cls.isolated_creds.get_conections() 
-        #cls.connections= ContrailConnections(cls.inputs)
         cls.quantum_h= cls.connections.quantum_h
         cls.nova_h = cls.connections.nova_h
         cls.vnc_lib= cls.connections.vnc_lib
-#        cls.logger= cls.inputs.logger
         cls.agent_inspect= cls.connections.agent_inspect
         cls.cn_inspect= cls.connections.cn_inspect
         cls.analytics_obj=cls.connections.analytics_obj
@@ -27,7 +28,6 @@ class BaseSriovTest(test.BaseTestCase):
 
     @classmethod
     def tearDownClass(cls):
-        #cls.isolated_creds.delete_user()
         cls.isolated_creds.delete_tenant()
         super(BaseSriovTest, cls).tearDownClass()
     #end tearDownClass 
@@ -43,9 +43,67 @@ class BaseSriovTest(test.BaseTestCase):
               break
           else:
               time.sleep(3)
+    def get_sriov_enabled_compute_list(self):
+        sriov_host_name_list=[]      
+        sriov_host_list=self.inputs.sriov_data[0].keys()
+        for item in sriov_host_list:
+            sriov_host_name_list.append(self.inputs.host_data[item.split('@')[1]]['name'])
+        return sriov_host_name_list
 
-    def remove_from_cleanups(self, fix):
-        for cleanup in self._cleanups:
-        #    if fix.cleanUp in cleanup:
-            self._cleanups.remove(cleanup)
-            #break
+    def get_sriov_physnets(self,compute_name):
+        host_key=self.inputs.host_data[compute_name]['username'] + '@' + self.inputs.host_data[compute_name]['ip'] 
+        physnets_list={}
+        physnets_list=self.inputs.sriov_data[0][host_key][0]['physnets']
+        return physnets_list
+    
+    def get_sriov_vf_number(self,compute_name):
+        host_key=self.inputs.host_data[compute_name]['username'] + '@' + self.inputs.host_data[compute_name]['ip']
+        vf_number=None
+        vf_number=self.inputs.sriov_data[0][host_key][0]['VF']
+        return vf_number
+ 
+    def get_sriov_pf(self,compute_name):
+        host_key=self.inputs.host_data[compute_name]['username'] + '@' + self.inputs.host_data[compute_name]['ip']
+        pf_intf=None
+        pf_intf=self.inputs.sriov_data[0][host_key][0]['interface']
+        return pf_intf
+
+    def ip_increment(self,base_ip,increase_by): 
+        ip2int = lambda ipstr: struct.unpack('!I', socket.inet_aton(ipstr))[0]
+        ip_num=ip2int(base_ip)
+        ip_num=ip_num + int(increase_by)
+        int2ip = lambda n: socket.inet_ntoa(struct.pack('!I', n))
+        new_ip=int2ip(ip_num)
+        return new_ip
+
+    def vm_force_delete(self,vm_obj):
+        cmd= 'source /etc/contrail/openstackrc;nova force-delete %s' %(vm_obj.vm_id)
+        status=self.inputs.run_cmd_on_server(self.inputs.cfgm_ip, cmd)
+        return status
+
+    def get_sriov_mac(self,vm_fix,interface):
+        intf_cmd='ifconfig %s| grep HWaddr'%(interface)
+        output=vm_fix.run_cmd_on_vm(cmds=[intf_cmd], as_sudo=True)
+        return output[intf_cmd].split(" ")[15]
+
+    def get_vf_in_use(self,vm_fix,interface,mac):
+        host = self.inputs.get_host_ip(vm_fix.vm_node_ip)
+        cmd='ip link show dev %s| grep %s'%(interface,mac)
+        output=self.inputs.run_cmd_on_server(host, cmd)
+        return output.split(" ")[1]
+ 
+    def set_mtu_on_vf(self,vm_fix,intf,vf_num,vlan_num,mtu):
+        host = self.inputs.get_host_ip(vm_fix.vm_node_ip)
+        cmd='ip link set %s vf %s vlan %s mtu %s'%(intf,vf_num,vlan_num,mtu)
+        output=self.inputs.run_cmd_on_server(host, cmd) 
+        return output
+         
+
+    def remove_from_cleanups(self, fix):                                                                                                                           
+        for cleanup in self._cleanups:                                                                                                                             
+            if fix.cleanUp in cleanup:                                                                                                                             
+                self._cleanups.remove(cleanup)                                                                                                                     
+                break                                                                                                                                              
+    #end remove_from_cleanups 
+       
+     
