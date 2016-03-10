978bcc85d7eefe691411dccbb030b32e141127c9
hkumarmk <hkumar@d4devops.org>
2016-03-10 12:39:00 +0530

    Avoid using openstackrc for ec2_base
    * Fix using openstackrc for ec2_base
    * Fixed some pep8 issues
    * Use auth_protocol read from testbed rather than hardcoded to http

--- fixtures/ec2_base.py
+++ fixtures/ec2_base.py
@@ -1,19 +1,31 @@
 from fabric.api import local, run
 from fabric.context_managers import shell_env, settings
 import time
+import os
 
 
 class EC2Base(object):
-
     def __init__(self, tenant=None, logger=None, inputs=None):
         self.inputs = inputs
         if not tenant:
             tenant = self.inputs.stack_tenant
         self.tenant = tenant
+        self.username = inputs.stack_user
+        self.password = inputs.stack_password
         self.logger = logger
         self.openstack_ip = self.inputs.openstack_ip
+        self.openstack_ip = self.inputs.openstack_ip
+        self.auth_protocol = self.inputs.auth_protocol
         self.os_username = self.inputs.host_data[self.openstack_ip]['username']
         self.os_password = self.inputs.host_data[self.openstack_ip]['password']
+
+        if not self.inputs.ha_setup:
+            self.auth_url = os.getenv('OS_AUTH_URL') or \
+                            "%s://%s:5000/v2.0" % (self.auth_protocol, self.openstack_ip)
+        else:
+            self.auth_url = os.getenv('OS_AUTH_URL') or \
+                "%s://%s:5000/v2.0" % (self.auth_protocol, self.inputs.auth_ip)
+
         if not self._set_ec2_keys(tenant):
             if not self.create_ec2_keys(tenant):
                 self.logger.error('ec2-key create failed for vpc tenant')
@@ -24,15 +36,21 @@ class EC2Base(object):
         Run cmd on openstack node
         '''
         with settings(
-            host_string='%s@%s' % (self.os_username, self.openstack_ip), password=self.os_password,
+                host_string='%s@%s' % (self.os_username, self.openstack_ip), password=self.os_password,
                 warn_only=True, abort_on_prompts=False):
             output = run(cmd)
             return output
+
     # end run_cmd_on_os_node
 
     def _set_ec2_keys(self, tenant):
         # export ec2 secret key and access key for admin or VPC
-        keys = self.run_cmd_on_os_node('(source /etc/contrail/openstackrc; keystone ec2-credentials-list)'
+        keys = self.run_cmd_on_os_node("(keystone --os-username %s --os-password %s \
+                                        --os-tenant-name %s --os-auth-url %s \
+                                        ec2-credentials-list)" % (self.username,
+                                                                  self.password,
+                                                                  self.tenant,
+                                                                  self.auth_url)
                                        ).split('\n')[3:]
         found = False
 
@@ -45,12 +63,13 @@ class EC2Base(object):
                 self.secret_key = key[2]
                 break
         return found
+
     # end set_ec2_keys
 
     def _shell_with_ec2_env(self, command, ret):
         # shell to run Euca commands on machine with ec2 credentials
         with settings(
-            host_string='%s@%s' % (self.os_username, self.openstack_ip), password=self.os_password,
+                host_string='%s@%s' % (self.os_username, self.openstack_ip), password=self.os_password,
                 warn_only=True, abort_on_prompts=False):
             with shell_env(EC2_ACCESS_KEY=self.access_key,
                            EC2_SECRET_KEY=self.secret_key,
@@ -74,14 +93,22 @@ class EC2Base(object):
                     self.logger.debug('Output : %s' % (out))
             if ret:
                 return out
+
     # end _shell_with_ec2_env
 
     def create_ec2_keys(self, tenant_name):
         key_data = {}
         # create ec2 credentials for VPC
         tenantId = self._get_tenant_id(tenant_name)
-        output = self.run_cmd_on_os_node('(source /etc/contrail/openstackrc; keystone ec2-credentials-create \
-                               --tenant-id %s)' % tenantId, ).split('\n')
+        output = self.run_cmd_on_os_node('(keystone ec2-credentials-create \
+                                        --os-username %s --os-password %s \
+                                        --os-tenant-name %s --os-auth-url %s \
+                                        --tenant-id %s)' % (self.username,
+                                                            self.password,
+                                                            self.tenant,
+                                                            self.auth_url,
+                                                            tenantId)
+                                         ).split('\n')
         self.logger.info('EC2 keys created for %s' % tenant_name)
         for row in output:
             if row[0] == '+':
@@ -93,17 +120,33 @@ class EC2Base(object):
         self.secret_key = key_data['secret']
         self.logger.debug(key_data)
         return key_data
+
     # end create_ec2_keys
 
     def delete_ec2_keys(self, accessKey):
-        self.run_cmd_on_os_node('(source /etc/contrail/openstackrc; keystone ec2-credentials-delete \
-                                                         --access %s)' % accessKey)
+        self.run_cmd_on_os_node('(keystone ec2-credentials-delete \
+                                        --os-username %s --os-password %s \
+                                        --os-tenant-name %s --os-auth-url %s \
+                                        --access %s)' % (self.username,
+                                                         self.password,
+                                                         self.tenant,
+                                                         self.auth_url,
+                                                         accessKey)
+                                )
         self.logger.info('EC2 keys deleted for VPC')
+
     # end delete_ec2_keys
 
     def _get_tenant_id(self, tenantName):
-        tenants = self.run_cmd_on_os_node('(source /etc/contrail/openstackrc; keystone tenant-get %s)'
-                                          % tenantName, ).split('\n')
+        tenants = self.run_cmd_on_os_node('(keystone tenant-get \
+                                        --os-username %s --os-password %s \
+                                        --os-tenant-name %s --os-auth-url %s \
+                                        %s)' % (self.username,
+                                                self.password,
+                                                self.tenant,
+                                                self.auth_url,
+                                                tenantName)
+                                          ).split('\n')
 
         for tenant in tenants:
             tenant = [k for k in filter(None, tenant.split(' ')) if k != '|']
@@ -112,5 +155,6 @@ class EC2Base(object):
                 break
 
         return self.tenant_id
-    # end _get_tenant_id
+        # end _get_tenant_id
+
 # end class EC2Base
