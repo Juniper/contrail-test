9db97abdd19e6355d54a709c78314891a4dc7a5e
hkumarmk <hkumar@d4devops.org>
2016-03-11 12:41:02 +0530

    Changes in docker build
    Changed docker buid into two parts
    
    1. a prep image which has all packages installed,
    contrail-install-packages setup and ready for setup contrail-test-ci or
    contrail-test
    2. final image build from prep image
    
    This will reduce the image build time, as prep image only need to build
    once on every sku, contrail version combination.

--- install.sh
+++ install.sh
@@ -28,8 +28,21 @@ Run $0 <Subcommand> -h|--help to get subcommand specific help
 EOF
 }
 
+# Provided Docker image available?
+is_image_available () {
+    tag=${1:-$pos_arg}
+    repo=${tag%:.*}
+    version=${tag#*:}
+    docker images $repo | grep -q $version
+}
+
 function have_command {
     type "$1" >/dev/null 2>/dev/null
+}
+
+function fail {
+    echo $@
+    exit 1
 }
 
 function distro {
@@ -195,18 +208,22 @@ EOT
 }
 
 function make_dockerfile {
+    type=$1
+    base_image=${2:-hkumar/ubuntu-14.04.2}
     cat <<EOF
-FROM hkumar/ubuntu-14.04.2
+FROM $base_image
 MAINTAINER Juniper Contrail <contrail@juniper.net>
+ARG http_proxy
+ARG https_proxy
 ARG CONTRAIL_INSTALL_PACKAGE_URL
 ARG ENTRY_POINT=docker_entrypoint.sh
 ARG SSHPASS
 ENV DEBIAN_FRONTEND=noninteractive
 ENV SKU=$openstack_release
 EOF
-
-if [[ $CONTRAIL_INSTALL_PACKAGE_URL =~ ^http[s]*:// ]]; then
-    cat <<EOF
+    if [[ $type == 'prep' ]]; then
+        if [[ $CONTRAIL_INSTALL_PACKAGE_URL =~ ^http[s]*:// ]]; then
+            cat <<EOF
 # Just check if $CONTRAIL_INSTALL_PACKAGE_URL is there, if not valid, build will fail
 RUN wget -q --spider $CONTRAIL_INSTALL_PACKAGE_URL
 
@@ -218,11 +235,11 @@ RUN wget $CONTRAIL_INSTALL_PACKAGE_URL -O /contrail-install-packages.deb && \
     apt-get install -y $PACKAGES_REQUIRED && \
                     rm -fr /opt/contrail/* ; apt-get -y autoremove && apt-get -y clean;
 EOF
-elif [[ $CONTRAIL_INSTALL_PACKAGE_URL =~ ^ssh[s]*:// ]]; then
-    scp_package=1
-    server=` echo $CONTRAIL_INSTALL_PACKAGE_URL | sed 's/ssh:\/\///;s|\/.*||'`
-    path=`echo $CONTRAIL_INSTALL_PACKAGE_URL |sed -r 's#ssh://[a-zA-Z0-9_\.\-]+##'`
-    cat << EOF
+        elif [[ $CONTRAIL_INSTALL_PACKAGE_URL =~ ^ssh[s]*:// ]]; then
+            scp_package=1
+            server=` echo $CONTRAIL_INSTALL_PACKAGE_URL | sed 's/ssh:\/\///;s|\/.*||'`
+            path=`echo $CONTRAIL_INSTALL_PACKAGE_URL |sed -r 's#ssh://[a-zA-Z0-9_\.\-]+##'`
+            cat << EOF
 
 RUN apt-get install -y sshpass && \
     sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${sshuser_sub}${server}:${path} /contrail-install-packages.deb && \
@@ -232,65 +249,71 @@ RUN apt-get install -y sshpass && \
     apt-get install -y $PACKAGES_REQUIRED && \
                     rm -fr /opt/contrail/* && apt-get -y autoremove && apt-get -y clean
 EOF
-else
-    echo "ERROR, Unknown url format, only http[s], ssh supported" >&2
-    exit 1
-fi
-    # In case of contrail-test, ci should be added in /contrail-test-ci and later merge both
-    # /contrail-test-ci and /contrail-test together
-    ci_dir='/contrail-test'
-
-    if [[ $build_type == 'contrail-test' ]]; then
-        ci_dir='/contrail-test-ci'
-        merge_code='cp -RTf /contrail-test-ci /contrail-test; '
-        if [[ -f $CONTRAIL_TEST_ARTIFACT ]]; then
-            echo -e "ADD $(basename $CONTRAIL_TEST_ARTIFACT) /"
         else
-            cat <<EOF
+            echo "ERROR, Unknown url format, only http[s], ssh supported" >&2
+            exit 1
+        fi
+
+        cat <<EOF
+    RUN wget -q --spider $CIRROS_IMAGE_URL
+    RUN mkdir -p /images && wget $CIRROS_IMAGE_URL -O /images/cirros-0.3.0-x86_64-disk.vmdk.gz
+EOF
+    #Finished dockerfile for prep image
+    else
+        # In case of contrail-test, ci should be added in /contrail-test-ci and later merge both
+        # /contrail-test-ci and /contrail-test together
+        ci_dir='/contrail-test'
+
+        if [[ $build_type == 'contrail-test' ]]; then
+            ci_dir='/contrail-test-ci'
+            merge_code='cp -RTf /contrail-test-ci /contrail-test; '
+            if [[ -f $CONTRAIL_TEST_ARTIFACT ]]; then
+                echo -e "ADD $(basename $CONTRAIL_TEST_ARTIFACT) /"
+            else
+                cat <<EOF
 RUN git clone $CONTRAIL_TEST_REPO /contrail-test; \
     cd /contrail-test ; \
     git checkout $CONTRAIL_TEST_REF; \
     git reset --hard; \
     rm -fr .git
 EOF
+            fi
+        elif [[ $build_type == 'contrail-test-ci' && -f $CONTRAIL_TEST_CI_ARTIFACT ]] ; then
+            merge_code='mv /contrail-test-ci /contrail-test; '
         fi
-    elif [[ $build_type == 'contrail-test-ci' && -f $CONTRAIL_TEST_CI_ARTIFACT ]] ; then
-        merge_code='mv /contrail-test-ci /contrail-test; '
-    fi
 
-    if [[ -f $CONTRAIL_TEST_CI_ARTIFACT ]]; then
-        echo -e "ADD $(basename $CONTRAIL_TEST_CI_ARTIFACT) /"
-    else
-        cat <<EOF
+        if [[ -f $CONTRAIL_TEST_CI_ARTIFACT ]]; then
+            echo -e "ADD $(basename $CONTRAIL_TEST_CI_ARTIFACT) /"
+        else
+            cat <<EOF
 RUN git clone $CONTRAIL_TEST_CI_REPO $ci_dir; \
     cd $ci_dir ; \
     git checkout $CONTRAIL_TEST_CI_REF; \
     git reset --hard; \
     rm -fr .git
 EOF
-     fi
+         fi
 
-    if [[ -f $CONTRAIL_FAB_ARTIFACT ]]; then
-        echo -e "ADD $(basename $CONTRAIL_FAB_ARTIFACT) /opt/contrail/"
-        fab_utils_mv="mv /opt/contrail/fabric-utils /opt/contrail/utils; "
-    else
-        cat <<EOF
+        if [[ -f $CONTRAIL_FAB_ARTIFACT ]]; then
+            echo -e "ADD $(basename $CONTRAIL_FAB_ARTIFACT) /opt/contrail/"
+            fab_utils_mv="mv /opt/contrail/fabric-utils /opt/contrail/utils; "
+        else
+            cat <<EOF
 RUN git clone $CONTRAIL_FAB_REPO /opt/contrail/utils; \
     cd /opt/contrail/utils ; \
     git checkout $CONTRAIL_FAB_REF; \
     git reset --hard;
 EOF
-    fi
+        fi
 
-    cat <<EOF
+        cat <<EOF
 RUN  $merge_code $fab_utils_mv cd /contrail-test && pip install --upgrade -r requirements.txt
-RUN wget -q --spider $CIRROS_IMAGE_URL
-RUN mkdir -p /contrail-test/images && wget $CIRROS_IMAGE_URL -O /contrail-test/images/cirros-0.3.0-x86_64-disk.vmdk.gz
+RUN mv /images /contrail-test/images
 COPY \$ENTRY_POINT /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 EOF
-
+    fi
 }
 
 docker_build () {
@@ -322,6 +345,8 @@ Usage: $0 docker-build [OPTIONS] (contrail-test|contrail-test-ci)
 
   -c|--use-cache                Use docker cache for the build
   -e|--export EXPORT_PATH       Export Container image to the path provided
+  -f|--force                    Build the "prep" containers even though they are exist. Prep containers are the intermediate
+                                containers which has all contrail packages installed, and ready to setup contrail-test-ci/contrail-test
 
   positional arguments
     Type of build               What to build, valid options are contrail-test, contrail-test-ci
@@ -341,7 +366,82 @@ Usage: $0 docker-build [OPTIONS] (contrail-test|contrail-test-ci)
 EOF
     }
 
-    if ! options=$(getopt -o hcu:t:e: -l help,test-artifact:,ci-artifact:,fab-artifact:,use-cache,ci-repo:,ci-ref:,export:,test-repo:,test-ref:,contrail-install-package-url:,fab-repo:,fab-ref:,container-tag: -- "$@"); then
+    build_prep () {
+        image_tag=${1:-$PREP_IMAGE}
+        BUILD_DIR=`mktemp -d`
+        make_dockerfile prep 'hkumar/ubuntu-14.04.2' > $BUILD_DIR/Dockerfile
+
+        if [[ -n $scp_package ]]; then
+            # Disabling xtrace to avoid printing SSHPASS
+            if xtrace_status; then
+                set +x
+                xtrace=1
+            fi
+            ssh_build_arg="--build-arg SSHPASS=$SSHPASS"
+        fi
+        docker build ${cache_opt} ${proxy_args} -t $image_tag \
+            --build-arg CONTRAIL_INSTALL_PACKAGE_URL=$CONTRAIL_INSTALL_PACKAGE_URL \
+            ${ssh_build_arg} $BUILD_DIR; rv=$?
+        [[ -n $xtrace ]] && set -x
+
+        rm -rf $BUILD_DIR
+        return $rv
+    }
+
+    build_final () {
+        base_image=${1:-$PREP_IMAGE}
+        BUILD_DIR=`mktemp -d`
+        make_dockerfile final $base_image > $BUILD_DIR/Dockerfile
+
+        if [[ -f $CONTRAIL_TEST_ARTIFACT ]]; then
+            cp $CONTRAIL_TEST_ARTIFACT $BUILD_DIR/
+        fi
+
+        if [[ -f $CONTRAIL_TEST_CI_ARTIFACT ]]; then
+            cp $CONTRAIL_TEST_CI_ARTIFACT $BUILD_DIR
+        fi
+
+        if [[ -f $CONTRAIL_FAB_ARTIFACT ]]; then
+            cp $CONTRAIL_FAB_ARTIFACT $BUILD_DIR
+        fi
+
+
+        if [[ $build_type == 'contrail-test' ]]; then
+            make_entrypoint_contrail_test > ${BUILD_DIR}/docker_entrypoint.sh
+        elif [[ $build_type == 'contrail-test-ci' ]]; then
+            make_entrypoint_contrail_test_ci > ${BUILD_DIR}/docker_entrypoint.sh
+        else
+            echo "ERROR! Unknown build_type: $build_type"
+            exit 1
+        fi
+
+        docker build ${proxy_args} ${cache_opt} -t $CONTAINER_TAG $BUILD_DIR; rv=$?
+        rm -rf $BUILD_DIR
+        return $rv
+    }
+
+    save () {
+        if [[ -n $EXPORT_PATH ]]; then
+            echo "Exporting the image to $EXPORT_PATH"
+            mkdir -p $EXPORT_PATH
+            docker save $CONTAINER_TAG | gzip -c > ${EXPORT_PATH}/docker-image-${CONTAINER_TAG/:/-}.tar.gz; rv=$?
+            if [ $rv -eq 0 ]; then
+                echo "Successfully exported the image to ${EXPORT_PATH}/docker-image-${CONTAINER_TAG/:/-}.tar.gz"
+                docker rmi -f $CONTAINER_TAG; rv=$?
+                if [ $rv -eq 0 ]; then
+                    echo "Cleaned up the image $CONTAINER_TAG from build environment"
+                else
+                    echo "Failed to cleanup the image $CONTAINER_TAG from the build environment, please cleanup manually"
+                    exit 1
+                fi
+            else
+                echo "Failed to export the image $CONTAINER_TAG"
+                exit 2
+            fi
+        fi
+    }
+
+    if ! options=$(getopt -o hcu:t:e:f -l help,force,test-artifact:,ci-artifact:,fab-artifact:,use-cache,ci-repo:,ci-ref:,export:,test-repo:,test-ref:,contrail-install-package-url:,fab-repo:,fab-ref:,container-tag: -- "$@"); then
 # parse error
         usage
         exit 1
@@ -364,6 +464,7 @@ EOF
             -t|--container-tag) CONTAINER_TAG=$2; shift;;
             -c|--use-cache) CACHE=1;;
             -e|--export) EXPORT_PATH=$2; shift;;
+            -f|--force) FORCE_BUILD=1;;
             --) :;;
             *) build_type=$1;;
 	    esac
@@ -388,21 +489,6 @@ EOF
         CONTAINER_TAG=${build_type}-${openstack_release}:${contrail_version}
     fi
 
-    BUILD_DIR=`mktemp -d`
-    make_dockerfile > $BUILD_DIR/Dockerfile
-
-    if [[ -f $CONTRAIL_TEST_ARTIFACT ]]; then
-        cp $CONTRAIL_TEST_ARTIFACT $BUILD_DIR/
-    fi
-
-    if [[ -f $CONTRAIL_TEST_CI_ARTIFACT ]]; then
-        cp $CONTRAIL_TEST_CI_ARTIFACT $BUILD_DIR
-    fi
-
-    if [[ -f $CONTRAIL_FAB_ARTIFACT ]]; then
-        cp $CONTRAIL_FAB_ARTIFACT $BUILD_DIR
-    fi
-
     # IS docker runnable?
     docker  -v &> /dev/null ; rv=$?
 
@@ -415,50 +501,31 @@ EOF
       cache_opt='--no-cache'
     fi
 
-    if [[ $build_type == 'contrail-test' ]]; then
-        make_entrypoint_contrail_test > ${BUILD_DIR}/docker_entrypoint.sh
-    elif [[ $build_type == 'contrail-test-ci' ]]; then
-        make_entrypoint_contrail_test_ci > ${BUILD_DIR}/docker_entrypoint.sh
-    else
-        echo "ERROR! Unknown build_type: $build_type"
-        exit 1
+    if [[ -n $http_proxy ]]; then
+        proxy_args="--build-arg http_proxy=$http_proxy "
     fi
 
-    if [[ -n $scp_package ]]; then
-        # Disabling xtrace to avoid printing SSHPASS
-        if xtrace_status; then
-            set +x
-            xtrace=1
+    if [[ -n $https_proxy ]]; then
+        proxy_args="$proxy_args --build-arg https_proxy=$https_proxy "
+    fi
+
+    PREP_IMAGE="prep-${openstack_release}:${contrail_version}"
+    if ! is_image_available "$PREP_IMAGE" || [[ -n $FORCE_BUILD ]]; then
+        echo "Building prep image - $PREP_IMAGE"
+        build_prep; rv=$?
+        if [ $rv -eq 0 ]; then
+            echo "Successfully built prep image"
+        else
+            fail "ERROR!! Failed to build prep image"
         fi
-        ssh_build_arg="--build-arg SSHPASS=$SSHPASS"
     fi
-    docker build ${cache_opt} -t ${CONTAINER_TAG} \
-        --build-arg CONTRAIL_INSTALL_PACKAGE_URL=$CONTRAIL_INSTALL_PACKAGE_URL \
-        ${ssh_build_arg} $BUILD_DIR; rv=$?
-    [[ -n $xtrace ]] && set -x
-
-    rm -rf $BUILD_DIR
-
+    echo "Building final image - $CONTAINER_TAG"
+    build_final; rv=$?
     if [ $rv -eq 0 ]; then
         echo "Successfully built the container image - $CONTAINER_TAG"
-        if [[ -n $EXPORT_PATH ]]; then
-            echo "Exporting the image to $EXPORT_PATH"
-            mkdir -p $EXPORT_PATH
-            docker save $CONTAINER_TAG | gzip -c > ${EXPORT_PATH}/docker-image-${CONTAINER_TAG/:/-}.tar.gz; rv=$?
-            if [ $rv -eq 0 ]; then
-                echo "Successfully exported the image to ${EXPORT_PATH}/docker-image-${CONTAINER_TAG/:/-}.tar.gz"
-                docker rmi -f $CONTAINER_TAG; rv=$?
-                if [ $rv -eq 0 ]; then
-                    echo "Cleaned up the image $CONTAINER_TAG from build environment"
-                else
-                    echo "Failed to cleanup the image $CONTAINER_TAG from the build environment, please cleanup manually"
-                    exit 1
-                fi
-            else
-                echo "Failed to export the image $CONTAINER_TAG"
-                exit 2
-            fi
-        fi
+        save
+    else
+        fail "ERROR! Failed to build the container image - $CONTAINER_TAG"
     fi
 }
 
