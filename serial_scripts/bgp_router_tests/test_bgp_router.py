2d5c700553d6934ae08843536947bba51dc0caf2
Aswani Kumar Reddy G <aswanikr@juniper.net>
2016-04-04 21:05:52 +0530

    Added testcase to test control node bgp peering
    1)Control node should not be Active if it doesn't have bgp-router for self
    2)Delete control_node and check for bgp_router for self
    
    Change-Id: Ib89e3d3c36a7957e97b2f0f6e2680e3515fe69a7
    Closes-Bug: #1423676

---
+++ serial_scripts/bgp_router_tests/test_bgp_router.py
@@ -0,0 +1,59 @@
+from tcutils.commands import *
+from tcutils.util import retry
+from tcutils.wrappers import preposttest_wrapper
+from control_node import CNFixture
+import base
+
+class BgpRouterTest(base.BaseBgpRouterTest):
+    
+    @classmethod
+    def setUpClass(cls):
+        super(BgpRouterTest, cls).setUpClass()
+    #end setUpClass
+
+    @classmethod
+    def tearDownClass(cls):
+        super(BgpRouterTest, cls).tearDownClass()
+    #end tearDownClass
+    
+    @preposttest_wrapper
+    def test_control_node_bgp_peering(self):
+        '''Control node should not be Active if it doesn't have bgp-router for self 
+        1)delete bgp_router of control node
+        2)check contrail-status for control-service  
+        3)Test passed if control service is inactive with message No BGP configuration for self
+        '''
+        bgp_ip=self.inputs.bgp_ips[0]
+        hostname = self.inputs.host_data[bgp_ip]['name']
+        control_ip = self.inputs.host_data[bgp_ip]['host_control_ip']
+        cn_user = self.inputs.host_data[control_ip]['username']
+        cn_password = self.inputs.host_data[control_ip]['password']
+        cn_status = 'contrail-status'
+        router_type = 'contrail'
+        check_str ='No BGP configuration for self'
+        cn_fixture = self.useFixture(
+            CNFixture(
+                connections=self.connections,
+                router_name=hostname,
+                router_ip=control_ip,
+                router_type=router_type,
+                inputs=self.inputs))
+        self.logger.info('Delete control node bgp peer %s' %control_ip)
+        cn_fixture.del_cn_node(control_ip)
+        output=self.inputs.run_cmd_on_server(control_ip,cn_status)
+        if check_str not in output:
+            self.logger.info('Control node should not be Active if it does not have bgp-router for self.Test failed')
+            cn_fixture.create_cn_node(control_ip,router_type)
+            return False
+        cn_fixture.create_cn_node(control_ip,router_type)
+        self.logger.info('Control service is inactive and has message No BGP configuration for self.Test Passed')  
+    #end test_control_node_bgp_peering
+    
+    @preposttest_wrapper
+    def test_bgp_peer_passive(self):
+        pass
+    
+    @preposttest_wrapper
+    def test_bgp_router_local_AS(self):
+        pass
+    
