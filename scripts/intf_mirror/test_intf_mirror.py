145f79cdf5f4c5dca801390e3f2c376c67d5354d
Ankit Jain <ankitja@juniper.net>
2016-05-10 04:31:24 -0700

    Adding intf mirroring infra code and basic test case - New
        Adding intf mirroring tests for the following :
            -src vm in vn1, mirror vm in vn1, and dst vm in vn2
            -src vm in vn1, mirror vm in vn3, and dst vm in vn2
            -src vm, dst vm and mirror vm all are in vn1
            -src vm in vn1, dst vm in vn2, and mirror vm in vn2
    
    Each of below test cases (compute node combinations) has above 4 VN combinations : Total 20
    
    Sanity:
    
    test_intf_mirroring_1:  src vm, dst vm and analyzer vm on different CNs
    
    Regression :
    
    test_intf_mirroring_2: src vm, dst vm and analyzer vm on same CN
    
    test_intf_mirroring_3: src vm, dst vm on same CN and analyzer vm on different CN
    
    test_intf_mirroring_4:  src vm, analyzer on same CN and dst vm on different CN
    
    test_intf_mirroring_5: dst vm, analyzer vm on same CN and src vm on different CN
    
    Change-Id: Id35d988e820c89334c06ccb4f69e6d71f52dff96

---
+++ scripts/intf_mirror/test_intf_mirror.py
@@ -0,0 +1,61 @@
+"""Intf mirroring Regression tests."""
+import os
+import unittest
+import fixtures
+import testtools
+import test
+
+from common.connections import ContrailConnections
+from common.contrail_test_init import ContrailTestInit
+from tcutils.wrappers import preposttest_wrapper
+from common.intf_mirroring.verify import VerifyIntfMirror
+from base import BaseIntfMirrorTest
+
+class TestIntfMirror(BaseIntfMirrorTest, VerifyIntfMirror):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestIntfMirror, cls).setUpClass()
+
+    def runTest(self):
+        pass
+    # end runTest
+
+    @test.attr(type=['ci_sanity_WIP', 'sanity', 'quick_sanity'])
+    @preposttest_wrapper
+    def test_intf_mirroring_1(self):
+        """Validate the intf mirroring
+        src vm, dst vm and analyzer vm on different CNs
+        """
+        return self.verify_intf_mirroring_1()
+
+    @preposttest_wrapper
+    def test_intf_mirroring_2(self):
+        """Validate the intf mirroring
+        src vm, dst vm and analyzer vm on same CN
+        """
+        return self.verify_intf_mirroring_2()
+
+    @preposttest_wrapper
+    def test_intf_mirroring_3(self):
+        """Validate the intf mirroring
+        src vm, dst vm on same CN and analyzer vm on different CN
+        """
+        return self.verify_intf_mirroring_3()
+
+    @preposttest_wrapper
+    def test_intf_mirroring_4(self):
+        """Validate the intf mirroring
+        src vm, analyzer on same CN and dst vm on different CN
+        """
+        return self.verify_intf_mirroring_4()
+
+    @preposttest_wrapper
+    def test_intf_mirroring_5(self):
+        """Validate the intf mirroring
+        dst vm, analyzer vm on same CN and src vm on different CN
+        """
+        return self.verify_intf_mirroring_5()
+
+if __name__ == '__main__':
+    unittest.main()
