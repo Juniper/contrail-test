2840122f17be4f598c63fda6e67bdd96e3f32410
skiranh <skiranh@juniper.net>
2016-06-02 22:17:44 +0530

    New testcases for xmpp related tests   - Added basic testcases for checking xmpp auth status   - Testcases for checking server vs agent auth precedence   - Testcases for adding/ deleting auth.
    Change-Id: I691a50f7da55c7dfa5aa4ec57cf05313ed702637
    Closes-Bug: #1565411

--- serial_scripts/xmpp/test_xmpp_auth.py
+++ serial_scripts/xmpp/test_xmpp_auth.py
@@ -1,8 +1,4 @@
 from tcutils.wrappers import preposttest_wrapper
-from vnc_api.vnc_api import NoIdError
-from vnc_api.vnc_api import VncApi
-from scripts.securitygroup.verify import VerifySecGroup
-from policy_test import PolicyFixture
 from vn_test import MultipleVNFixture
 from vm_test import MultipleVMFixture
 from base import XmppBase
@@ -12,7 +8,6 @@ from vm_test import VMFixture
 import os
 import sys
 import test
-from time import sleep
 from tcutils.contrail_status_check import *
 
 
@@ -27,9 +22,14 @@ class TestXmpptests(XmppBase, ConfigPolicy):
         super(TestXmpptests, cls).tearDownClass()
 
     def is_test_applicable(self):
-        for node in self.inputs.bgp_control_ips:
-            if not self.check_if_xmpp_auth_enabled(node):
+        try:
+            assert self.inputs.xmpp_auth_enable
+            assert self.inputs.xmpp_dns_auth_enable
+            if not (self.inputs.xmpp_auth_enable and self.inputs.xmpp_dns_auth_enable):
                 return (False, 'Xmpp auth should be set before running tests')
+        except:
+            return (False, 'Xmpp auth should be set before running tests')
+
         return (True, None)
 
     def setUp(self):
@@ -40,12 +40,12 @@ class TestXmpptests(XmppBase, ConfigPolicy):
         else:
             return
 
-    @test.attr(type=['sanity'])
+    @test.attr(type=['sm_sanity'])
     @preposttest_wrapper
     def test_precedence_xmpp_auth(self):
         """
-        Description: Undo xmpp auth with allow specific protocol on all ports 
-        and policy with allow all between VN's
+        Precedence in XMPP auth is that server auth takes precedence over agent auth. To check this,
+        undo xmpp auth on server side and check if XMPP is up and it shows no "TLS" string in introspect.
         """
         # Have to add cleanup here before entering for loop to disable auth as
         # there are asserts in the loop
@@ -69,7 +69,7 @@ class TestXmpptests(XmppBase, ConfigPolicy):
                 operation='set',
                 section='DEFAULT',
                 knob='xmpp_auth_enable',
-                value='True',
+                value='true',
                 node=node,
                 service='supervisor-control')
             assert (self.check_xmpp_status(node)
@@ -81,12 +81,12 @@ class TestXmpptests(XmppBase, ConfigPolicy):
 
     # end test_precedence_xmpp_auth
 
-    @test.attr(type=['sanity'])
+    @test.attr(type=['sm_sanity'])
     @preposttest_wrapper
     def test_undo_xmpp_auth(self):
         """
-        Description: Undo xmpp auth with allow specific protocol on all ports 
-        and policy with allow all between VN's
+        Check if undoing setting and deleting xmpp auth creates any issues.
+        Also confirm if introspect reflects the changes as and when they are done
         """
         for node in self.inputs.bgp_control_ips:
             self.update_contrail_conf(
@@ -98,9 +98,9 @@ class TestXmpptests(XmppBase, ConfigPolicy):
                 service='supervisor-control')
         # adding cleanup before assert
         self.addCleanup(self.enable_auth_on_cluster)
-        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         assert (self.check_xmpp_status(node)
                 ), "XMPP between nodes not up after deleting xmpp auth"
+        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         assert (self.check_if_xmpp_auth_enabled(node, 'NIL')
                 ), "Xmpp auth still set after disabling it on server side"
         for node in self.inputs.bgp_control_ips:
@@ -109,23 +109,23 @@ class TestXmpptests(XmppBase, ConfigPolicy):
                 operation='set',
                 section='DEFAULT',
                 knob='xmpp_auth_enable',
-                value='True',
+                value='true',
                 node=node,
                 service='supervisor-control')
-        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         assert (self.check_xmpp_status(node)
                 ), "XMPP between nodes not up after adding back xmpp auth"
+        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         assert (self.check_if_xmpp_auth_enabled(node)
                 ), "Xmpp auth not set after enabling it on server side"
         return True
     # end test_undo_xmpp_auth
 
-    @test.attr(type=['sanity'])
+    @test.attr(type=['sm_sanity'])
     @preposttest_wrapper
     def test_compute_negative_xmpp_auth(self):
         """
-        Description: Undo xmpp auth with allow specific protocol on all ports 
-        and policy with allow all between VN's
+        Configure xmpp auth only on agent side. Because of server precedence over agent, XMPP should go down.
+        Reconfigure auth back and confirm if things go back to normal.
         """
 
         for node in self.inputs.compute_ips:
@@ -138,34 +138,36 @@ class TestXmpptests(XmppBase, ConfigPolicy):
                 service='supervisor-vrouter')
         # adding cleanup before assert
         self.addCleanup(self.enable_auth_on_cluster)
-        assert (not (self.check_if_cluster_has_xmpp())
-                ), "XMPP connections should not be found"
         for node in self.inputs.bgp_control_ips:
             assert (not (self.check_if_xmpp_connections_present(node))
                     ), "XMPP between nodes should not be up after deleting xmpp auth on agent side"
+        assert (not (self.check_if_cluster_has_xmpp())
+                ), "XMPP connections should not be found"
+
         for node in self.inputs.compute_ips:
             self.update_contrail_conf(
                 conf_file='/etc/contrail/contrail-vrouter-agent.conf',
                 operation='set',
                 section='DEFAULT',
                 knob='xmpp_auth_enable',
-                value='True',
+                value='true',
                 node=node,
                 service='supervisor-vrouter')
-        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         for node in self.inputs.bgp_control_ips:
             assert (self.check_xmpp_status(node)
                     ), "XMPP between nodes not up after adding back xmpp auth"
             assert (self.check_if_xmpp_auth_enabled(node)
                     ), "Xmpp auth not set after enabling it on agent side"
+        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         return True
     # end test_compute_negative_xmpp_auth
 
     @preposttest_wrapper
     def test_restart_services_xmpp_auth(self):
         """
-        Description: Undo xmpp auth with allow specific protocol on all ports 
-        and policy with allow all between VN's
+        Configure xmpp auth and restart control and vrouter services for 10 iterations.
+        Cluster status is checked after each restart iteration. XMPP status is check after
+        all iterations are done.
         """
 
         # adding cleanup here before for loop as the loop has assert
@@ -177,10 +179,10 @@ class TestXmpptests(XmppBase, ConfigPolicy):
                 ).wait_till_contrail_cluster_stable(nodes=[node])
                 assert cluster_status, 'Hash of error nodes and services : %s' % (
                     error_nodes)
-        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         for node in self.inputs.bgp_control_ips:
             assert (self.check_xmpp_status(node)), "XMPP between nodes not up"
             assert (self.check_if_xmpp_auth_enabled(node)), "Xmpp auth not set"
+        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         for i in range(1, 10):
             for node in self.inputs.bgp_control_ips:
                 self.inputs.restart_service('supervisor-control', [node])
@@ -188,10 +190,10 @@ class TestXmpptests(XmppBase, ConfigPolicy):
                 ).wait_till_contrail_cluster_stable(nodes=[node])
                 assert cluster_status, 'Hash of error nodes and services : %s' % (
                     error_nodes)
-        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         for node in self.inputs.bgp_control_ips:
             assert (self.check_xmpp_status(node)), "XMPP between nodes not up"
             assert (self.check_if_xmpp_auth_enabled(node)), "Xmpp auth not set"
+        assert (self.check_if_cluster_has_xmpp), "XMPP connections not found"
         return True
     # end test_restart_services_xmpp_auth
 
