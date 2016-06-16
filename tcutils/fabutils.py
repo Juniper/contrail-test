5e95a0da250fced66f29f1c09c2bcb8fc0d5ee57
hkumarmk <hkumar@d4devops.org>
2016-06-16 13:35:32 +0530

    Added remote_copy, updated remote_cmd
    * updated remote_cmd
      * Added proper doc string
      * No need to use remove_unwanted_output method, as it only required if
        running fab commands as a comand, which is not going to happen with
        remote_cmd
    * Added method remote_copy which to be used to copy the
      files/directories remotely - it supports
      * copy files from remote node1 to remote node2
      * Copy files locally from src to dest path
      * copy files between VMs through gateway nodes (i.e compute nodes)
      * Copy from server to VM and vice versa
    
    Change-Id: Id0d675d820ed1c2616c701255946fbb0a59b2f4c

--- tcutils/fabutils.py
+++ tcutils/fabutils.py
@@ -1,4 +1,4 @@
-from fabric.operations import get, put, sudo
+from fabric.operations import get, put, sudo, local
 from fabric.api import run, env
 from fabric.exceptions import CommandTimeout, NetworkError
 from fabric.contrib.files import exists
@@ -8,16 +8,44 @@ from fabric.context_managers import settings, hide, cd
 import re
 import logging
 import time
+import os
+import tempfile
 
 log = logging.getLogger('log01')
 
 
 def remote_cmd(host_string, cmd, password=None, gateway=None,
                gateway_password=None, with_sudo=False, timeout=120,
-               as_daemon=False, raw=False, cwd=None, warn_only=True):
-    """ Run command on remote node through another node (gateway).
-        This is useful to run commands on VMs through compute node
+               as_daemon=False, raw=False, cwd=None, warn_only=True, tries=1):
+    """ Run command on remote node.
+    remote_cmd method to be used to run command on any remote nodes - whether it
+    is a remote server or VM or between VMs. This method has capability to
+    handle:
+
+    1. run remote command on remote server from test node
+    2. Run remote command on node-a through node-b from the test node
+      * in this case node-a is the target node, node-b is gateway, and the
+    nodes will be connect from testnode
+      * This is to avoid situation to login to remote node (node-b in this
+        case) and run script (fab script or pexpect or any such code) on
+        that remote node (node-b) against running command on target node
+        (node-a)
+    3. Run remote command on VM thorugh compute node - Same usecase as of #2
+    4. Run remote commands between VMs - say copy a file from vm1 to vm2
+    through compute node of vm1.
+      * This will use ssh-agent forward to avoid copying ssh private keys to
+        subsequent servers - Previously we used to copy ssh private keys to
+        compute node and then copy the same file to vm1 in order to be able
+        to connect from vm1 to vm2.
+      * The commands will be running sitting on the test node then run an
+      * "ssh/scp" command on vm1 through compute node of vm1 with
+        agent_forward on
+      * in this case flow is like this: test_node ->
+        compute_of_vm1(gateway - passthrough) -> vm1 (run ssh/scp there) ->
+        vm2 (final command is run)
+
     Args:
+        tries: Number of retries in case of failure
         host_string: host_string on which the command to run
         password: Password
         cmd: command
@@ -59,32 +87,18 @@ def remote_cmd(host_string, cmd, password=None, gateway=None,
             shell=shell,
             disable_known_hosts=True,
             abort_on_prompts=False):
-        env.forward_agent = True
-        gateway_hoststring = (gateway if re.match(r'\w+@[\d\.]+:\d+', gateway)
-                              else gateway + ':22')
-        node_hoststring = (host_string
-                           if re.match(r'\w+@[\d\.]+:\d+', host_string)
-                           else host_string + ':22')
-        if password:
-            env.passwords.update({node_hoststring: password})
-            # If gateway_password is not set, guess same password
-            # (if key is used, it will be tried before password)
-            if not gateway_password:
-                env.passwords.update({gateway_hoststring: password})
-
-        if gateway_password:
-            env.passwords.update({gateway_hoststring: gateway_password})
-            if not password:
-                env.passwords.update({node_hoststring: gateway_password})
+        update_env_passwords(host_string, password, gateway, gateway_password)
 
         log.debug(cmd)
-        tries = 1
         output = None
         while tries > 0:
             try:
                 output = _run(cmd, timeout=timeout, **kwargs)
-            except CommandTimeout:
+            except (CommandTimeout, NetworkError):
                 pass
+                tries -= 1
+                time.sleep(5)
+                continue
 
             if output and 'Fatal error' in output:
                 tries -= 1
@@ -93,19 +107,18 @@ def remote_cmd(host_string, cmd, password=None, gateway=None,
                 break
         # end while
 
-        if not raw:
-            real_output = remove_unwanted_output(output)
-        else:
+        if raw:
             real_output = output
+        else:
+            real_output = remove_unwanted_output(output)
+
         return real_output
 
 
 def remove_unwanted_output(text):
     """ Fab output usually has content like [ x.x.x.x ] out : <content>
-
     Args:
         text: Text to be parsed
-
     """
     if not text:
         return None
@@ -122,3 +135,127 @@ def remove_unwanted_output(text):
                 return_list1.append(line)
     real_output = '\n'.join(return_list1)
     return real_output
+
+
+def remote_copy(src, dest, src_password=None, src_gw=None, src_gw_password=None,
+                dest_password=None, dest_gw=None, dest_gw_password=None,
+                with_sudo=False, warn_only=True):
+    """ Copy files/folders to remote server or VM (in case of VM,
+        copy will happen through gateway node - i.e compute node)
+
+    Args:
+        src: source can be remote or local
+                in case of remote node, it should be in the form of
+                    node1:/tmp/source_directory
+                in case of local node, it can be just a file/directory path
+                    /tmp/source_file
+        dest: Can be remote or local
+                in case of remote node, it should be in the form of
+                    node1:/tmp/destination_directory
+                in case of local node, it can be just a file/directory path
+                    /tmp/
+        src_password: source node password if required
+        src_gw: host_string of the node through which source will be connecting
+        src_gw_password: src_gw password if required
+        dest_password: destination node password if required
+        dest_gw: host_string of the node through which destination will be connecting
+        dest_gw_password: src_gw password if required
+        with_sudo: use Sudo
+        warn_only: run fab with warn_only
+    """
+    fab_connections.clear()
+
+    # dest is local file path
+    if re.match(r"^[\t\s]*/", dest):
+        dest_node = None
+        dest_path = dest
+    # dest is remote path
+    elif re.match(r"^.*:", dest):
+        dest = re.split(':', dest)
+        dest_node = dest[0]
+        dest_path = dest[1]
+    else:
+        raise AttributeError("Invalid destination path - %s " % dest)
+
+    # src is local file path
+    if re.match(r"^[\t\s]*/", src):
+        if os.path.exists(src):
+            src_node = None
+            src_path = src
+        else:
+            raise IOError("Source not found - %s No such file or directory" % src)
+    # src is remote path
+    elif re.match(r"^.*:", src):
+        src = re.split(':', src)
+        src_node = src[0]
+        src_path = src[1]
+    else:
+        raise AttributeError("Invalid source path - %s" % src)
+
+    if src_node:
+        # Source is remote
+        with settings(host_string=src_node, gateway=src_gw,
+                      warn_only=warn_only, disable_known_hosts=True,
+                      abort_on_prompts=False):
+            update_env_passwords(src_node, src_password, src_gw, src_gw_password)
+            try:
+                if exists(src_path, use_sudo=with_sudo):
+                    if dest_node:
+                        # Both source and destination are remote
+                        local_dest = tempfile.mkdtemp()
+                        get(src_path, local_dest, use_sudo=True)
+                        src_path = os.path.join(local_dest, os.listdir(local_dest)[0])
+                    else:
+                        # Source is remote and destination is local
+                        # Copied to destination
+                        get(src_path, dest_path, use_sudo=True)
+                        return True
+                else:
+                    raise IOError("Source not found - %s No such file or directory" % src)
+            except NetworkError:
+                pass
+
+    if dest_node:
+        # Source is either local or remote
+        with settings(host_string=dest_node, gateway=dest_gw,
+                      warn_only=warn_only, disable_known_hosts=True,
+                      abort_on_prompts=False):
+            update_env_passwords(dest_node, dest_password, dest_gw, dest_gw_password)
+            try:
+                put(src_path, dest_path, use_sudo=True)
+                return True
+            except NetworkError:
+                pass
+    else:
+        # Both are local
+        local("cp -r %s %s" % (src_path, dest_path))
+        return True
+
+
+def update_env_passwords(host, password=None, gateway=None, gateway_password=None):
+    """ Update env_passwords for the hosts provided
+    Args:
+        host: host string
+        password: password
+        gateway: gateway host string
+        gateway_password: gateway password
+    """
+    env.forward_agent = True
+    gateway_hoststring = "fake_gateway"
+    if gateway:
+        gateway_hoststring = (gateway if re.match(r'\w+@[\d\.]+:\d+', gateway)
+                              else gateway + ':22')
+    node_hoststring = (host
+                       if re.match(r'\w+@[\d\.]+:\d+', host)
+                       else host + ':22')
+    if password:
+        env.passwords.update({node_hoststring: password})
+        # If gateway_password is not set, guess same password
+        # (if key is used, it will be tried before password)
+        if not gateway_password:
+            env.passwords.update({gateway_hoststring: password})
+
+    if gateway_password:
+        env.passwords.update({gateway_hoststring: gateway_password})
+        if not password:
+            env.passwords.update({node_hoststring: gateway_password})
