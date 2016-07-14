479e5dda491d45204ae80ed87ec0fd69f4947e04
Ankit Jain <ankitja@juniper.net>
2016-07-14 05:17:19 -0700

    Adding missing folder intf_mirror     Adding intf_mirror test folder from R3.0
    Change-Id: If8cd6d83ad2177c804f18b278c46d3ae26b3b8ba

---
+++ scripts/intf_mirror/base.py
@@ -0,0 +1,29 @@
+import test_v1
+from common.connections import ContrailConnections
+from common import isolated_creds
+
+class BaseIntfMirrorTest(test_v1.BaseTestCase_v1):
+
+    @classmethod
+    def setUpClass(cls):
+        super(BaseIntfMirrorTest, cls).setUpClass()
+        cls.orch = cls.connections.orch
+        cls.quantum_h= cls.connections.quantum_h
+        cls.nova_h = cls.connections.nova_h
+        cls.vnc_lib= cls.connections.vnc_lib
+        cls.agent_inspect= cls.connections.agent_inspect
+        cls.cn_inspect= cls.connections.cn_inspect
+        cls.analytics_obj=cls.connections.analytics_obj
+    #end setUpClass
+
+    @classmethod
+    def tearDownClass(cls):
+        super(BaseIntfMirrorTest, cls).tearDownClass()
+    #end tearDownClass
+
+    def remove_from_cleanups(self, fix):
+        for cleanup in self._cleanups:
+            if fix.cleanUp in cleanup:
+                self._cleanups.remove(cleanup)
+                break
+    #end remove_from_cleanups
