26439d65b080c496ca47e094c91149a41bcce312
Pulkit <pulkitt@juniper.net>
2016-08-12 16:35:47 +0530

    Adding a sleep of 3 seconds to give time for records to get updated in VMs
    This sleep is required now due to dev code changes of sending the
    update in every 3 seconds.
    
    Change-Id: I7bcb6ed4b7126efbd2612a47f49e656c6f9a648e

--- scripts/vdns/test_vdns.py
+++ scripts/vdns/test_vdns.py
@@ -177,8 +177,10 @@ class TestvDNS0(BasevDNSTest):
                         continue
                     else:
                         self.assertTrue(False, 'Failed to Modify TTL values')
+            sleep(3) # Record is taking around 3 seconds to get updated on the VM
             vm_fixture['vm1-test'].run_cmd_on_vm(cmds=[cmd])
             result = vm_fixture['vm1-test'].return_output_cmd_dict[cmd]
+            assert result, "Record not updated on VM "
             result = result.replace("\t", " ")
             #m_obj = re.search(r"rec1.juniper.net\.*\s*([0-9.]*)",result)
             m_obj = re.search(
