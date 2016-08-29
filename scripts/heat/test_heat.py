6a98e78ff196a682bbb4fcef5e3d91bd465bab7f
Senthilnathan Murugappan <msenthil@juniper.net>
2016-08-28 23:42:57 -0700

    Move all the externally shared testcases to contrail-test-ci repo
    Change-Id: I4fc1df56b503e7bd11e66dfbdf93ede40cdd40cf

--- scripts/heat/test_heat.py
+++ scripts/heat/test_heat.py
@@ -20,10 +20,11 @@ from netaddr import IPNetwork, IPAddress
 from common.ecmp.ecmp_traffic import ECMPTraffic
 from common.ecmp.ecmp_verify import ECMPVerify
 from common.servicechain.verify import VerifySvcChain
+from test_heat_basic import TestBasicHeat
 
 try:
     from heat_test import *
-    from base import BaseHeatTest
+    from common.heat.base import BaseHeatTest
 
     class TestHeat(BaseHeatTest, ECMPTraffic, ECMPVerify):
 
@@ -50,31 +51,6 @@ try:
             self.logger.info(
                 'The following are the stacks currently : %s' % stacks_list)
         # end test_heat_stacks_list
-
-        @test.attr(type=['sanity'])
-        @preposttest_wrapper
-        def test_svc_creation_with_heat(self):
-            '''
-            Validate creation of a in-network-nat service chain using heat
-            '''
-            vn_list = []
-            right_net_fix, r_hs_obj = self.config_vn(stack_name='right_net')
-            left_net_fix, l_h_obj = self.config_vn(stack_name='left_net')
-            vn_list = [left_net_fix, right_net_fix]
-            vms = []
-            vms = self.config_vms(vn_list)
-            svc_template = self.config_svc_template(stack_name='st', mode='in-network')
-            svc_instance, si_hs_obj = self.config_svc_instance(
-                'si', svc_template, vn_list)
-            si_fq_name = (':').join(svc_instance.si_fq_name)
-            svc_rules = []
-            svc_rules.append(self.config_svc_rule(si_fq_names=[si_fq_name], src_vns=[left_net_fix], dst_vns=[right_net_fix]))
-            if self.inputs.get_af() == 'v6':
-                svc_rules.append(self.config_svc_rule(proto='icmp6', si_fq_names=[si_fq_name], src_vns=[left_net_fix], dst_vns=[right_net_fix]))
-            svc_chain = self.config_svc_chain(svc_rules, vn_list, [l_h_obj, r_hs_obj])
-            time.sleep(10)
-            assert vms[0].ping_with_certainty(vms[1].vm_ip, expectation=True)
-        # end test_svc_creation_with_heat
 
         @test.attr(type=['sanity'])
         @preposttest_wrapper
