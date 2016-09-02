2be5a2ea1f4be6ca14b424f7e258f69d1bc7eb3a
Ankit Jain <ankitja@juniper.net>
2016-09-02 03:58:39 -0700

    Adding Svc V2 mirroring test cases
    Change-Id: I6563cd091701f29aaaf5a3508ac8db56c185ef65
    Depends-On: I3326f22ab0c3c87df9d0971e60a3e643573fdb88

--- scripts/svc_mirror/test_mirror.py
+++ scripts/svc_mirror/test_mirror.py
@@ -50,6 +50,44 @@ class TestSVCMirror(BaseMirrorTest, VerifySvcMirror):
         self.verify_svc_mirroring()
         return self.verify_add_new_vns(self.si_prefix)
 
+class TestSVCV2Mirror(BaseMirrorTest, VerifySvcMirror):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSVCV2Mirror, cls).setUpClass()
+
+    def runTest(self):
+        pass
+    # end runTest
+
+    @test.attr(type=['ci_sanity_WIP', 'sanity', 'quick_sanity'])
+    @preposttest_wrapper
+    def test_svc_v2_mirroring(self):
+        """Validate the service chain mirroring"""
+        return self.verify_svc_mirroring(ci=True, st_version=2)
+
+    @preposttest_wrapper
+    def test_in_network_svc_v2_mirroring(self):
+        """Validate the in network service chain mirroring"""
+        return self.verify_svc_mirroring(svc_mode='in-network', st_version=2)
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_with_2_analyzer(self):
+        """Validate the service chain mirroring with three analyzers"""
+        return self.verify_svc_mirroring(si_count=2, st_version=2)
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_policy_add_delete(self):
+        """Validate the service chain mirroring after delete recreate policy"""
+        self.verify_svc_mirroring(st_version=2)
+        return self.verify_policy_delete_add(self.si_prefix)
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_add_more_vns(self):
+        """Validate the service chain mirroring after adding rule to mirror traffic from aditional VNs"""
+        self.verify_svc_mirroring(st_version=2)
+        return self.verify_add_new_vns(self.si_prefix)
+
 
 class TestSVCMirrorFIP(BaseMirrorTest, VerifySvcMirror):
 
@@ -70,6 +108,26 @@ class TestSVCMirrorFIP(BaseMirrorTest, VerifySvcMirror):
     def test_svc_mirroring_with_floating_ip_with_2_analyzer(self):
         """Validate the service chain mirroring with floating IP with 2 analyzer"""
         return self.verify_svc_mirroring_with_floating_ip(si_count=2)
+
+class TestSVCV2MirrorFIP(BaseMirrorTest, VerifySvcMirror):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSVCV2MirrorFIP, cls).setUpClass()
+
+    def runTest(self):
+        pass
+    # end runTest
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_with_floating_ip(self):
+        """Validate the service chain mirroring with floating IP"""
+        return self.verify_svc_mirroring_with_floating_ip(st_version=2)
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_with_floating_ip_with_2_analyzer(self):
+        """Validate the service chain mirroring with floating IP with 2 analyzer"""
+        return self.verify_svc_mirroring_with_floating_ip(si_count=2, st_version=2)
 
 
 class TestSVCMirrorPolicy(BaseMirrorTest, VerifySvcMirror):
@@ -142,6 +200,77 @@ class TestSVCMirrorPolicy(BaseMirrorTest, VerifySvcMirror):
 
         return self.verify_policy_order_change()
 
+
+class TestSVCV2MirrorPolicy(BaseMirrorTest, VerifySvcMirror):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSVCV2MirrorPolicy, cls).setUpClass()
+
+    def runTest(self):
+        pass
+    # end runTest
+
+    @preposttest_wrapper
+    def test_svc_v2_mirror_with_deny_rule(self):
+        """Validate the service chain mirroring after adding rule to mirror traffic from aditional VNs"""
+        return self.verify_svc_mirror_with_deny(st_version=2)
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_with_unidirection_rule(self):
+        """Validate the service chain mirroring with allow traffic in unidirection rule"""
+        return self.verify_svc_mirroring_unidirection(st_version=2)
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring_with_unidirection_rule_with_2_analyzer(self):
+        """Validate the service chain mirroring with allow traffic in unidirection rule"""
+        return self.verify_svc_mirroring_unidirection(si_count=2, st_version=2)
+
+    @preposttest_wrapper
+    def test_attach_detach_policy_with_svc_v2_mirroring(self):
+        """Test case for bug 1533"""
+        return self.verify_attach_detach_policy_with_svc_mirroring(st_version=2)
+
+    @preposttest_wrapper
+    def test_detach_attach_diff_policy_with_svc_v2_mirroring(self):
+        """Test case for bug 2414"""
+        '''steps and checkpoints:
+        1.      attach pol1 to 2 n/w VN1 and VN2, routes get exchanged as expected
+        2.      Detach pol1 from VN1(only), routes are removed from both VNs.
+        3.      In VN2, detach pol1 and attach again. Routes are NOT exchanged.
+        4.      In VN2, detach pol1 and attach pol-analyzer. routes are exchanged.
+        5.      Now detach pol-analyzer from VN2, routes get deleted from both the n/ws
+        6.      again attach pol1 to VN2.  routes should not be exchanged between both the n/ws'''
+
+        return self.verify_detach_attach_diff_policy_with_mirroring(st_version=2)
+
+    @preposttest_wrapper
+    def test_detach_attach_policy_change_rules_with_svc_v2(self):
+        """Test case for bug 2414"""
+        '''steps and checkpoints:
+        1.      attach pol-analyzer to both n/w, routes get exchanged.
+        2.      Detach pol-analyzer from VN1(only), routes are NOT removed from both VNs.
+        3.      Change pol-analyzer rules to remove mirror, routes should be REMOVED from both n/ws
+        4.      In VN2, detach pol-analyzer.
+        5.      In VN2, attach pol-analyzer again. routes should NOT be exchanged.'''
+
+        return self.verify_detach_attach_policy_change_rules(st_version=2)
+
+    @preposttest_wrapper
+    def test_policy_order_change_with_svc_v2(self):
+        """Validate mirroring after policy order change."""
+        '''steps and checkpoints:
+        pol1  : pass protocol any network any port any <> network any port any
+        pol-analyzer: pass protocol any network vn1 port any <> network vn2 port any mirror_to default-domain:admin:si-2
+        analyzer: transparent, automatic VN
+        1. pol-analyzer is attached to both vn1 and vn2, traffic should be mirrored.
+        2. attach the policy in both VN in order as (pol1, pol-analyzer), traffic should not be mirrored.
+        3. change the order of policy in vn1 as (pol-analyzer, pol1), traffic should be mirrored
+        4. change the order of policy in vn2 as (pol-analyzer, pol1), traffic should be mirrored
+        5. now detach pol1 from both VN, traffic should be mirrored'''
+
+        return self.verify_policy_order_change(st_version=2)
+
 class TestSVCMirrorIPv6(TestSVCMirror):
 
     @classmethod
@@ -153,6 +282,19 @@ class TestSVCMirrorIPv6(TestSVCMirror):
     def test_svc_mirroring(self):
         """Validate the service chain mirroring"""
         return self.verify_svc_mirroring()
+
+class TestSVCV2MirrorIPv6(TestSVCMirror):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSVCV2MirrorIPv6, cls).setUpClass()
+        cls.inputs.set_af('v6')
+
+    @preposttest_wrapper
+    def test_svc_v2_mirroring(self):
+        """Validate the service chain mirroring"""
+        return self.verify_svc_mirroring(st_version=2)
+
 
 class TestSVCMirrorPolicyIPv6(TestSVCMirrorPolicy):
 
