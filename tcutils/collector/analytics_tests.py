6bb3377578a752ae88c6859ca663250ad5442d05
Sandip Dey <sandipd@juniper.net>
2016-09-15 03:50:43 -0700

    Passing auth headers when querying analytics
    Change-Id: Ide86fc2d68c785e3f2fd51f30a8910e26d73a861

--- tcutils/collector/analytics_tests.py
+++ tcutils/collector/analytics_tests.py
@@ -3216,11 +3216,19 @@ class AnalyticsVerification(fixtures.Fixture):
     def search_links(self, link, selected_uve=''):
 #
         result = True
+        _headers = {
+        'Content-type': 'application/json; charset="UTF-8"',
+                   }
+        self.ops_inspect[self.inputs.collector_ips[0]]._drv._auth()
+        _auth = self.ops_inspect[self.inputs.collector_ips[0]]._drv._headers
+        _headers['X-AUTH-TOKEN'] = _auth['X-AUTH-TOKEN']
+
         links = self.parse_links(link)
         dct = {}
         for ln in links:
             try:
-                response = urllib2.urlopen(str(ln))
+                req = urllib2.Request(ln, headers=_headers)
+                response = urllib2.urlopen(req)
                 data = json.load(response)
                 if selected_uve:
                     if selected_uve in ln:
