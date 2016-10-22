52a722ac251f97e4205fb4d0b254b58bbf2fce55
skiranh <skiranh@juniper.net>
2016-10-22 22:54:25 +0530

    Adding static table (interface and network) tests
    Change-Id: If81139d73dcb5a0fa0aeb2aacc7ec0f4e38bfce6
    Closes-Bug: #1628454

---
+++ scripts/static_route_table/test_static_tables.py
@@ -0,0 +1,91 @@
+import sys
+import os
+import fixtures
+import testtools
+import unittest
+from vn_test import *
+from tcutils.wrappers import preposttest_wrapper
+from base import StaticRouteTableBase 
+from common import isolated_creds
+import test 
+from common.servicechain.firewall.verify import VerifySvcFirewall
+
+class TestStaticRouteTables(StaticRouteTableBase, VerifySvcFirewall):
+    
+    @classmethod
+    def setUpClass(cls):
+        super(TestStaticRouteTables, cls).setUpClass()
+
+    def setUp(self):
+        super(TestStaticRouteTables, self).setUp()
+        self.config_basic()
+ 
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_interface_static_table(self):
+
+        self.add_interface_route_table(self.vn1_fixture, self.vn2_fixture, self.vm1_fixture)
+        sport = 8001
+        dport = 9001
+
+        self.verify_traffic(
+            self.left_vm_fixture, self.right_vm_fixture, 'udp', sport=sport, dport=dport)
+
+        self.check_route_in_agent(expected_next_hops = 1)
+
+    #end test_static_table
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_iter_interface_static_table(self):
+
+        for i in range(1,5):
+            self.add_interface_route_table(self.vn1_fixture, self.vn2_fixture, self.vm1_fixture)
+            sport = 8001
+            dport = 9001
+            self.unbind_interface_table(self.vn1_fixture, self.vn2_fixture, self.vm1_fixture)
+            self.bind_interface_table(self.vn1_fixture, self.vn2_fixture, self.vm1_fixture)
+            self.verify_traffic(
+                self.left_vm_fixture, self.right_vm_fixture, 'udp', sport=sport, dport=dport)
+
+            self.check_route_in_agent(expected_next_hops = 1)
+
+    #end test_add_delete_static_table
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_iter_network_static_table(self):
+
+        for i in range(1,5):
+            self.add_network_table_to_vn(self.vn1_fixture, self.vn2_fixture)
+            sport = 8001
+            dport = 9001
+            self.unbind_network_table(self.vn1_fixture, self.vn2_fixture)
+            self.bind_network_table(self.vn1_fixture, self.vn2_fixture)
+            self.verify_traffic(
+                self.left_vm_fixture, self.right_vm_fixture, 'udp', sport=sport, dport=dport)
+
+            self.check_route_in_agent(expected_next_hops = 1)
+
+    #end test_add_delete_static_table
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_network_route_table(self):
+
+        sport = 8001
+        dport = 9001
+
+        self.add_network_table_to_vn(self.vn1_fixture, self.vn2_fixture)
+
+        self.verify_traffic(
+            self.left_vm_fixture, self.right_vm_fixture, 'udp', sport=sport, dport=dport)
+        self.check_route_in_agent(expected_next_hops = 1)
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_with_neutron_router(self):
+        self.test_interop_with_neutron_router()
+
+    # end test_network_table
+
