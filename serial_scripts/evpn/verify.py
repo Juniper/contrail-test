e54d4a0d0fb7e7b7e1e7a971f5c248e369181d6d
Vedu Joshi <vjoshi@juniper.net>
2016-10-28 13:57:39 +0530

    Update tests not to use few ubuntu images
    There is no need to use ubuntu-dhcpdns-server and ubuntu-with-vlan8021q
    images
    
    Change-Id: Iae6dd964685b781358637465a3028c538ffb0974

--- serial_scripts/evpn/verify.py
+++ serial_scripts/evpn/verify.py
@@ -66,11 +66,10 @@ class VerifyEvpnCases():
             VMFixture(
                 project_name=self.inputs.project_name,
                 connections=self.connections,
-                flavor='contrail_flavor_large',
                 vn_objs=[
                     vn3_fixture.obj,
                     vn4_fixture.obj],
-                image_name='ubuntu-dhcpdns-server',
+                image_name='ubuntu-dns-server',
                 vm_name=vm1_name,
                 node_name=compute_2))
 
@@ -79,7 +78,6 @@ class VerifyEvpnCases():
             VMFixture(
                 project_name=self.inputs.project_name,
                 connections=self.connections,
-                flavor='contrail_flavor_large',
                 vn_objs=[
                     vn3_fixture.obj,
                     vn4_fixture.obj],
@@ -1525,22 +1523,20 @@ class VerifyEvpnCases():
             VMFixture(
                 project_name=self.inputs.project_name,
                 connections=self.connections,
-                flavor='contrail_flavor_large',
                 vn_objs=[
                     vn3_fixture.obj,
                     vn4_fixture.obj],
-                image_name='ubuntu-with-vlan8021q',
+                image_name='ubuntu-traffic',
                 vm_name=vn_l2_vm1_name,
                 node_name=compute_1))
         vn_l2_vm2_fixture = self.useFixture(
             VMFixture(
                 project_name=self.inputs.project_name,
                 connections=self.connections,
-                flavor='contrail_flavor_large',
                 vn_objs=[
                     vn3_fixture.obj,
                     vn4_fixture.obj],
-                image_name='ubuntu-with-vlan8021q',
+                image_name='ubuntu-traffic',
                 vm_name=vn_l2_vm2_name,
                 node_name=compute_2))
 
@@ -1687,11 +1683,10 @@ class VerifyEvpnCases():
             VMFixture(
                 project_name=self.inputs.project_name,
                 connections=self.connections,
-                flavor='contrail_flavor_large',
                 vn_objs=[
                     vn3_fixture.obj,
                     vn4_fixture.obj],
-                image_name='ubuntu-with-vlan8021q',
+                image_name='ubuntu-traffic',
                 vm_name=vn_l2_vm1_name,
                 node_name=compute_1))
         vn_l2_vm2_fixture = self.useFixture(
@@ -1702,7 +1697,7 @@ class VerifyEvpnCases():
                 vn_objs=[
                     vn3_fixture.obj,
                     vn4_fixture.obj],
-                image_name='ubuntu-with-vlan8021q',
+                image_name='ubuntu-traffic',
                 vm_name=vn_l2_vm2_name,
                 node_name=compute_2))
 
