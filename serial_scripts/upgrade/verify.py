00f88e905a9c2cbba7ce9538ad8d998fc849ac6c
Vedu Joshi <vjoshi@juniper.net>
2016-11-03 00:55:44 +0530

    Adapt to ECMP test infra changes
    * Changes to testcases as per review
      https://review.opencontrail.org/25611
      a) pass vns/vms as args instead of making them instance vars
      b) Derive tests from ECMPTestBase in common/base.py
    * Remove stale file scripts/ecmp/sanity_with_setup.py
    
    Change-Id: Ifddabcf00694def8353166c73f3098be7e0249e9

--- serial_scripts/upgrade/verify.py
+++ serial_scripts/upgrade/verify.py
@@ -157,7 +157,9 @@ class BaseResource(fixtures.Fixture, ConfigSvcChain, VerifySvcChain, BaseTestLba
         self.vn1_fixture = self.config_vn(self.vn1_name, self.vn1_subnets)
         self.vn2_fixture = self.config_vn(self.vn2_name, self.vn2_subnets)
         self.st_fixture, self.si_fixtures = self.config_st_si(
-            self.st_name, si_prefix, si_count, svc_scaling, max_inst, project=self.inputs.project_name, left_vn=self.vn1_fq_name, right_vn=self.vn2_fq_name, svc_mode=svc_mode, flavor=flavor)
+            self.st_name, si_prefix, si_count, svc_scaling, max_inst,
+            project=self.inputs.project_name, left_vn_fixture=self.vn1_fixture,
+            right_vn_fixture=self.vn2_fixture, svc_mode=svc_mode, flavor=flavor)
         self.action_list = self.chain_si(si_count, si_prefix, self.inputs.project_name)
         self.rules = [
             {
