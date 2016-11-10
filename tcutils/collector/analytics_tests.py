41c9975264bdc50f6afecdb314bd295f74936a26
Ankit Jain <ankitja@juniper.net>
2016-11-10 04:38:15 -0800

    Script fix for alarm test case on multi cfgm single analytics setup <-Cherry-pick from master
        Lines below were casuing issue, fixing it in this commit, use collector[1] only on multi analytics setup:
        if multi_instances:
            collector_ip = self.inputs.collector_ips[1]
    
    Change-Id: I02c5ad54b96b8ad3421e040e308a709b609c86ca

--- tcutils/collector/analytics_tests.py
+++ tcutils/collector/analytics_tests.py
@@ -2799,7 +2799,7 @@ class AnalyticsVerification(fixtures.Fixture):
         if service in supervisors:
             alarm_type = ['node-status']
         collector_ip = self.inputs.collector_ips[0]
-        if multi_instances:
+        if multi_instances and role == 'analytics-node':
             collector_ip = self.inputs.collector_ips[1]
         hostname = socket.gethostbyaddr(service_ip)[0].split('.')[0]
         if not isinstance(alarm_type, list):
