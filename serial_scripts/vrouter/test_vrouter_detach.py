896bc35819abda2277df11364cb911dd435d7565
Aswani Kumar Reddy G <aswanikr@juniper.net>
2016-11-18 17:22:04 +0530

    Added vrouter testcases
    1) Porting xconnect case from vrouter_tests
    2) Added testcase for unload and reload vrouter kernal module
    
    Change-Id: I9ecf08680c74b38440ca66056c4ef4504586d707

---
+++ serial_scripts/vrouter/test_vrouter_detach.py
@@ -0,0 +1,96 @@
+from tcutils.commands import *
+from tcutils.util import retry
+from tcutils.wrappers import preposttest_wrapper
+from common.vrouter.base import BaseVrouterTest
+from tcutils.contrail_status_check import ContrailStatusChecker
+from common.neutron.base import *
+import sys
+
+class TestVrouterDetach(BaseNeutronTest):
+    
+    @classmethod
+    def setUpClass(cls):
+        super(TestVrouterDetach, cls).setUpClass()
+    #end setUpClass
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestVrouterDetach, cls).tearDownClass()
+    #end tearDownClass
+
+    @preposttest_wrapper
+    def test_vrouter_xconnect(self):
+        '''Test vrouter cross connect mode by taking vrouter agent down
+        1. get compute node ip
+        2. stop vrouter_agent
+        3. Try to ssh to compute node from cfgm
+        4. Verify Xconnect mode
+        5. start vrouter-agent'''
+        result = True
+        cmd_stop = 'service contrail-vrouter-agent stop'
+        cmd_start = 'service contrail-vrouter-agent start'
+        verify_Xconnect ="vif --list | grep Flags:X"
+        compute_ip=self.inputs.compute_ips[0]
+        self.inputs.run_cmd_on_server(compute_ip,cmd_stop)
+        self.logger.info('Verify Xconnect mode ')
+        output=self.inputs.run_cmd_on_server(compute_ip,issue_cmd=verify_Xconnect)
+        if not output:
+            result = result and False
+        else:
+            self.logger.info('Xconnect mode got enabled')
+        self.inputs.run_cmd_on_server(compute_ip,issue_cmd=cmd_start)
+        status = ContrailStatusChecker(self.inputs)
+        status.wait_till_contrail_cluster_stable([compute_ip])
+        assert result,'Xconnect mode not enabled'
+    #end test_vrouter_xconnect
+    
+    @preposttest_wrapper
+    def test_vrouter_kernal_module_unload_reload(self):
+        '''
+        1. create Vn and two vms
+        2. Verify ping between vms ping should pass
+        3. Stop Vrouter services and Unload Vrouter Kernal Module and verify status
+        4. Reload Vrouter Module and start Vrouter Services
+        5. Verify ping between vms ping should pass
+        '''
+        compute_ip =self.inputs.compute_ips[0]
+        compute_control_ip = self.inputs.compute_control_ips[0]
+        if compute_ip == compute_control_ip:
+            self.logger.info('Skipping test_case need multi_interface testbed')
+            return True
+        result = True
+        cmd_vr_stop = 'service supervisor-vrouter stop'
+        cmd_vr_start = 'service supervisor-vrouter start'
+        cmd_vr_unload = 'modprobe -r vrouter'
+        cmd_vr_reload = 'modprobe -a vrouter'
+        vn1_fixture = self.create_vn()
+        vm1_fixture = self.create_vm(vn1_fixture,
+                                              image_name='cirros-0.3.0-x86_64-uec')
+        vm2_fixture = self.create_vm(vn1_fixture,
+                                              image_name='cirros-0.3.0-x86_64-uec')
+        vm1_fixture.wait_till_vm_is_up()
+        vm2_fixture.wait_till_vm_is_up()
+        compute_ip = vm1_fixture.vm_node_ip
+        assert vm1_fixture.ping_with_certainty(vm2_fixture.vm_ip)
+        self.inputs.run_cmd_on_server(compute_ip,issue_cmd=cmd_vr_stop)
+        self.inputs.run_cmd_on_server(compute_ip,issue_cmd=cmd_vr_unload)
+        status = self.inputs.run_cmd_on_server(compute_ip,issue_cmd = 'lsmod | grep vrouter')
+        if status:
+            result = result and False
+            self.logger.info('Vrouter kernel module failed to unload')
+        else:
+            self.logger.info('Vrouter kernel module unloaded successfully')
+        self.logger.info('Reloading vrouter kernel module')
+        self.inputs.run_cmd_on_server(compute_ip,issue_cmd=cmd_vr_reload)
+        status = self.inputs.run_cmd_on_server(compute_ip,issue_cmd = 'lsmod | grep vrouter')
+        if not status:
+            result = result and False
+            self.logger.error('Vrouter kernel module failed to reload')
+        else:
+            self.logger.info('Vrouter kernel module reloaded successfully')
+        self.inputs.run_cmd_on_server(compute_ip,issue_cmd=cmd_vr_start)
+        status = ContrailStatusChecker(self.inputs)
+        status.wait_till_contrail_cluster_stable()
+        assert result,'Vrouter kernel module failed to unload and reload'
+        assert vm1_fixture.ping_with_certainty(vm2_fixture.vm_ip)
+    #end  test_vrouter_kernal_module_unload_reload
