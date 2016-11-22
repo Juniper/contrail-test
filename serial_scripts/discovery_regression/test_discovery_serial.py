d353a646583c0ce00fe44b3228dffb12c8d1d561
Vinoth Ganapathy <vganapathy@juniper.net>
2016-11-21 18:21:48 -0800

    Heat testcases to support tiny core image for SVM and other fixes
    Change-Id: I32b352e767af64fccde4dd396bea1e9c58a1a39f
    
    issue #1
      need to add static route even in the case innat for the left chain alone
    
    issue #2
       test_src_cidr_svc_creation_with_heat only support v4 since the template
       (src_cidr_svc) written that way
    
    Change-Id: I00d01c9291875c49f342f48db7ffe580410eb602

--- serial_scripts/discovery_regression/test_discovery_serial.py
+++ serial_scripts/discovery_regression/test_discovery_serial.py
@@ -46,7 +46,8 @@ class TestDiscoverySerial(base.BaseDiscoveryTest):
             ip = elem[0]
             self.logger.info("Stopping service %s.." % (elem,))
             self.inputs.stop_service('contrail-control', [ip])
-
+        # Setting the sleep time to be hc_miss of discover.conf + 5 secs buffer
+        time.sleep(40)        
         for elem in svc_lst:
             ip = elem[0]
             if self.ds_obj.get_service_status(self.inputs.cfgm_ip, elem, 'down', 8):
