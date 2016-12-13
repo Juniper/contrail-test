30dcc5ab0b5d696bfafef69465dee2b523ec23e8
ankitja <ankitja@juniper.net>
2016-12-13 04:34:46 -0800

    Adding test case to verify drop count in dropstats/vif dropstats
       Adding test case to verify drop count when pkts are dropped:
          1) Flow action drop stats count gets incremented, get it from the introspect for verification
          2) Flow action drop stats count gets incremented, get it from the introspect for verification
    
    Change-Id: I5aa731fc9517caad6fdb20e75bf0fc9293e17f28

---
+++ scripts/vrouter/test_drop_stats.py
@@ -0,0 +1,38 @@
+import os
+from tcutils.wrappers import preposttest_wrapper
+from common.vrouter.base import BaseVrouterTest
+from common.vrouter.base_drop_stats import BaseDropStats
+from tcutils.util import *
+from netaddr import IPNetwork, IPAddress
+import test
+
+class TestDropStats(BaseVrouterTest, BaseDropStats):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestDropStats, cls).setUpClass()
+        cls.agent_inspect_h = cls.connections.agent_inspect
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestDropStats, cls).tearDownClass()
+
+    @test.attr(type=['sanity', 'vcenter'])
+    @preposttest_wrapper
+    def test_flow_action_drop_stats(self):
+        """
+        Description: Verify flow action drop stats
+        Steps:
+            1. Create 2 VNs and launch 1 VM in each VN, on different nodes on multi node setup
+            2. Add policy to deny pkts between VNs.
+            3. Ping VM2 from VM1
+        Pass criteria:
+            1. Verify flow action count gets incremented, get it from introspect
+            2. Verify flow action count gets incremented for the vmi of VM1, get it from introspect
+              
+        """
+        assert self.verify_flow_action_drop_stats()
+        return True
+    # end test_drop_stats
+
+
