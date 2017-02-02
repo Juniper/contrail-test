c85c6cc9174b99db609f14a64fb4164ae4b1da54
ankitja <ankitja@juniper.net>
2017-02-02 12:52:49 -0800

    Adding test case to verify contrail-flows cli cmd with various optional args
        Test to verify contrail-flows cli cmd with various optional arguments is not broken
    
    Change-Id: I61c23a74f7edb9ccd148e6fe2141dbf8b0ef87e2

--- serial_scripts/analytics/base.py
+++ serial_scripts/analytics/base.py
@@ -39,6 +39,33 @@ class AnalyticsBaseTest(test_v1.BaseTestCase_v1):
                 break
     #end remove_from_cleanups
 
+    def _form_cmd(self, cmd_type, cmd_args):
+        cmd = cmd_type
+        for k,v in cmd_args.iteritems():
+            if k == 'no_key':
+                for elem in v:
+                    cmd = cmd + ' --' +  elem
+            else:
+                cmd = cmd + ' --' + k + ' ' + v
+        return cmd
+    # _form_cmd
+
+    def execute_cli_cmd(self, cmd, check_output=False):
+        result = True
+        analytics = self.res.inputs.collector_ips[0]
+        output = self.res.inputs.run_cmd_on_server(analytics, cmd)
+        self.logger.info("Output: %s \n" % output)
+        if output.failed:
+            self.logger.error('%s command failed..' % cmd)
+            result = result and False
+        if check_output:
+            output_str = str(output)
+            if not output_str:
+                self.logger.error("Output is empty")
+                result = result and False
+        return result
+    # end execute_cli_cmd
+
 class ResourceFactory:
     factories = {}
     def createResource(id):
