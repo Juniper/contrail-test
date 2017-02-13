e5e853deee20ab8f655feaa379e18bc3b041d4d0
usha <musharani@juniper.net>
2017-02-13 16:09:50 +0530

    Added 2 testcases for ServiceApplianceSet and ServiceAppliances creation
    Change-Id: I352da258c79d7a103cf5e0b9f3b3ced4eb71b47a

--- serial_scripts/webui/webui_common.py
+++ serial_scripts/webui/webui_common.py
@@ -302,6 +302,8 @@ class WebuiCommon:
                     self.select_dns_server(prj_name)
                 elif element_type == 'Interface':
                     self.select_project(prj_name, proj_type='prouter')
+                elif element_type == 'Service Appliance':
+                    self.select_project(prj_name, proj_type='service appliance set')
                 elif not element_type in ['DNS Server']:
                     self.select_project(prj_name)
             self.logger.info("Creating %s %s using contrail-webui" %
@@ -833,11 +835,9 @@ class WebuiCommon:
         return True
     # end click_if_element_found
 
-    def select_project(self, project_name='admin', proj_type='project'):
-        if proj_type == 'project':
-            proj_crumb = 's2id_projects\-breadcrumb\-dropdown'
-        elif proj_type == 'prouter':
-            proj_crumb = 's2id_prouter\-breadcrumb\-dropdown'
+    def select_project(self, project_name='admin', proj_type='projects'):
+        proj_type = proj_type.replace(' ', '\-')
+        proj_crumb = 's2id_' + proj_type + '\-breadcrumb\-dropdown'
         current_project = self.find_element(
             [proj_crumb, 'span'], ['id', 'tag']).text
         if not current_project == project_name:
@@ -1342,6 +1342,14 @@ class WebuiCommon:
                 result = result and False
             element_id = 'linkvRouterDelete'
             popup_id = 'configure-config_vrouterbtn1'
+        elif element_type == 'svc_appliance_delete':
+            element_id = 'btnActionDelSecGrp'
+            popup_id = 'configure-svcAppliancebtn1'
+        elif element_type == 'svc_appliance_set_delete':
+            if not self.click_configure_svc_appliance_set():
+                result = result and False
+            element_id = 'btnActionDelSecGrp'
+            popup_id = 'configure-svcApplianceSetbtn1'
         rows = self.get_rows(canvas=True)
         ln = 0
         if rows:
@@ -1387,6 +1395,15 @@ class WebuiCommon:
                         continue
                     else:
                         element_name = element_text
+                elif element_type == 'svc_appliance_set_delete':
+                    element_text = self.find_element('div', 'tag', elements=True,
+                                       browser=element)[2].text
+                    div_obj = self.find_element('div', 'tag', elements=True,
+                                  browser=element)[1]
+                    if element_text == 'opencontrail' or element_text == 'native':
+                        continue
+                    else:
+                        element_name = element_text
                 else:
                     element_text = element.find_elements_by_tag_name(
                         'div')[2].text
@@ -1396,7 +1413,8 @@ class WebuiCommon:
                             version = re.match('\S+(\s.*)', element_text)
                             element_name+= version.group(1)
                             ver_flag = True
-
+                    if element_type == 'svc_appliance_delete':
+                        element_name = element_text
                 if (element_text == element_name):
                     div_obj.find_element_by_tag_name('input').click()
                     if_select = True
@@ -1710,6 +1728,22 @@ class WebuiCommon:
         self.wait_till_ajax_done(self.browser, wait=2)
         return self.check_error_msg("configure virtual routers")
     # end click_configure_vrouter
+
+    def click_configure_svc_appliance_set(self):
+        self.wait_till_ajax_done(self.browser)
+        self._click_on_config_dropdown(self.browser, 0)
+        self.click_element(['config_infra_sapset', 'a'], ['id', 'tag'])
+        self.wait_till_ajax_done(self.browser, wait=2)
+        return self.check_error_msg("configure Service Appliance Set")
+    # end click_configure_svc_appliance_set
+
+    def click_configure_svc_appliances(self):
+        self.wait_till_ajax_done(self.browser)
+        self._click_on_config_dropdown(self.browser, 0)
+        self.click_element(['config_infra_sap', 'a'], ['id', 'tag'])
+        self.wait_till_ajax_done(self.browser, wait=2)
+        return self.check_error_msg("configure Service Appliance Set")
+    # end click_configure_svc_appliances
 
     def _click_on_config_dropdown(self, br, index=2):
         # index = 3 if svc_instance or svc_template
