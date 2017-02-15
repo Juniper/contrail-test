6974af5c91aa027214d696889e061302ebd5dcf9
usha <musharani@juniper.net>
2017-02-15 13:24:24 +0530

    Added new testcase for Virtual Router creation
    Change-Id: I49d88bdd0e9a936c53ab413f46b3c0b6e3f9163f

--- serial_scripts/webui/webui_common.py
+++ serial_scripts/webui/webui_common.py
@@ -1214,6 +1214,15 @@ class WebuiCommon:
         return self.check_error_msg("configure service instances")
     # end click_configure_service_instance_in_webui
 
+    def find_div_element_by_tag(self, index, browser, return_type='text'):
+        element = self.find_element('div', 'tag', elements=True,
+                                       browser=browser)[index]
+        if return_type == 'text':
+            return element.text
+        else:
+            return element
+    # end find_div_element_by_tag
+
     def delete_element(self, fixture=None, element_type=None):
         result = True
         delete_success = None
@@ -1337,6 +1346,11 @@ class WebuiCommon:
                 result = result and False
             element_id = 'btnActionDelLLS'
             popup_id = 'configure-link_local_servicesbtn1'
+        elif element_type == 'vrouter_delete':
+            if not self.click_configure_vrouter():
+                result = result and False
+            element_id = 'linkvRouterDelete'
+            popup_id = 'configure-config_vrouterbtn1'
         rows = self.get_rows(canvas=True)
         ln = 0
         if rows:
@@ -1372,9 +1386,8 @@ class WebuiCommon:
                     else:
                         element_name = element_text
                 else:
-                    element_text = element.find_elements_by_tag_name(
-                        'div')[2].text
-                    div_obj = element.find_elements_by_tag_name('div')[1]
+                    element_text = self.find_div_element_by_tag(2, element)
+                    div_obj = self.find_div_element_by_tag(1, element, return_type='obj')
                     if not ver_flag:
                         if element_type == 'svc_template_delete':
                             version = re.match('\S+(\s.*)', element_text)
@@ -1686,6 +1699,22 @@ class WebuiCommon:
         self.wait_till_ajax_done(self.browser, wait=2)
         return self.check_error_msg("configure link local services")
     # end click_configure_link_local_service
+
+    def click_configure_vrouter(self):
+        return self.click_menu_element(0, 'config_infra_vrouters',
+                                  msg="configure virtual routers")
+    # end click_configure_vrouter
+
+    def click_menu_element(self, index, element, msg=None, wait=2):
+        self.wait_till_ajax_done(self.browser)
+        self._click_on_config_dropdown(self.browser, index)
+        self.click_element([element, 'a'], ['id', 'tag'])
+        self.wait_till_ajax_done(self.browser, wait)
+        if msg:
+            return self.check_error_msg(msg)
+        else:
+            return True
+    # end click_menu_element
 
     def _click_on_config_dropdown(self, br, index=2):
         # index = 3 if svc_instance or svc_template
