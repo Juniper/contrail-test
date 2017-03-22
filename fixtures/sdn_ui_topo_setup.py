0bff2f51ed6fbd804da6759ef36b2f870e8cf313
Pavana <pavanap@juniper.net>
2017-03-22 16:08:27 +0530

    Adding new test case support for Routing Policy on UI
    Change-Id: I1c33fa2ab08524860e2a5b47f5c187bbd29567f6

--- fixtures/sdn_ui_topo_setup.py
+++ fixtures/sdn_ui_topo_setup.py
@@ -265,6 +265,11 @@ class sdnUiTopoSetupFixture(fixtures.Fixture):
         return True
     # end attach_network_route_table
 
+    def create_routing_policies(self):
+        assert ui_topo_steps.createRoutingPolicy(self)
+        return True
+    # end create_routing_policies
+
     def cleanUp(self):
         if self.inputs.fixture_cleanup == 'yes':
             super(sdnUiTopoSetupFixture, self).cleanUp()
