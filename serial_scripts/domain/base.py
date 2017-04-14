660822e0cc51576e9c92287fa6213818cd51e048
Aswani Kumar Reddy G <aswanikr@juniper.net>
2017-04-14 15:58:00 +0530

    Added some more cases to keystone_v3 testsuite
    Change-Id: If11f6ca3e728fdd94b688d79824d00ac2153a6c0

--- serial_scripts/domain/base.py
+++ serial_scripts/domain/base.py
@@ -1,6 +1,8 @@
 import test_v1
 from common import isolated_creds
-
+from project_test import *
+import domain_test
+from vn_test import *
 
 class BaseDomainTest(test_v1.BaseTestCase_v1):
 
@@ -22,3 +24,42 @@ class BaseDomainTest(test_v1.BaseTestCase_v1):
         super(BaseDomainTest, cls).tearDownClass()
     # end tearDownClass
 # end BaseDomainTest class
+
+    def create_project(self,domain_name,project_name,username, password):
+        
+        return self.useFixture(ProjectFixture(
+            domain_name=domain_name,
+            project_name=project_name,
+            auth=self.admin_connections.auth,
+            username=username,
+            password=password,
+            connections=self.admin_connections))
+
+    def create_domain(self,domain_name=None,connections=None):
+        try:
+            domain = domain_test.DomainFixture(connections=connections or self.admin_connections ,
+                                                   domain_name=domain_name)
+            domain.setUp()
+            return domain
+        except Exception as e:
+                self.logger.exception("Exception while creating domain")
+
+    def create_vn(self,project_fix,connections,vn_name,subnet):
+        return self.useFixture(
+            VNFixture(
+                project_name=project_fix.project_name,
+                connections=connections,
+                vn_name=vn_name,
+                subnet=subnet,
+                inputs=self.inputs))
+
+    def read_vn(self, connections, uuid):
+        try:
+            obj = connections.orch.get_vn_obj_from_id(uuid)
+        except PermissionDenied:
+            obj = None
+        if obj:
+            self.logger.info('Read VN %s'%uuid)
+        else:
+            self.logger.info('Permission Denied to read VN %s'%uuid)
+        return obj
