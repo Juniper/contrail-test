d3f8c4d5bf5067c996620a62d78129f2d4a1edd5
usha <musharani@juniper.net>
2017-08-16 11:35:00 +0530

    Fixing vrouter, service_instance, interface verification testcases on Monitor page
    Change-Id: I00240445c7f8674e53126b8d4d14cf9728693702

--- serial_scripts/webui/webui_common.py
+++ serial_scripts/webui/webui_common.py
@@ -1361,6 +1361,7 @@ class WebuiCommon:
         self.click_element('Service Instances', 'link_text')
         self.check_error_msg("configure service instance")
         count = 0
+        self.select_project(self.inputs.project_name)
         rows = self.get_rows(canvas=True)
         while True:
             if count > 120:
@@ -2010,7 +2011,7 @@ class WebuiCommon:
     def click_configure_project_quotas(self):
         self._click_on_config_dropdown(self.browser, index=0)
         self.click_element(
-            ['config_net_quotas', 'Project Quotas'], ['id', 'link_text'])
+            ['config_net_quotas', 'Project Settings'], ['id', 'link_text'])
         self.wait_till_ajax_done(self.browser)
         return self.check_error_msg("configure project quotas")
     # end click_configure_project_quota
