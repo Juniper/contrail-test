dc9873371622c1fd051113612996b9f1bb6d7e8b
Ganesha HV <ganeshahv@juniper.net>
2017-08-21 14:26:59 +0530

    Adding Service Chaining tests to ci_sanity.
    Will add only in-network service chain case in the first attempt.
    
    Need to monitor the review for a cpouple of runs before
    merging.
    Change-Id: Icc0bad59a79caf9a3441bb3816ec20c8c942589a

--- scripts/svc_firewall/test_svc_fw.py
+++ scripts/svc_firewall/test_svc_fw.py
@@ -30,10 +30,10 @@ class TestSvcRegr(BaseSvc_FwTest, VerifySvcFirewall, ECMPVerify):
         pass
     # end runTest
 
-    @test.attr(type=['vcenter'])
+    @test.attr(type=['ci_sanity', 'vcenter'])
     @preposttest_wrapper
     def test_svc_in_network_datapath(self):
-        return self.verify_svc_chain(service_mode='in-network',
+        return self.verify_svc_chain(svc_img_name='cirros_in_net', service_mode='in-network',
                                      create_svms=True)
 
 
