d1e01fb337c8464ab980bdf6c4d29ffe96db21d7
Chhandak Mukherjee <chhandak@juniper.net>
2017-08-25 17:26:02 -0700

    Removing test test_verify_flow_tables from CB Sanity
    Change-Id: I3e285390c643a68d7556050b731ab2e7fdf612be

--- serial_scripts/analytics/test_analytics_resource.py
+++ serial_scripts/analytics/test_analytics_resource.py
@@ -554,7 +554,7 @@ class AnalyticsTestSanityWithResource(
                     assert result
         return True
 
-    @test.attr(type=['cb_sanity', 'sanity', 'vcenter'])
+    @test.attr(type=['sanity', 'vcenter'])
     @preposttest_wrapper
     def test_verify_flow_tables(self):
         '''
