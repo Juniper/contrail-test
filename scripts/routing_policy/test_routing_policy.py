9d30a7947f7cc4ec291821e0a6aa015fba6ea543
Shashikiran H <skiranh@juniper.net>
2018-04-13 12:56:40 +0530

    Script to check for 5.0 granular routing policy
    Should have testcases to test these:
    - Configure interface type match with community, as-path, local pref and
    med and see if they take effect.
    - Configure bgpaas type match with community, as-path, local pref and
    med and see if they take effect.
    - Configure service-interface type match with community, as-path, local pref and
    med and see if they take effect.
    - Configure interface-static type match with community, as-path, local pref and
    med and see if they take effect.
    
    Change-Id: I943e0e4d938d6989caa2d11b430ad62bd6908fde
    Closes-Bug: #1763606

---
+++ scripts/routing_policy/test_routing_policy.py
@@ -0,0 +1,224 @@
+from tcutils.wrappers import preposttest_wrapper
+from common.servicechain.firewall.verify import VerifySvcFirewall
+from common.svc_health_check.base import BaseHC
+import test
+import time
+from common import isolated_creds
+from tcutils.wrappers import preposttest_wrapper
+from common.bgpaas.base import BaseBGPaaS
+from common.neutron.base import BaseNeutronTest
+from tcutils.util import *
+from tcutils.tcpdump_utils import *
+#BaseBGPaaS,
+from contrailapi import ContrailVncApi
+from base import RPBase
+ 
+class TestRP(RPBase, BaseBGPaaS, BaseHC, VerifySvcFirewall):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestRP, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestRP, cls).tearDownClass()
+
+    def is_test_applicable(self):
+        return (True, None)
+
+    def setUp(self):
+        super(TestRP, self).setUp()
+        result = self.is_test_applicable()
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_rp_interface(self):
+        '''
+        1. Create a routing policy with interface match.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+        #ret_dict = self.verify_svc_chain(service_mode='in-network-nat',
+        #                                 create_svms=True, max_inst=1)
+        vn_name = get_random_name('bgpaas_vn')
+        vn_subnets = [get_random_cidr()]
+        vn_fixture = self.create_vn(vn_name, vn_subnets)
+        test_vm = self.create_vm(vn_fixture, 'test_vm',
+                                 image_name='cirros')
+        assert test_vm.wait_till_vm_is_up()
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface', to_term = 'community', sub_to = '64512:55555')
+        self.verify_policy_in_control(vn_fixture, test_vm.vm_ip, '55555')
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_rp_interface_static(self):
+        '''
+        1. Create a routing policy with interface-static match.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+
+        #ret_dict = self.verify_svc_chain(service_mode='in-network-nat',
+        #                                 create_svms=True, max_inst=1)
+        vn_name = get_random_name('bgpaas_vn')
+        vn_subnets = [get_random_cidr()]
+        vn_fixture = self.create_vn(vn_name, vn_subnets)
+        test_vm = self.create_vm(vn_fixture, 'test_vm',
+                                 image_name='cirros')
+        assert test_vm.wait_till_vm_is_up()
+        self.static_table_handle = ContrailVncApi(self.vnc_lib, self.logger)
+        self.intf_table_to_right_obj = self.static_table_handle.create_route_table(
+            prefixes=['14.15.16.17/32'],
+            name="int_table_right",
+            parent_obj=self.project.project_obj,
+        )
+        id_entry = self.inputs.project_fq_name[0] + ':' + \
+            self.inputs.project_fq_name[1] + ':' + vn_fixture.vn_name
+        self.static_table_handle.bind_vmi_to_interface_route_table(
+            str(test_vm.get_vmi_ids()[id_entry]),
+            self.intf_table_to_right_obj)
+
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface-static', to_term = 'community', sub_to = '64512:55555')
+        self.verify_policy_in_control(vn_fixture, '14.15.16.17/32', '55555')
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_rp_service_interface(self):
+        '''
+        1. Create a routing policy with service-interface match.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+        
+        ret_dict = self.verify_svc_chain(service_mode='in-network',
+                                         create_svms=True, max_inst=1)
+        #vn_name = get_random_name('bgpaas_vn')
+        #vn_subnets = [get_random_cidr()]
+        #vn_fixture = self.create_vn(vn_name, vn_subnets)
+        #test_vm = self.create_vm(vn_fixture, 'test_vm',
+        #                         image_name='cirros')
+        si_fixture = ret_dict['si_fixture']
+        left_vm_fixture = ret_dict['left_vm_fixture']
+        right_vm_fixture = ret_dict['right_vm_fixture']
+        left_vn_fixture = ret_dict['left_vn_fixture']
+        self.configure_term_routing_policy(vn_fixture = left_vn_fixture, from_term = 'protocol', sub_from = 'service-interface', to_term = 'community', sub_to = '64512:55555')
+        self.verify_policy_in_control(left_vn_fixture, str(self.vnc_lib.routing_instance_read(id = str(self.vnc_lib.virtual_network_read(id = left_vn_fixture.uuid).get_routing_instances()[1]['uuid'])).get_service_chain_information().service_chain_address), '55555')
+
+    @test.attr(type=['sanity'])
+    @preposttest_wrapper
+    def test_rp_bgpaas(self):
+        '''
+        1. Create a routing policy with bgpaas match.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+
+        vn_name = get_random_name('bgpaas_vn')
+        vn_subnets = [get_random_cidr()]
+        vn_fixture = self.create_vn(vn_name, vn_subnets)
+        test_vm = self.create_vm(vn_fixture, 'test_vm',
+                                 image_name='cirros')
+        assert test_vm.wait_till_vm_is_up()
+        bgpaas_vm1 = self.create_vm(vn_fixture, 'bgpaas_vm1',image_name='vsrx')
+        assert bgpaas_vm1.wait_till_vm_is_up()
+        bgpaas_fixture = self.create_bgpaas(bgpaas_shared=True, autonomous_system=64500, bgpaas_ip_address=bgpaas_vm1.vm_ip)
+        bgpaas_vm1.wait_for_ssh_on_vm()
+        port1 = {}
+        port1['id'] = bgpaas_vm1.vmi_ids[bgpaas_vm1.vn_fq_name]
+        address_families = []
+        address_families = ['inet', 'inet6']
+        autonomous_system = 64500
+        gw_ip = vn_fixture.get_subnets()[0]['gateway_ip']
+        dns_ip = vn_fixture.get_subnets()[0]['dns_server_address']
+        neighbors = []
+        neighbors = [gw_ip, dns_ip]
+        self.logger.info('We will configure BGP on the two vSRX')
+        self.config_bgp_on_vsrx(src_vm=test_vm, dst_vm=bgpaas_vm1, bgp_ip=bgpaas_vm1.vm_ip, lo_ip=bgpaas_vm1.vm_ip,
+                                address_families=address_families, autonomous_system=autonomous_system, neighbors=neighbors, bfd_enabled=False)
+        bgpaas_vm1.wait_for_ssh_on_vm()
+        self.attach_vmi_to_bgpaas(port1['id'], bgpaas_fixture)
+        self.addCleanup(self.detach_vmi_from_bgpaas,port1['id'], bgpaas_fixture)
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'bgpaas', to_term = 'community', sub_to = '64512:55555')
+        self.verify_policy_in_control(vn_fixture, str(vn_fixture.get_subnets()[0]['cidr']), '55555')
+
+    @preposttest_wrapper
+    def test_rp_interface_matrix(self):
+        '''
+        1. Create a routing policy with interface match and different "to" conditions:med, as-path, local-pref, community.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+
+        #ret_dict = self.verify_svc_chain(service_mode='in-network-nat',
+        #                                 create_svms=True, max_inst=1)
+        vn_name = get_random_name('bgpaas_vn')
+        vn_subnets = [get_random_cidr()]
+        vn_fixture = self.create_vn(vn_name, vn_subnets)
+        test_vm = self.create_vm(vn_fixture, 'test_vm',
+                                 image_name='cirros')
+        assert test_vm.wait_till_vm_is_up()
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface', to_term = 'med', sub_to = '444')
+        self.verify_policy_in_control(vn_fixture, test_vm.vm_ip, '444')
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface', to_term = 'local-preference', sub_to = '555')
+        self.verify_policy_in_control(vn_fixture, test_vm.vm_ip, '555')
+
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface', to_term = 'as-path', sub_to = '666')
+        self.verify_policy_in_control(vn_fixture, test_vm.vm_ip, '666')
+
+    @preposttest_wrapper
+    def test_rp_interface_static_matrix(self):
+        '''
+        1. Create a routing policy with interface-static match and different "to" conditions:med, as-path, local-pref, community.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+
+        #ret_dict = self.verify_svc_chain(service_mode='in-network-nat',
+        #                                 create_svms=True, max_inst=1)
+        vn_name = get_random_name('bgpaas_vn')
+        vn_subnets = [get_random_cidr()]
+        vn_fixture = self.create_vn(vn_name, vn_subnets)
+        test_vm = self.create_vm(vn_fixture, 'test_vm',
+                                 image_name='cirros')
+        assert test_vm.wait_till_vm_is_up()
+        self.static_table_handle = ContrailVncApi(self.vnc_lib, self.logger)
+        self.intf_table_to_right_obj = self.static_table_handle.create_route_table(
+            prefixes=['14.15.16.17/32'],
+            name="int_table_right",
+            parent_obj=self.project.project_obj,
+        )
+        id_entry = self.inputs.project_fq_name[0] + ':' + \
+            self.inputs.project_fq_name[1] + ':' + vn_fixture.vn_name
+        self.static_table_handle.bind_vmi_to_interface_route_table(
+            str(test_vm.get_vmi_ids()[id_entry]),
+            self.intf_table_to_right_obj)
+
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface-static', to_term = 'med', sub_to = '444')
+        self.verify_policy_in_control(vn_fixture, '14.15.16.17/32', '444')
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface-static', to_term = 'local-preference', sub_to = '555')
+        self.verify_policy_in_control(vn_fixture, '14.15.16.17/32', '555')
+
+        self.configure_term_routing_policy(vn_fixture = vn_fixture, from_term = 'protocol', sub_from = 'interface-static', to_term = 'as-path', sub_to = '666')
+        self.verify_policy_in_control(vn_fixture, '14.15.16.17/32', '666')
+
+    @preposttest_wrapper
+    def test_rp_service_interface_matrix(self):
+        '''
+        1. Create a routing policy with service-interface match and different "to" conditions:med, as-path, local-pref, community.
+        2. Launch VMs. 
+        3. Attach policy to VN and confirm if policy takes hold. 
+        '''
+
+        ret_dict = self.verify_svc_chain(service_mode='in-network',
+                                         create_svms=True, max_inst=1)
+        si_fixture = ret_dict['si_fixture']
+        left_vm_fixture = ret_dict['left_vm_fixture']
+        right_vm_fixture = ret_dict['right_vm_fixture']
+        left_vn_fixture = ret_dict['left_vn_fixture']
+
+        self.configure_term_routing_policy(vn_fixture = left_vn_fixture, from_term = 'protocol', sub_from = 'service-interface', to_term = 'med', sub_to = '444')
+        self.verify_policy_in_control(left_vn_fixture, str(self.vnc_lib.routing_instance_read(id = str(self.vnc_lib.virtual_network_read(id = left_vn_fixture.uuid).get_routing_instances()[1]['uuid'])).get_service_chain_information().service_chain_address), '444')
+        self.configure_term_routing_policy(vn_fixture = left_vn_fixture, from_term = 'protocol', sub_from = 'service-interface', to_term = 'local-preference', sub_to = '555')
+        self.verify_policy_in_control(left_vn_fixture, str(self.vnc_lib.routing_instance_read(id = str(self.vnc_lib.virtual_network_read(id = left_vn_fixture.uuid).get_routing_instances()[1]['uuid'])).get_service_chain_information().service_chain_address), '555')
+        self.configure_term_routing_policy(vn_fixture = left_vn_fixture, from_term = 'protocol', sub_from = 'service-interface', to_term = 'as-path', sub_to = '666')
+        self.verify_policy_in_control(left_vn_fixture, str(self.vnc_lib.routing_instance_read(id = str(self.vnc_lib.virtual_network_read(id = left_vn_fixture.uuid).get_routing_instances()[1]['uuid'])).get_service_chain_information().service_chain_address), '666')
