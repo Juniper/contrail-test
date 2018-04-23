2bf947a11979be979bb9147de0e703651a2e32b8
VIVEK GARG <vivekgarg@juniper.net>
2018-04-23 15:59:13 -0700

    Closes-bug: #1766367 Introducing new variable deployer in contrail_test_input.yml for openshift sanity
    Change-Id: I8d79a5ddd7e935b9a79e62360cb3449e7bdf6afc

--- common/contrail_test_init.py
+++ common/contrail_test_init.py
@@ -843,6 +843,7 @@ class TestInputs(object):
         self.mx_rt = str(test_configs.get('public_rt') or '')
         self.router_asn = str(test_configs.get('router_asn') or '64512')
         self.kube_config_file = test_configs.get('kube_config_file') or '/etc/kubernetes/admin.conf'
+        self.openshift_config_file = test_configs.get('openshift_config_file') or '/root/.kube/config'
         self.ext_routers = []
         for rtr_name, address in test_configs.get('ext_routers', {}).iteritems():
             self.ext_routers.append((rtr_name, address))
@@ -1393,8 +1394,12 @@ class ContrailTestInit(object):
         self.address_family = 'v4'
         if self.orchestrator == 'kubernetes' or self.slave_orchestrator == 'kubernetes':
             if not os.path.exists(self.kube_config_file):
-                self.copy_file_from_server(self.k8s_master_ip,
-                    self.kube_config_file, self.kube_config_file)
+                if self.deployer == 'openshift' :
+                    self.copy_file_from_server(self.k8s_master_ip,
+                        self.openshift_config_file, self.kube_config_file)
+                else:
+                    self.copy_file_from_server(self.k8s_master_ip,
+                        self.kube_config_file, self.kube_config_file)
     # end __init__
 
     def is_ci_setup(self):
