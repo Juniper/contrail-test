99cb8f49c7870e080eed5d0c82ce922be722174d
sarath <nsarath@juniper.net>
2018-05-02 17:00:20 -0700

    Add testcase for non-Isolated POD with SNAT and add public DNS reachability closes-bug: #1768677
    Change-Id: I58e65957e5e08f742aa475f8ac5635cdc650f00f

--- scripts/k8s_scripts/test_snat.py
+++ scripts/k8s_scripts/test_snat.py
@@ -102,6 +102,88 @@ class TestSNAT(BaseK8sTest):
         assert client1[0].ping_to_ip(client3[0].pod_ip, expectation=False)
     #end test_pod_publicreachability_with_snat_enabled
 
+    @test.attr(type=['openshift_1'])
+    @preposttest_wrapper
+    def test_isolated_pod_external_reachability_with_snat_enabled(self):
+        """
+        Verify the pods reachability to public network when ip snat forwarding is enables
+           1.create 2 isolated namespaces with snat forwarding enabled
+           2.create pods in thos namespaces(ns1:pod1,pod2(c1,c2), ns2:pod1 ,default:pod1)
+           3.ping ns1:pod1 to public host dns should PASS
+           4.ping from ns1:pod2:c1 and ns1:pod2:c2 to the public dns should PASS
+           5.ping from ns1:pod1 to ns2:pod1 should FAIL
+           6.ping from ns1:pod1 to dafeult:pod1 should FAIL
+           7.ping public_host_url DNS verification both from user-defined namespace
+        """
+        ## Setup Pods
+        client1, client2, client3 = self.setup_namespaces_pods_for_snat_test(isolation=True,
+                                                                            ip_fabric_snat=True)
+
+        ## Setup additional busy box pod with nslookup utility to verify dns
+        namespace4_name = get_random_name("ns4")
+        namespace4 = self.setup_namespace(name = namespace4_name, isolation=True,
+                                         ip_fabric_snat=True)
+        assert namespace4.verify_on_setup()
+        client4 = self.setup_busybox_pod(namespace=namespace4_name)
+        assert client4.verify_on_setup()
+
+        ## Verify Reachability
+        assert client1[0].ping_to_ip(self.inputs.public_host)
+        assert client1[1].ping_to_ip(self.inputs.public_host,container="c1")
+        assert client1[1].ping_to_ip(self.inputs.public_host,container="c2")
+        assert client2[0].ping_to_ip(self.inputs.public_host)
+        assert client1[0].ping_to_ip(client1[2].pod_ip)
+        assert client1[0].ping_to_ip(client2[0].pod_ip, expectation=False)
+        assert client1[0].ping_to_ip(client3[0].pod_ip, expectation=False)
+        lookup_str = "nslookup %s" % (self.inputs.public_host_url)
+        output = client4.run_cmd(lookup_str)
+        msg = 'DNS resolution failed'
+        assert 'nslookup: can\'t resolve' not in output, msg
+        self.logger.info('DNS resolution check : %s passed. Output: %s' % (lookup_str, output))
+
+    #end test_pod_publicreachability_with_snat_enabled
+
+    @test.attr(type=['openshift_1'])
+    @preposttest_wrapper
+    def test_non_isolated_pod_external_reachability_with_snat_enabled(self):
+        """
+        Verify the pods reachability to public network when ip snat forwarding is enables
+           1.create 2 non isolated namespaces with snat forwarding enabled
+           2.create pods in thos namespaces(ns1:pod1,pod2(c1,c2), ns2:pod1 ,default:pod1)
+           3.ping ns1:pod1 to public host dns should PASS
+           4.ping from ns1:pod2:c1 and ns1:pod2:c2 to the public dns should PASS
+           5.ping from ns1:pod1 to ns2:pod1 should FAIL
+           6.ping from ns1:pod1 to dafeult:pod1 should FAIL
+           7.ping public_host_url DNS verification from user-defined namespace
+        """
+        ## Setup Pods
+        client1, client2, client3 = self.setup_namespaces_pods_for_snat_test(isolation=False,
+                                                                            ip_fabric_snat=True)
+
+        ## Setup additional busy box pod with nslookup utility to verify dns
+        namespace4_name = get_random_name("ns4")
+        namespace4 = self.setup_namespace(name = namespace4_name, isolation=False,
+                                         ip_fabric_snat=True)
+        assert namespace4.verify_on_setup()
+        client4 = self.setup_busybox_pod(namespace=namespace4_name)
+        assert client4.verify_on_setup()
+
+        ## Verify Reachability
+        assert client1[0].ping_to_ip(self.inputs.public_host)
+        assert client1[1].ping_to_ip(self.inputs.public_host,container="c1")
+        assert client1[1].ping_to_ip(self.inputs.public_host,container="c2")
+        assert client2[0].ping_to_ip(self.inputs.public_host)
+        assert client1[0].ping_to_ip(client1[2].pod_ip)
+        assert client1[0].ping_to_ip(client2[0].pod_ip)
+        assert client1[0].ping_to_ip(client3[0].pod_ip)
+        lookup_str = "nslookup %s" % (self.inputs.public_host_url)
+        output = client4.run_cmd(lookup_str)
+        msg = 'DNS resolution failed'
+        assert 'nslookup: can\'t resolve' not in output, msg
+        self.logger.info('DNS resolution check : %s passed. Output: %s' % (lookup_str, output))
+
+    #end test_pod_publicreachability_with_snat_enabled
+
     @preposttest_wrapper
     def test_snat_forwarding_disabled_by_default(self):
         """
