bfc3f0d3e130f64e5bf25d3bbcc826bb776e0f5d
Aswani Kumar Reddy G <aswanikr@juniper.net>
2018-06-20 14:45:49 +0530

    added testcase for agent restart in vcenter_only mode
    Change-Id: Ifbf38b94068a167cad388a6b42c02cc4e58d712f
    Closes-Bug: #1777844

--- serial_scripts/vm_regression/test_vcenter_serial.py
+++ serial_scripts/vm_regression/test_vcenter_serial.py
@@ -74,11 +74,12 @@ class TestVcenterSerial(BaseVnVmTest):
         '''
         Description:  Rebooting ContrailVM and verify compute services.
         Test steps:
-
-               1. reboot contrail-compute VM's
-               2. Verify contrail-status
-               3. Create two guest VM's
-               4. ping between the vm after compute VM reboot
+               1. Create two vms and ping between them
+               2. reboot contrail-compute VM's
+               3. Verify contrail-status
+               4. ping between existing vms
+               5. Create two more guest VM's
+               6. ping between the vm after compute VM reboot
         Pass criteria: Contrail status should be active after reboot and ping between the VM has to work.
         Maintainer : shajuvk@juniper.net
         '''
@@ -101,8 +102,12 @@ class TestVcenterSerial(BaseVnVmTest):
         for compute_vm in self.inputs.compute_ips:
             self.inputs.run_cmd_on_server(compute_vm, 'ifconfig ens192 up')
         cluster_status, error_nodes = ContrailStatusChecker().wait_till_contrail_cluster_stable()
-        assert cluster_status, 'Cluster is not stable after reboot...'
 
+        assert vm1_fixture.ping_with_certainty(dst_vm_fixture=vm2_fixture),\
+            "Ping from %s to %s failed" % (vn1_vm1_name, vn1_vm2_name)
+        assert vm2_fixture.ping_with_certainty(dst_vm_fixture=vm1_fixture),\
+            "Ping from %s to %s failed" % (vn1_vm2_name, vn1_vm1_name)
+       
         vn1_vm1_name = get_random_name('vm1_after_reboot')
         vn1_vm2_name = get_random_name('vm2_after_reboot')
         vm1_fixture = self.create_vm(vn_fixture=vn1_fixture, vm_name=vn1_vm1_name)
@@ -117,6 +122,39 @@ class TestVcenterSerial(BaseVnVmTest):
 
      # end of test test_vcenter_compute_vm_reboot
 
+    @test.attr(type=['vcenter'])
+    @preposttest_wrapper
+    def test_vcenter_vrouter_agent_restart(self):
+        ''' Test steps:
+               1. Create two guest VM's
+               2. ping between the vms
+               3. restart agents on Contrail VM'
+               4. Verify contrail-status
+               5. ping between the vms
+        Pass criteria: Contrail status should be active after restart  and ping between the VM has to work.'''
+ 
+        vn1_name = get_random_name('vn50')
+        vn1_vm1_name = get_random_name('vm1')
+        vn1_vm2_name = get_random_name('vm2')
+        vn1_fixture = self.create_vn(vn1_name, [get_random_cidr()])
+        vm1_fixture = self.create_vm(vn_fixture=vn1_fixture, vm_name=vn1_vm1_name)
+        vm2_fixture = self.create_vm(vn_fixture=vn1_fixture, vm_name=vn1_vm2_name)
+        vm1_fixture.wait_till_vm_is_up()
+        vm2_fixture.wait_till_vm_is_up()
+        assert vm1_fixture.ping_with_certainty(dst_vm_fixture=vm2_fixture),\
+            "Ping from %s to %s failed" % (vn1_vm1_name, vn1_vm2_name)
+        assert vm2_fixture.ping_with_certainty(dst_vm_fixture=vm1_fixture),\
+            "Ping from %s to %s failed" % (vn1_vm2_name, vn1_vm1_name)
+        contrail_vms = self.inputs.compute_ips
+        self.inputs.restart_service('contrail-vrouter-agent', contrail_vms,
+                                 container='agent',
+                                 verify_service=True)
+        cluster_status, error_nodes = ContrailStatusChecker().wait_till_contrail_cluster_stable()
+        assert cluster_status, 'Cluster is not stable after restart...'
+        assert vm1_fixture.ping_with_certainty(dst_vm_fixture=vm2_fixture),\
+            "Ping from %s to %s failed" % (vn1_vm1_name, vn1_vm2_name)
+        assert vm2_fixture.ping_with_certainty(dst_vm_fixture=vm1_fixture),\
+            "Ping from %s to %s failed" % (vn1_vm2_name, vn1_vm1_name)
 
     def get_compute_vm(self, host):
         for esxi_host in self.inputs.esxi_vm_ips:
