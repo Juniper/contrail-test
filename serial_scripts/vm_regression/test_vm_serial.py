2e34a98376981a813c96fd739bd3cb10ef3225c0
usha <musharani@juniper.net>
2018-09-20 17:29:02 +0530

    script fix for test_max_vm_flows testcase in 5.0
    Change-Id: I0752dd53f669530eb05bf3802d688624578b0a95
    Closes-bug: #1793510

--- serial_scripts/vm_regression/test_vm_serial.py
+++ serial_scripts/vm_regression/test_vm_serial.py
@@ -739,6 +739,21 @@ class TestBasicVMVN0(BaseVnVmTest):
         for cmp_node in compute_ips:
             comp_node_fixt[cmp_node] = self.useFixture(ComputeNodeFixture(
                 self.connections, cmp_node))
+            compute_user = self.inputs.host_data[cmp_node]['username']
+            compute_pwd = self.inputs.host_data[cmp_node]['password']
+            host_data = dict()
+            host_data['host_ip'] = cmp_node
+            host_data['username'] = compute_user
+            host_data['password'] = compute_pwd
+            self.inputs._check_containers(host_data)
+            container_list = host_data['containers'].keys()
+            if 'agent' in container_list:
+                cmd = 'docker cp %s:/%s %s' % (host_data['containers']['agent'], \
+                        comp_node_fixt[cmp_node].agent_conf_file, \
+                        comp_node_fixt[cmp_node].agent_conf_file)
+                compute_user = self.inputs.host_data[cmp_node]['username']
+                compute_pwd = self.inputs.host_data[cmp_node]['password']
+                self.inputs.run_cmd_on_server(cmp_node,cmd,compute_user, compute_pwd)
             comp_node_fixt[cmp_node].set_flow_aging_time(
                 flow_cache_timeout)
             comp_node_fixt[cmp_node].get_config_per_vm_flow_limit()
