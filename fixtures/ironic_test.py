daa3a5624d3392a39b0656f4e0f2b8a44cb5b1f9
Vageesan Thanikachalam <vageesant@juniper.net>
2018-11-04 13:51:36 +0530

    Changes for BMS LCM scripts
    Closes-Bug: 1792491
    
    Change-Id: I7bb9ace7e445a84e1de161b9327c784bd1106c39

--- fixtures/ironic_test.py
+++ fixtures/ironic_test.py
@@ -1,6 +1,8 @@
 import os
 import openstack
+from ironicclient import exc as ironic_exc
 from common.openstack_libs import ironic_client as client
+from common import log_orig as contrail_logging
 
 class IronicHelper(object):
     '''
@@ -20,11 +22,11 @@ class IronicHelper(object):
        :param verify: Enable or Disable ssl cert verification
     '''
     def __init__(self, auth_h=None, **kwargs):
-        inputs = kwargs.get('inputs')
-        self.logger = kwargs.get('logger') or inputs.logger if inputs \
+        self.inputs = kwargs.get('inputs')
+        self.logger = kwargs.get('logger') or self.inputs.logger if self.inputs \
                           else contrail_logging.getLogger(__name__)
         self.region_name = kwargs.get('region_name') or \
-                           inputs.region_name if inputs else None
+                           inputs.region_name if self.inputs else None
         if not auth_h:
             auth_h = self.get_auth_h(**kwargs)
         self.auth_h = auth_h
@@ -34,10 +36,73 @@ class IronicHelper(object):
         return openstack.OpenstackAuth(**kwargs)
 
     def setUp(self):
+        if not self.inputs.host_data[self.inputs.openstack_names[0]]['containers'].get('ironic_conductor'):
+           return
         self.obj = client.get_client('1',
                        session=self.auth_h.get_session(scope='project'),
                        os_region_name=self.region_name)
+        self.obj.http_client.api_version_select_state = self.inputs.ironic_api_config.get('api_version_select_state','user')
+        self.obj.http_client.os_ironic_api_version = self.inputs.ironic_api_config.get('os_ironic_api_version','1.31')
     # end setUp
 
     def get_auth_h(self, **kwargs):
         return openstack.OpenstackAuth(**kwargs)
+
+    def delete_ironic_node(self,node_id):
+
+        self.obj.node.delete(node_id)
+
+    def create_ironic_port(self,port,mac_address,node_uuid,portgroup_uuid,pxe_enabled):
+        self.obj.port.create(local_link_connection=port,\
+            address=mac_address,node_uuid=node_uuid,\
+                portgroup_uuid=portgroup_uuid,pxe_enabled=pxe_enabled)
+
+    def create_ironic_portgroup(self,node_id,pg_name,mac_addr):
+
+        try:
+          pg_obj = self.obj.portgroup.get(pg_name)
+        except ironic_exc.NotFound:
+          pg_obj = None
+        if not pg_obj:
+          try:
+             pg_obj = self.obj.portgroup.create(mode="802.3ad",name=pg_name,\
+                                       address=mac_addr,\
+                                       node_uuid=node_id)
+          except ironic_exc.Conflict,ex:
+             self.logger.info(ex.message) # TO_FIX: handle so that this is not hit
+          except Exception,ex:
+             self.logger.info("ERROR: exception in creating PG")
+             return
+
+        self.portgroup_uuid = pg_obj.uuid
+
+    def create_ironic_node(self,ironic_node_name,port_list,driver_info,properties):
+
+        port_group_name                   = ironic_node_name + "_pg"
+
+        node_obj = self.obj.node.create(name=ironic_node_name,driver='pxe_ipmitool',\
+                                        driver_info=driver_info,properties=properties)
+
+        self.portgroup_uuid = None
+        if len(port_list) > 1:
+           pg_obj = self.create_ironic_portgroup(node_obj.uuid,port_group_name,\
+                                           port_list[0]['mac_addr'])
+           if pg_obj:
+              self.portgroup_uuid = pg_obj.uuid
+        for i,port in enumerate(port_list):
+            port_dl = {}
+            port_dl['switch_info'] = port['switch_info']
+            port_dl['port_id']     = port['port_id']
+            port_dl['switch_id']   = port['switch_id']
+            self.create_ironic_port(port=port_dl,node_uuid=node_obj.uuid,\
+                               mac_address=port['mac_addr'],
+                               portgroup_uuid=self.portgroup_uuid,pxe_enabled=port['pxe_enabled'])
+        return node_obj
+
+    def set_ironic_node_state(self,node_id,new_state):
+
+        if new_state == "available":
+           self.obj.node.set_provision_state(node_id,"manage")
+           self.obj.node.set_provision_state(node_id,"provide")
+
+
