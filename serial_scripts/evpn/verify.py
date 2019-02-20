760b4196f22027069b2876c54fe4dfc51fac902b
Aswani Kumar Reddy G <aswanikr@juniper.net>
2019-02-20 19:36:53 +0530

    reduce evpn testcases running time and fix single interface hostname change
    Change-Id: I4b662f9a7ecd227e6f55853e0b777e4b139cab49
    closes-jira-bug: JCB-219302

--- serial_scripts/evpn/verify.py
+++ serial_scripts/evpn/verify.py
@@ -1050,10 +1050,7 @@ class VerifyEvpnCases():
             dest_vm_ip = match.group(1)
         valid_ip = re.search('13.1.1.(.*)', output)
         assert valid_ip, 'failed to get ip from 13.1.1.0 subnet as configured in dhcp vm'
-        vn_l2_vm1_fixture.put_pub_key_to_vm()
-        vn_l2_vm2_fixture.put_pub_key_to_vm()
-        file_sizes = ['1000', '1101', '1202', '1303', '1373',
-                      '1374', '2210', '2845', '3000', '10000', '2000000']
+        file_sizes = ['3000', '10000', '2000000']
         for size in file_sizes:
             self.logger.info("-" * 80)
             self.logger.info("FILE SIZE = %sB" % size)
