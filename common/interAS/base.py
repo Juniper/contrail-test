bc1f1729514bf19ab84adf0363922f21ac453274
vganapathy <vganapathy@juniper.net>
2019-05-22 12:39:09 +0530

    interAS Option C feature
    Change-Id: I6265dc7af0a11580b0c16d42bb40a371611bbbd4
    Partial-Jira-Bug: CEM-3077

--- common/interAS/base.py
+++ common/interAS/base.py
@@ -14,9 +14,12 @@ from vnc_api.vnc_api import *
 from tcutils.traffic_utils.iperf3_traffic import Iperf3
 from collections import OrderedDict
 from compute_node_test import ComputeNodeFixture
+from common.pbb_evpn.base import PbbEvpnTestBase
+from common.device_connection import NetconfConnection
 
 
-class BaseInterAS(BaseNeutronTest, ConfigPolicy):
+class BaseInterAS(PbbEvpnTestBase, BaseNeutronTest):
+    ENCAP = 'gre'
 
     @classmethod
     def setUpClass(cls):
@@ -65,7 +68,32 @@ class BaseInterAS(BaseNeutronTest, ConfigPolicy):
             address_families = params.get_address_families()
             if 'inet-vpn' not in address_families.family:
                self.vnc_h.update_bgp_router_af(router, 'inet-vpn')
+            self.associate_peers_for_remote_asbr(bgp_router_obj)
             self.remote_asbr.append(bgp_router_obj)
+
+    def associate_peers_for_remote_asbr(self, remote_asbr_obj):
+        #bgp_addr_fams = AddressFamilies(['inet-vpn'])
+        #bgp_sess_attrs = [
+            #BgpSessionAttributes(address_families=bgp_addr_fams)]
+        #bgp_sessions = [BgpSession(attributes=bgp_sess_attrs)]
+        bgp_family_attr = BgpFamilyAttributes()
+        bgp_family_attr.set_address_family(u'inet-vpn')
+        bgp_family_attr.set_default_tunnel_encap([u'mpls'])
+        
+        bgp_session_param = BgpSessionAttributes()
+        bgp_session_param.set_family_attributes([bgp_family_attr])
+
+        bgp_session = BgpSession()
+        bgp_session.set_attributes([bgp_session_param])
+        bgp_peering_attrs = BgpPeeringAttributes(session=[bgp_session])
+        for ctrl_node in self.inputs.bgp_ips:
+            fq_name=['default-domain', 'default-project',
+                'ip-fabric', '__default__',
+                self.inputs.host_data[ctrl_node]['fqname']]
+            ctrl_node_obj = self.vnc_lib.bgp_router_read(fq_name=fq_name)
+            remote_asbr_obj.add_bgp_router(ctrl_node_obj, bgp_peering_attrs)
+            self.vnc_lib.bgp_router_update(remote_asbr_obj)
+  
 
     def control_node_fix(self, bgp_router):
         router_name = bgp_router.name
@@ -74,7 +102,7 @@ class BaseInterAS(BaseNeutronTest, ConfigPolicy):
                                connections=self.connections,
                                router_name=router_name,
                                router_ip=router_ip,
-                               router_type='mx',
+                               router_type='router',
                                inputs=self.inputs))
         return cntrl_fix
 
@@ -84,10 +112,12 @@ class BaseInterAS(BaseNeutronTest, ConfigPolicy):
         for asbr in asbrs_obj:
             cn_fix = self.control_node_fix(asbr)
             if not cn_fix.verify_peer_in_control_nodes():
-                self.logger.debug("Neighbor verification failed for %s", asbr.name)
+                self.logger.debug("Neighbor verification failed for %s",\
+                    asbr.name)
                 result = result and False
         return result
 
+    @retry(delay=5, tries=5)
     def verify_inet3_routing_instance(self):
         for ctrl_node in self.inputs.bgp_ips:
             cn_inspect = self.connections.cn_inspect[ctrl_node]
@@ -97,9 +127,105 @@ class BaseInterAS(BaseNeutronTest, ConfigPolicy):
                 rt_found = False
                 for route in routes['routes']:
                     if compute in route['prefix']:
-                        self.logger.info ("vhost ip , %s, is seen in inet.3 table" % compute)
+                        self.logger.info ("vhost ip , %s, is seen in\
+                           inet.3 table" % compute)
                         rt_found = True
                         break
                 if not rt_found:
                     return rt_found
         return True
+
+    @retry(delay=5, tries=5)
+    def verify_l3vpn_routing_instance(self):
+        for router in self.inputs.remote_asbr_info:
+            params = self.inputs.remote_asbr_info[router]
+            rt_found = False
+            for ctrl_node in self.inputs.bgp_ips:
+                cn_inspect = self.connections.cn_inspect[ctrl_node]
+                routes = \
+                    cn_inspect.get_cn_route_table(rt_name='bgp.l3vpn.0')
+                for route in routes['routes']:
+                    if route['prefix'].split('/')[0] == \
+                      params['ip']+':'+str(params['target'])+':'+params['CE'][0]:
+                          self.logger.info("route, %s , found" \
+                            %routes['routes'][0]['prefix'])
+                          rt_found = True
+                          break
+            if not rt_found:
+                return False
+        return True
+
+    def run_cmds_on_mx(self, mx_ip, cmds):
+        mx_handle = NetconfConnection(host = mx_ip)
+        mx_handle.connect()
+        cli_output = mx_handle.config(stmts = cmds, timeout = 120)
+        mx_handle.disconnect()
+        assert (not('failed' in cli_output)), "Not able to push config to mx"
+
+    def tunnel_config_on_local_asbr(self, mode='gre'):
+        cmd = 'set groups testbed_l7_l3_h5 routing-options dynamic-tunnels testbed_l3 %s' %mode
+        for (router, ip) in self.inputs.inputs.local_asbr_info:
+            mx_ip = self.inputs.physical_routers_data[router]['mgmt_ip']
+            self.run_cmds_on_mx(mx_ip, [cmd])
+
+    def basic_end_to_end_ping(self, aap=False, forwarding_mode=None):
+        self.config_encap_priority(self.ENCAP)
+        self.tunnel_config_on_local_asbr(self.ENCAP)
+        for router in self.inputs.remote_asbr_info:
+            vn = {}
+            vn['count']=1
+            vn['vn1']={}
+            vn['vn1']['subnet']=\
+                self.inputs.remote_asbr_info[router]['subnet']
+            vn['vn1']['asn']=\
+                self.inputs.remote_asbr_info[router]['asn']
+            vn['vn1']['target']=\
+                self.inputs.remote_asbr_info[router]['target']
+
+            vn_fixtures = self.setup_vns(vn)
+    
+            if forwarding_mode:
+                self.set_vn_forwarding_mode(vn_fixtures['vn1'], forwarding_mode)
+
+            vmi = {'count': 2,
+               'vmi1': {'vn': 'vn1'},
+               'vmi2': {'vn': 'vn1'}
+              }
+
+            vmi_fixtures = self.setup_vmis(vn_fixtures, vmi)
+            vm = {'count':2,
+              'vm1':{'vn':['vn1'], 'vmi':['vmi1'], 'userdata':{}},
+              'vm2':{'vn':['vn1'], 'vmi':['vmi2'], 'userdata':{}}
+            }
+            vm_fixtures = self.setup_vms(vn_fixtures, vmi_fixtures, vm)
+            #import pdb;pdb.set_trace()
+            if aap:
+                vIP = get_an_ip(vn['vn1']['subnet'], offset=100)
+                for vmi in vmi_fixtures.values():
+                    self.config_aap(vmi.uuid, vIP, \
+                      mac=vmi.mac_address, aap_mode='active-active', contrail_api=True)
+                for vm in vm_fixtures.values():
+                    output = vm.run_cmd_on_vm(
+                        ['sudo ifconfig eth0:10 ' + vIP + ' netmask 255.255.255.0'])
+                result = False
+                for vm in vm_fixtures.values():
+                    result = result or vm.ping_with_certainty\
+                      (ip=self.inputs.remote_asbr_info[router]['CE'][0], other_opt="-I "+vIP)
+                    if result:
+                        self.logger.info("Ping from VM to remote ce with vIP, %s, as source passed" %vIP)
+                        return True
+                assert result, "Ping from both VM to remote ce with vIP, %s, as source failed" %vIP
+            else:
+              for vm_fix in vm_fixtures:
+                assert  vm_fixtures[vm_fix].ping_with_certainty\
+                  (ip=self.inputs.remote_asbr_info[router]['CE'][0])
+
+
+    def set_vn_forwarding_mode(self, vn_fix, forwarding_mode="default"):
+        self.logger.info("Set VN Forwarding mode to %s" %forwarding_mode)
+        vn_fix = self.vnc_h.virtual_network_read(id=vn_fix.uuid)
+        vni_obj_properties = vn_fix.get_virtual_network_properties(
+            ) or VirtualNetworkType()
+        vni_obj_properties.set_forwarding_mode(forwarding_mode)
+        vn_fix.set_virtual_network_properties(vni_obj_properties)
+        self.vnc_h.virtual_network_update(vn_fix)
---
+++ serial_scripts/interAS/test_interas.py
@@ -0,0 +1,102 @@
+from tcutils.wrappers import preposttest_wrapper
+from common.interAS.base import BaseInterAS
+from common.neutron.base import BaseNeutronTest
+from security_group import SecurityGroupFixture
+import test
+import time
+from tcutils.util import *
+from tcutils.tcpdump_utils import *
+from common import isolated_creds
+from test import attr
+
+class TestInterAS(BaseInterAS):
+
+    @preposttest_wrapper
+    def test_interAS_controller_restart(self):
+        self.inputs.restart_service('contrail-control',
+            self.inputs.bgp_control_ips, container='control')
+        self.verify_asbr_connection()
+        assert self.verify_l3vpn_routing_instance()
+
+    @preposttest_wrapper
+    def test_interAS_ecmp_to_VM(self):
+        self.ENCAP='udp'
+        self.basic_end_to_end_ping(aap=True)
+
+class TestInterASGRE(BaseInterAS):
+
+    @preposttest_wrapper
+    def test_interAS_ping_remote_ce(self):
+        self.ENCAP='gre'
+        self.basic_end_to_end_ping()
+
+    @preposttest_wrapper
+    def test_interAS_ping_with_subintf(self, ENCAP='gre'):
+        self.config_encap_priority(ENCAP)
+        self.tunnel_config_on_local_asbr(ENCAP)
+        for router in self.inputs.remote_asbr_info:
+            vn = {}
+            vn['count']=2
+            vn['vn1']={}
+            vn['vn1']['subnet']=\
+                self.inputs.remote_asbr_info[router]['subnet']
+            vn['vn1']['asn']=\
+                self.inputs.remote_asbr_info[router]['asn']
+            vn['vn1']['target']=\
+                self.inputs.remote_asbr_info[router]['target']
+
+            vn['vn2']={}
+            vn['vn2']['subnet']='10.10.10.0/24'
+            vn_fixtures = self.setup_vns(vn)
+    
+            vmi = {'count': 2,
+               'vmi1': {'vn': 'vn2'},
+               'vmi2': {'vn': 'vn1','parent':'vmi1','vlan':20}
+              }
+
+            vmi_fixtures = self.setup_vmis(vn_fixtures, vmi)
+            vm = {'count':1,
+              'vm1':{'vn':['vn2'], 'vmi':['vmi1'], 'userdata':{ 
+                  'vlan': str(vmi['vmi2']['vlan'])}}
+            }
+            vm_fixtures = self.setup_vms(vn_fixtures, vmi_fixtures, vm)
+            gw_ip=vn['vn1']['subnet'].split('/')[0].replace('0','1',-1)
+            cmd = ['ip route add default via %s dev eth0.20' %gw_ip]
+            for vm_fix in vm_fixtures:
+                vm_fixtures[vm_fix].run_cmd_on_vm(cmds=cmd, as_sudo=True, timeout=10)
+                assert vm_fixtures[vm_fix].ping_with_certainty\
+                  (ip=self.inputs.remote_asbr_info[router]['CE'][0])
+
+    @preposttest_wrapper
+    def test_interAS_vrouter_restart(self):
+        self.ENCAP='gre'
+        self.inputs.restart_service('contrail-vrouter',
+            self.inputs.compute_ips, container='agent')
+        assert self.verify_inet3_routing_instance()
+        self.basic_end_to_end_ping()
+
+class TestInterASUDP(TestInterASGRE):
+
+    @preposttest_wrapper
+    def test_interAS_ping_remote_ce(self):
+        self.ENCAP = 'udp'
+        self.basic_end_to_end_ping()
+
+    @preposttest_wrapper
+    def test_interAS_ping_with_subintf(self):
+        self.ENCAP = 'udp'
+        super(TestInterASUDP,self).test_interAS_ping_with_subintf(ENCAP='udp')
+
+    @preposttest_wrapper
+    def test_interAS_vrouter_restart(self):
+        self.ENCAP='udp'
+        self.inputs.restart_service('contrail-vrouter',
+            self.inputs.compute_ips, container='agent')
+        assert self.verify_inet3_routing_instance()
+        self.basic_end_to_end_ping()
+
+    @preposttest_wrapper
+    def test_interAS_forwarding_mode(self):
+        self.ENCAP = 'udp'
+        self.basic_end_to_end_ping(forwarding_mode='l3')
+
