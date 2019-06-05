03a827f91c8e554adfa82094a61be12b01eef38a
lmadhusudhan <lmadhusudhan@juniper.net>
2019-06-05 15:05:17 -0700

    Added ZTP automation support -Closes-jira-bug: CTINFRA-60
    Change-Id: Ic6c004cf2591b8ff65b16a42dd4103ddd083db54

---
+++ serial_scripts/ztp/test_ztp.py
@@ -0,0 +1,13 @@
+import test
+import time
+from ztp_base import ZtpBaseTest
+from tcutils.wrappers import preposttest_wrapper
+from tcutils.util import skip_because
+
+class TestZtp(ZtpBaseTest):
+
+    @preposttest_wrapper
+    def test_ztp_workflow(self):
+ 
+        pass
+
