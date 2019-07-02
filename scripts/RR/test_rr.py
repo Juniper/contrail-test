5b4041bb66db360fe82ddead892d2f908be07909
kpatel333 <kpatel@juniper.net>
2019-07-02 16:11:04 +0530

    Sanity 'test_create_vm_after_RR_set' and 'test_basic_RR' test need check for MX GW
    Change-Id: I4a72999ff685c0d867cc3413871ada073eb28f5b
    closes-jira-bug: CEM-6925

--- scripts/RR/test_rr.py
+++ scripts/RR/test_rr.py
@@ -43,6 +43,9 @@ class TestBasicRR(BaseRRTest):
             2. Verify ping between VM's works
         Pass criteria: Step 1 and 2 should pass
         '''
+        if os.environ.get('MX_GW_TEST', 0) != '1':
+            self.logger.info("Skipping Test. Env variable MX_GW_TEST is not set. Skipping the test")
+            raise self.skipTest("Skipping Test. Env variable MX_GW_TEST is not set. Skipping the test")
         if len(set(self.inputs.bgp_ips)) < 3:
             self.logger.info(
                 "Skipping Test. At least 3 control node required to run the test")
@@ -105,6 +108,9 @@ class TestBasicRR(BaseRRTest):
             2. Verify ping between VM's works
         Pass criteria: Step 1 and 2 should pass
         '''
+        if os.environ.get('MX_GW_TEST', 0) != '1':
+            self.logger.info("Skipping Test. Env variable MX_GW_TEST is not set. Skipping the test")
+            raise self.skipTest("Skipping Test. Env variable MX_GW_TEST is not set. Skipping the test")
         if len(set(self.inputs.bgp_ips)) < 3:
             self.logger.info(
                 "Skipping Test. At least 3 control node required to run the test")
