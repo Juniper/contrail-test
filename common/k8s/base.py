2e0620f96f5de9281b424eefa1d14c1f4e5fd08a
sarath <nsarath@juniper.net>
2019-11-14 20:32:21 -0800

    POD status and contrail status checks Closes-jira-bug: CEM-3889
    Change-Id: I3a94d8832da342fa0b83ab1278997c5af5d37184

--- common/k8s/base.py
+++ common/k8s/base.py
@@ -1277,3 +1277,53 @@ class BaseK8sTest(GenericTestBase, vnc_api_test.VncLibFixture):
             spec=spec))
     # end setup_network_attachment
 
+    def verify_daemonset_status(self, namespace=''):
+        '''
+        Returns 
+           True, if Desired and available are same for all daemonsets
+           False, otherwise
+        '''
+        if namespace:
+            daemonset_info = self.connections.k8s_client.read_daemonsets(namespace=namespace)
+        else:
+            daemonset_info = self.connections.k8s_client.read_daemonsets()
+        #import pdb; pdb.set_trace()
+        for item in daemonset_info.items:
+            self.logger.info('daemonsets %s' % (item))
+            item_status = item.status
+            if item_status.desiredNumberScheduled == item_status.currentNumberScheduled == \
+                item_status.numberReady == item_status.numberAvailable:
+                continue
+            self.logger.error('One or more daemonsets not in expected state')
+            return False
+        self.logger.info('All daemonsets are in expected states')
+        return True
+
+    def get_daemonset_status(self, namespace=''):
+        if namespace:
+            daemonset_info = self.connections.k8s_client.read_daemonsets(namespace=namespace)
+        else:
+            daemonset_info = self.connections.k8s_client.read_daemonsets()
+        daemonset_status = {}
+        for item in daemonset_info.items:
+            daemonset_status[str(item.metadata.name)] = item.status
+        return daemonset_status
+
+    def verify_pods_status(self, namespace=''):
+        '''
+        Returns
+          True, if all pods are in Running state
+          False, otherwise
+        '''
+        if namespace:
+            pods_info = self.connections.k8s_client.read_pods_namespace(namespace)
+        else:
+            pods_info = self.connections.k8s_client.read_pods_namespace()
+        for pod in pods_info.items:
+            if 'Running' in pod.status.phase or 'Succeeded' in pod.status.phase:
+                self.logger.info('Pod is in %s' %(pod.status.phase))
+                self.logger.info("Pod {} is in Running or Succeeded state".format(pod.metadata.name))
+                continue
+            self.logger.error("Pod {} is not in Running or Succeeded state".format(pod.metadata.name))
+            return False
+        return True
