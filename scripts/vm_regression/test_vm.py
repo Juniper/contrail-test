7fb473ac6aa00fc5aaf56aa63aada3296bfc2c27
Ritam Gangopadhyay <ritam@juniper.net>
2020-02-04 23:26:40 +0530

    Introduce a retry and delay incase of multi vn VM interface check for policy scripts.
    to fix failure in:-
    TestBasicPolicyConfig.test_policy_with_multi_vn_in_vm
    TestBasicPolicyConfigIpv4v6.test_policy_with_multi_vn_in_vm
    
    Closes-jira-bug: CEM-12488
    Change-Id: If304379ea47d0bb21f9600a51463b23df1ea07d3

--- scripts/vm_regression/test_vm.py
+++ scripts/vm_regression/test_vm.py
@@ -282,7 +282,7 @@ class TestBasicVMVN0(BaseVnVmTest):
         vn_obj1 = self.create_vn()
         assert vn_obj1.verify_on_setup()
 
-        vn_obj2 = self.create_vn(vn_obj1.get_name(), vn_obj1.get_cidrs(af='dual'))
+        vn_obj2 = self.create_vn(vn_name=vn_obj1.get_name(), subnets=vn_obj1.get_cidrs(af='dual'))
         assert vn_obj2.verify_on_setup()
         assert vn_obj2, 'Duplicate VN cannot be created'
         if (vn_obj1.vn_id == vn_obj2.vn_id):
@@ -859,7 +859,7 @@ class TestBasicVMVN2(BaseVnVmTest):
         vn1_vm2_name = get_random_name('vn1_vm2')
         vn1_vm3_name = get_random_name('vn1_vm3')
         vn1_vm4_name = get_random_name('vn1_vm4')
-        vn1_fixture = self.create_vn(vn_subnets=vn1_subnets[0])
+        vn1_fixture = self.create_vn(subnets=vn1_subnets)
         vm1_fixture = self.create_vm(vn1_fixture, vm_name=vn1_vm1_name)
         vm2_fixture = self.create_vm(vn1_fixture, vm_name=vn1_vm2_name)
         vm3_fixture = self.create_vm(vn1_fixture, vm_name=vn1_vm3_name)
@@ -1678,7 +1678,7 @@ class TestBasicVMVN5(BaseVnVmTest):
                 The same is not done for \
                  %s as it points to the default GW'%(other_interface,default_gateway_interface))
         self.logger.info('-' * 80)
-        vm1_intf = vm1_fixture.get_vm_interface_list()
+        vm1_intf = vm1_fixture.get_vm_interface_list()[1]
         vm1_intf.remove(other_interface)
         vm1_intf_local_ip = vm1_fixture.get_local_ip_vm_intf_name(vm1_intf[0])
         cmd = 'ifdown %s --force'%other_interface
@@ -1954,7 +1954,7 @@ class TestBasicVMVN5(BaseVnVmTest):
         if 'v6' in af or 'dual' in af:
             subnets.append(get_random_cidr(af='v6',
                            mask=SUBNET_MASK['v6']['max']))
-        vn_fixture = self.create_vn(vn_subnets=subnets[0].replace(':', '.'))
+        vn_fixture = self.create_vn(subnets=subnets)
         assert vn_fixture.verify_on_setup()
         self.logger.info(
             'out of /29 block, we can have 5 usable addresses. Only 5 VMs should get launched properly.')
