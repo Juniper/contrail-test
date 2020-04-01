0906781e2eab657b83c99881176d6aa866b4ae43
sarath <nsarath@juniper.net>
2020-03-31 18:03:50 -0700

    Openshift related changes for Deployment feature also in sync to Kubernetes requirements Closes-jira-bug: CEM-13969
    Change-Id: I46ad5715561f97728dfa665cbe419d83347bcbcf

--- scripts/k8s_scripts/test_deployment.py
+++ scripts/k8s_scripts/test_deployment.py
@@ -21,6 +21,13 @@ class TestPodDeployment(BaseK8sTest):
         client_pod = self.setup_busybox_pod()
         namespace = 'default'
         labels = {'a': 'b'}
+
+        # Openshift restricts SCC for API-access by default for Replication Controllers, below enables it
+        if self.inputs.deployer == 'openshift':
+            os_node = self.inputs.k8s_master_ip
+            ocad = "oc adm policy add-scc-to-user anyuid system:serviceaccount:%s:default"%(namespace)
+            self.inputs.run_cmd_on_server(os_node, ocad)
+
         dep_1 = self.setup_nginx_deployment(name='dep-test',
                                             replicas=3,
                                             pod_labels=labels)
@@ -61,6 +68,12 @@ class TestPodDeployment(BaseK8sTest):
         new_replicas = len(self.inputs.compute_ips)*3
         namespace = self.setup_namespace()
 
+        # By default, Openshift restricts SCC for API-access for Replication Controllers, below enables it
+        if self.inputs.deployer == 'openshift':
+            os_node = self.inputs.k8s_master_ip
+            ocad = "oc adm policy add-scc-to-user anyuid system:serviceaccount:%s:default"%(namespace.name)
+            self.inputs.run_cmd_on_server(os_node, ocad)
+
         client_pod = self.setup_busybox_pod(namespace=namespace.name)
         dep_1 = self.setup_nginx_deployment(namespace=namespace.name,
             replicas=replicas, pod_labels=labels)
@@ -82,4 +95,3 @@ class TestPodDeployment(BaseK8sTest):
             test_pod=client_pod)
 
     # end test_deployment_replica_updation
-
