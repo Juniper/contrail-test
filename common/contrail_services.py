910f14c5002b46be23bc3bacc30fb0ba2d56c1f2
Sowmya K <ksowmya@juniper.net>
2020-04-29 18:40:22 +0530

    Added missing entried to common/contrail_services.py under _CONTRAIL_SERVICES_RHOSP_CONTAINER_MAP "closes-jira-bug: CEM-14082"
    Change-Id: Id263d84f63709f9237e70def9b060b1d9f105c52

--- common/contrail_services.py
+++ common/contrail_services.py
@@ -169,6 +169,20 @@ _CONTRAIL_SERVICES_RHOSP_CONTAINER_MAP = {
     # WebUI
     'webui' : ['contrail_webui_web'],
     'webui-middleware': ['contrail_webui_job'],
+    #missing-entries
+    'agent-dpdk': ['contrail_vrouter_agent_dpdk'],
+    'nova': ['nova_api'],
+    'nova-compute': ['nova_compute'],
+    'nova-conductor': ['nova_conductor'],
+    'nova-scheduler': ['nova_scheduler'],
+    'glance': ['glance_api'],
+    'haproxy': ['haproxy'],
+    'keystone': ['keystone'],
+    'neutron': ['neutron_api'],
+    'mysql': ['clustercheck'],
+    'redis': ['contrail_redis'],
+    'stunnel': ['contrail_stunnel']
+    'analytics-zookeeper': ['contrail_analytics_zookeeper'],
 }
 
 CONTRAIL_PODS_SERVICES_MAP = {
