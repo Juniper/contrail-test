3ba54a2ad8edd4ff3021471fc1bcef6bb0f3adce
rranganathan <rranganathan@juniper.net>
2021-07-05 10:29:25 +0530

    CEM-22458 Squashed commit of the following:
    commit 8635ecf7fc566bc1674297f8c8816945c9a82d7d
    Author: rranganathan <rranganathan@juniper.net>
    Date:   Mon Jul 5 09:46:30 2021 +0530
    
        CEM-22458
    
        Change-Id: I15e46a7dc061c189dfabfecc03cec5e852f2bd7a
    
    commit 551b616a54890e6666ab0887a362b81dc0c9a194
    Author: rranganathan <rranganathan@juniper.net>
    Date:   Thu Jun 17 11:04:04 2021 +0530
    
        changed securitygrp testcase
    
    Change-Id: I5033545a6236c2153597a23f80d57edf4bef7882

--- scripts/securitygroup/test_regression.py
+++ scripts/securitygroup/test_regression.py
@@ -1436,9 +1436,10 @@ class SecurityGroupRegressionTests7(BaseSGTest, VerifySecGroup, ConfigPolicy):
         if self.inputs.pcap_on_vm:
             assert verify_tcpdump_count(self, session1, pcap1)
         else:
+            session, pcap = start_tcpdump_for_vm_intf(
+                self, src_vm_fix, src_vn_fq_name, filters=filters)
             assert verify_tcpdump_count(
-                self, None, None, vm_fix_pcap_pid_files=vm_fix_pcap_pid_files, exp_count=exp_count)
-        # stop traffic
+                self, session, pcap, exp_count=0), "pkt count in tcpdump is not ZERO for icmp type %s and code %s" % (icmp_type, icmp_code)
         sent, recv = self.stop_traffic_scapy(sender1, receiver1, recvr=False)
         return True
         # end test_icmp_error_payload_matching
